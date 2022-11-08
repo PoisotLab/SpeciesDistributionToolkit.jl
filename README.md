057a2c92bfcfb4497775dc415e65843323e6cc7a
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-07 21:28:39 -0500

    Add formatter everywhere

--- README.md
+++ README.md
@@ -6,11 +6,21 @@ of `Require`, which is not the case anymore.
 
 The *core* package is `SpeciesDistributionsToolkit.jl`, and it currently ships with:
 
-- `GBIF.jl`, a wrapper around the GBIF API, to retrieve taxa and occurrence data
-- `SimpleSDMDatasets.jl`, a series of wrappers around providers of raster datasets for use in
-species distribution models
-- `SimpleSDMLayers.jl`, a series of types and usual operations to handle raster data
-- `Fauxcurrences.jl`, a package to simulate faux species with distances distributions that
+🦌 `GBIF.jl`
+: a wrapper around the GBIF API, to retrieve taxa and occurrence datasets
+
+
+🗺️ `SimpleSDMDatasets.jl`
+: a series of wrappers around providers of raster datasets for use in species distribution models
+
+
+💻 `SimpleSDMLayers.jl`
+: a series of types and usual operations to handle raster data
+
+
+📊 `Fauxcurrences.jl`
+: a package to simulate faux species with distances distributions that
 mimick empirical ones
 
 [mnrp]: https://monorepo.tools/
+
--- SimpleSDMDatasets/src/SimpleSDMDatasets.jl
+++ SimpleSDMDatasets/src/SimpleSDMDatasets.jl
@@ -16,20 +16,20 @@ const _LAYER_PATH =
 isdir(_LAYER_PATH) || mkpath(_LAYER_PATH)
 
 # Types for datasets and providers
-include(joinpath(@__DIR__, "types", "datasets.jl"))
+include("types/datasets.jl")
 export RasterDataset
 export BioClim, Elevation, MinimumTemperature, MaximumTemperature, AverageTemperature,
     Precipitation, SolarRadiation, WindSpeed, WaterVaporPressure, LandCover,
     HabitatHeterogeneity, Topography
 
-include(joinpath(@__DIR__, "types", "providers.jl"))
+include("types/providers.jl")
 export RasterProvider
 export WorldClim2, EarthEnv, CHELSA1, CHELSA2
 
-include(joinpath(@__DIR__, "types", "futures.jl"))
+include("types/futures.jl")
 export FutureScenario, FutureModel
 
-include(joinpath(@__DIR__, "types", "enums.jl"))
+include("types/enums.jl")
 
 # CMIP6 exports
 export CMIP6Scenario, CMIP6Model
@@ -40,25 +40,25 @@ export ACCESS_CM2, ACCESS_ESM1_5, BCC_CSM2_MR, CanESM5, CanESM5_CanOE, CMCC_ESM2
     IPSL_CM6A_LR, MIROC_ES2L, MIROC6, MPI_ESM1_2_LR, MPI_ESM1_2_HR, MRI_ESM2_0, UKESM1_0_LL
 
 # Specifier types
-include(joinpath(@__DIR__, "types", "specifiers.jl"))
+include("types/specifiers.jl")
 export RasterData, Future
 
 # Common interface
-include(joinpath(@__DIR__, "interface.jl"))
+include("interface.jl")
 
 # Providers
-include(joinpath(@__DIR__, "providers", "CHELSA", "chelsa_v1.jl"))
-include(joinpath(@__DIR__, "providers", "CHELSA", "chelsa_v2.jl"))
-include(joinpath(@__DIR__, "providers", "CHELSA", "chelsa_future_v2.jl"))
-include(joinpath(@__DIR__, "providers", "EarthEnv", "earthenv.jl"))
-include(joinpath(@__DIR__, "providers", "WorldClim", "worldclim_v2.jl"))
-include(joinpath(@__DIR__, "providers", "WorldClim", "worldclim_future_v2.jl"))
+include("providers/CHELSA/chelsa_v1.jl")
+include("providers/CHELSA/chelsa_v2.jl")
+include("providers/CHELSA/chelsa_future_v2.jl")
+include("providers/EarthEnv/earthenv.jl")
+include("providers/WorldClim/worldclim_v2.jl")
+include("providers/WorldClim/worldclim_future_v2.jl")
 
 # Key checker
-include(joinpath(@__DIR__, "keychecker.jl"))
+include("keychecker.jl")
 
 # Downloader function
-include(joinpath(@__DIR__, "downloader.jl"))
+include("downloader.jl")
 export downloader
 
 end # module SimpleSDMDatasets

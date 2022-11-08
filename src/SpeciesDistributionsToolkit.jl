96408b766d7e4e50b5b8a71d9908aa4a7849725f
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-07 22:08:20 -0500

    🏗️ start moving pieces together

--- src/SpeciesDistributionsToolkit.jl
+++ src/SpeciesDistributionsToolkit.jl
@@ -1,5 +1,18 @@
 module SpeciesDistributionsToolkit
 
-greet() = print("Hello World!")
+# We make ample use of re-export
+using Reexport
+
+# GBIF to get species occurrence data
+@reexport using GBIF
+
+# SimpleSDMLayers to wrap everything together
+@reexport using SimpleSDMLayers
+
+# Faucurrences
+@reexport using Fauxcurrences
+
+# SimpleSDMDatasets for access to layers
+@reexport using SimpleSDMDatasets
 
 end # module SpeciesDistributionsToolkit

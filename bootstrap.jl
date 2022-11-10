4d9cf064f53e72df6e4dda7502c028a2af61a8bd
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-10 15:51:32 -0500

    test bootstrap code

--- bootstrap.jl
+++ bootstrap.jl
@@ -5,5 +5,5 @@ components = ["GBIF", "SimpleSDMDatasets", "SimpleSDMLayers", "Fauxcurrences"]
 # Cleanup local install and develop
 for package in components
     Pkg.rm(package)
-    Pkg.develop(; path = package)
+    Pkg.add(; path = package)
 end

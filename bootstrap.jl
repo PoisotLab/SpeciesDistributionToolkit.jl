e705959e5fe877d4586298ed7d3d54a9182de5f0
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-10 15:55:03 -0500

    .

--- bootstrap.jl
+++ bootstrap.jl
@@ -1,9 +1,11 @@
 import Pkg
 
-components = ["GBIF", "SimpleSDMDatasets", "SimpleSDMLayers", "Fauxcurrences"]
+components = ["SimpleSDMDatasets", "SimpleSDMLayers", "GBIF", "Fauxcurrences"]
 
 # Cleanup local install and develop
 for package in components
+    @info "Removing $(package)"
     Pkg.rm(package)
+    @info "Dev'ing $(package)"
     Pkg.develop(; path = "./$(package)")
 end

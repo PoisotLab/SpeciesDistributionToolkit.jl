e3d28236c6b8e25861c8eb703690f928fc2b83b9
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-10 14:34:12 -0500

    Proper todo notes

--- src/integrations/datasets_layers.jl
+++ src/integrations/datasets_layers.jl
@@ -1,5 +1,5 @@
 function SimpleSDMLayers.SimpleSDMPredictor(data::R; kwargs...) where {R <: RasterData}
-    # TODO remove the bounding box information (left/right/top/bottom) from kwargs...
+    # TODO: remove the bounding box information (left/right/top/bottom) from kwargs...
 
     # Get the data
     filepath, filetype, bandnumber, crs = downloader(data;)
@@ -16,7 +16,7 @@ function SimpleSDMLayers.SimpleSDMPredictor(
     future::F;
     kwargs...,
 ) where {R <: RasterData, F <: Future}
-    # TODO remove the bounding box information (left/right/top/bottom) from kwargs...
+    # TODO: remove the bounding box information (left/right/top/bottom) from kwargs...
 
     # Get the data
     filepath, filetype, bandnumber, crs = downloader(data;)

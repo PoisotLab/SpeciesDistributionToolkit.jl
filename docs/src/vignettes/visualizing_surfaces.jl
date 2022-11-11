1738b386f04f1417d789168c9a7b14366f5aed8a
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-10 22:31:15 -0500

    Update another vignette

---
+++ docs/src/vignettes/visualizing_surfaces.jl
@@ -0,0 +1,21 @@
+# Surfaces
+
+using SpeciesDistributionToolkit
+using CairoMakie
+
+# Get data
+
+spatial_extent = (left = -169.0, right = -50.0, bottom = 24.0, top = 71.0)
+
+# ...
+
+elevation = SimpleSDMPredictor(RasterData(WorldClim2, Elevation); spatial_extent...)
+precipitation = SimpleSDMPredictor(RasterData(WorldClim2, BioClim); spatial_extent..., layer="BIO12")
+
+# plot
+
+fig = Figure(resolution(1000, 1000))
+axs = Axis2(fig[1,1], aspect=:data)
+surface!(axs, sprinkle(elevation); color=last(sprinkle(precipitation)), colormap=:lapaz, shading=false)
+current_figure()
+

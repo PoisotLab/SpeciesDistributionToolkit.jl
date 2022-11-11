ccd3588db268efdf20d27050a453dcee98dec36a
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-11 09:13:29 -0500

    Update the surface vignette

--- docs/src/vignettes/visualizing_surfaces.jl
+++ docs/src/vignettes/visualizing_surfaces.jl
@@ -10,12 +10,18 @@ spatial_extent = (left = -169.0, right = -50.0, bottom = 24.0, top = 71.0)
 # ...
 
 elevation = SimpleSDMPredictor(RasterData(WorldClim2, Elevation); spatial_extent...)
-precipitation = SimpleSDMPredictor(RasterData(WorldClim2, BioClim); spatial_extent..., layer="BIO12")
+precipitation =
+    SimpleSDMPredictor(RasterData(WorldClim2, BioClim); spatial_extent..., layer = "BIO12")
 
 # plot
 
-fig = Figure(resolution(1000, 1000))
-axs = Axis2(fig[1,1], aspect=:data)
-surface!(axs, sprinkle(elevation); color=last(sprinkle(precipitation)), colormap=:lapaz, shading=false)
+fig = Figure(; resolution = (1000, 1000))
+axs = Axis2(fig[1, 1]; aspect = :data)
+surface!(
+    axs,
+    sprinkle(elevation);
+    color = last(sprinkle(precipitation)),
+    colormap = :lapaz,
+    shading = false,
+)
 current_figure()
-

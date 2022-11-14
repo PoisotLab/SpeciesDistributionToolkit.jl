ee96da357a5f90b7023a77c60dcf8b0db05553f3
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-14 13:47:51 -0500

    Convert to WGS84 using GDAL_WARP if needed

--- src/io/geotiff.jl
+++ src/io/geotiff.jl
@@ -39,12 +39,31 @@ function geotiff(
     bottom = -90.0,
     top = 90.0,
 ) where {LT <: SimpleSDMLayer}
+    ArchGDAL.read(file) do stuff
+        wkt = ArchGDAL.importPROJ4(ArchGDAL.getproj(stuff))
+        wgs84 = ArchGDAL.importEPSG(4326)
+        # The next comparison is complete bullshit but for some reason, ArchGDAL has no
+        # mechanism to test the equality of coordinate systems. I sort of understand why,
+        # but it's still nonsense. So we are left with checking the string representations.
+        if string(wkt) != string(wgs84)
+            @warn """The dataset is not in WGS84
+            We will convert it to WGS84 using gdal_warp, and write it to a temporary file.
+            This is not an apology, this is a warning.
+            Proceed with caution.
+            """
+            newfile = tempname()
+            run(
+                `$(GDAL.gdalwarp_path()) $file $newfile -t_srs "+proj=longlat +ellps=WGS84"`,
+            )
+            file = newfile
+        end
+    end
 
     # This next block is reading the geotiff file, but also making sure that we
     # clip the file correctly to avoid reading more than we need.
     layer = ArchGDAL.read(file) do dataset
         transform = ArchGDAL.getgeotransform(dataset)
-        wkt = ArchGDAL.getproj(dataset)
+        # wkt = ArchGDAL.getproj(dataset)
 
         # The data we need is pretty much always going to be stored in the first
         # band, but this is not the case for the future WorldClim data.

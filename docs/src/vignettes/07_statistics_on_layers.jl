af22c47ef3945fdffb6a9467390ea50e9ed0ac08
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-11-16 10:19:48 -0500

    Update the vignette on stats

--- docs/src/vignettes/07_statistics_on_layers.jl
+++ docs/src/vignettes/07_statistics_on_layers.jl
@@ -18,8 +18,12 @@ query = [
     "country" => "SE",
     "country" => "FI",
     "country" => "DE",
+    "limit" => 300,
 ]
 presences = occurrences(rangifer, query...)
+while length(presences) <= 3000
+    occurrences!(presences)
+end
 
 # layers
 
@@ -27,19 +31,21 @@ dataprovider = RasterData(CHELSA1, BioClim)
 temperature = 0.1SimpleSDMPredictor(dataprovider; layer = "BIO1", spatial_extent...)
 precipitation = 1.0SimpleSDMPredictor(dataprovider; layer = "BIO12", spatial_extent...)
 
-#
+# A large number of functions from `Statistics` have overloads to be applied directly to the
+# layers. We can get the mean temperature:
 
 mean(temperature)
 
-# 
+# Likewise, we can get the standard deviation:
 
 std(temperature)
 
-# 
+# Because of the way layers support arithmetic operations, we can esasily get the z-score of
+# temperature as a layer:
 
 z_temperature = (temperature - mean(temperature)) / std(temperature)
 
-# 
+# This can be plotted:
 
 fig, ax, hm = heatmap(
     sprinkle(z_temperature)...;
@@ -59,6 +65,7 @@ fig, ax, hm = heatmap(
     figure = (; resolution = (800, 400)),
     axis = (; aspect = DataAspect()),
 )
+scatter!(hm, longitudes(presences), latitudes(presences))
 Colorbar(fig[:, end + 1], hm)
 current_figure()
 

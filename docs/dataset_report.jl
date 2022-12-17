940a6839d82c7b32e67bac1892a67f3d4e345254
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-12-17 12:53:00 -0500

    Work on the dataset cards (#88)
    * work on the dataset cards
    
    * fix a typo
    
    * split examples in multiple folders
    
    * fix the format of generated page list
    
    * dataset model cards
    
    * fix typo
    
    * update
    
    * up
    
    * wrong path fixed
    
    * Fix the listing of layers
    
    * 🧑‍🏫 update the model cards
    
    * 🧑‍🏫 clarify the data loading example
    
    * 🧑‍🏫 add timespans to model cards
    
    * 🧑‍🏫 update datasetcard with extra keys
    
    * 🧑‍🏫 use values in a vignette

--- docs/dataset_report.jl
+++ docs/dataset_report.jl
@@ -1,58 +1,167 @@
-function report(data::RasterData{P, D}) where {P <: RasterProvider, D <: RasterDataset}
-    text = "## $(D)\n"
-    if ~isnothing(SimpleSDMDatasets.layers(data))
-        text *= "**Support for layers** - list with `SimpleSDMDatasets.layers($(typeof(data)))`"
+function _document_layers(
+    data::RasterData{P, D},
+) where {P <: RasterProvider, D <: RasterDataset}
+    if isnothing(SimpleSDMDatasets.layers(data))
+        return """
+        ## Layers
+
+        This dataset has no support for layers.
+        """
     end
-    text *= "\n\n"
-    if ~isnothing(SimpleSDMDatasets.months(data))
-        text *= "**Support for months** - list with `SimpleSDMDatasets.months($(typeof(data)))`"
+    text = "\n## Layers\n\n"
+    text *= "The following layers are accessible through the `layer` keyword:\n\n"
+    text *= "| Layer code | Description |\n"
+    text *= "|------------|-------------|\n"
+    for (k, v) in SimpleSDMDatasets.layerdescriptions(data)
+        text *= "| `$(k)` | $(v) |\n"
     end
-    text *= "\n\n"
-    if ~isnothing(SimpleSDMDatasets.resolutions(data))
-        text *= "**Support for resolutions** - list with `SimpleSDMDatasets.resolutions($(typeof(data)))`"
+    return text
+end
+
+function _document_extrakeys(
+    data::RasterData{P, D},
+) where {P <: RasterProvider, D <: RasterDataset}
+    if isnothing(SimpleSDMDatasets.extrakeys(data))
+        return """
+        ## Additional keyword arguments
+
+        This dataset has no non-standard keywords arguments.
+        """
     end
-    text *= "\n\n"
-    text *= "**Downloaded** as `$(SimpleSDMDatasets.downloadtype(data))`, **data stored** as `$(SimpleSDMDatasets.filetype(data))`\n"
-    text *= "\n\n"
-    # Supported futures?
+    text = "\n## Additional keyword arguments\n\n"
+    text *= "The following keyword arguments can be used with this dataset:\n\n"
+    for (k, v) in SimpleSDMDatasets.extrakeys(data)
+    text *= "**$(String(k))**: $(join(v, ", ", " and "))\n\n"
+    end
+    return text
+end
+
+function _document_resolutions(
+    data::RasterData{P, D},
+) where {P <: RasterProvider, D <: RasterDataset}
+    if isnothing(SimpleSDMDatasets.resolutions(data))
+        return """
+        ## Resolutions
+
+        This dataset is provided in a single resolution
+        """
+    end
+    text = "\n## Resolutions\n\n"
+    text *= "The following resolutions are accessible through the `resolution` keyword argument:\n\n"
+    text *= "| Resolution | Key |\n"
+    text *= "|------------|-------------|\n"
+    for (k, v) in SimpleSDMDatasets.resolutions(data)
+        text *= "| `$(v)` | $(k) |\n"
+    end
+    text *= "\nYou can also list the resolutions using `SimpleSDMDatasets.resolutions($(typeof(data)))`.\n\n"
+    return text
+end
+
+function _document_months(
+    data::RasterData{P, D},
+) where {P <: RasterProvider, D <: RasterDataset}
+    if isnothing(SimpleSDMDatasets.months(data))
+        return """
+        ## Months
+
+        This dataset is not indexed by months
+        """
+    end
+    text = "\n## Months\n\n"
+    text *= "This dataset can be accessed monthly, using the `month` keyword argument.\n\n"
+    text *= "You can list the available months using `SimpleSDMDatasets.months($(typeof(data)))`.\n\n"
+    return text
+end
+
+function _document_scenarios(
+    data::RasterData{P, D},
+) where {P <: RasterProvider, D <: RasterDataset}
+    text = ""
     for S in subtypes(FutureScenario)
         models = []
+        spans = []
         for M in subtypes(FutureModel)
-            if SimpleSDMDatasets.provides(data, Projection(S, M))
+            F = Projection(S, M)
+            if SimpleSDMDatasets.provides(data, F)
                 push!(models, "`$(M)`")
+                if ~isnothing(SimpleSDMDatasets.timespans(data, F))
+                    append!(spans, SimpleSDMDatasets.timespans(data, F))
+                end
             end
         end
         if ~isempty(models)
-            text *= "**Future scenario `$(S)` supported** with models $(join(models, ", ", " and "))"
-            text *= "\n\n"
+            text *= "## Support for future scenario $(S)\n\n"
+            text *= "Note that the future scenarios support the *same* keyword arguments as the contemporary data.\n\n"
+            text *= "**Models**: $(join(models, ", ", " and "))\n\n"
+            if ~isempty(spans)
+                text *= "**Timespans**: $(join(spans, ", ", " and "))\n\n"
+            end
         end
     end
     text *= "\n\n"
     return text
 end
 
-function report(::Type{P}) where {P <: RasterProvider}
+function report(::Type{P}, ::Type{D}) where {P <: RasterProvider, D <: RasterDataset}
     # Name of the provider
-    _header = "# $(P)"
-    # List of supported datasets
-    _datasets = [
-        report(RasterData(P, D)) for
-        D in subtypes(RasterDataset) if SimpleSDMDatasets.provides(P, D)
-    ]
+    _header = "# $(D)"
+    # Short description
+    _description = """
+    The `$(D)` dataset is provided as part of the `$(P)` provider. For more information about this dataset, please refer to: $(SimpleSDMDatasets.url(RasterData(P, D)))
 
+    To access this dataset:
+
+    ~~~julia
+    using SpeciesDistributionToolkit
+    layer = SimpleSDMPredictor(RasterData($(P), $(D))) # You will probably need keyword arguments here
+    ~~~
+
+    The remainder of this page will list the keywords you can use to retrieve specific months, layers, etc.
+    """
     # Prepare and return
     full_text = """$(_header)
 
-    $(reduce(*, _datasets))
+    $(_description)
+
+    $(_document_layers(RasterData(P, D)))
+
+    $(_document_resolutions(RasterData(P, D)))
+
+    $(_document_extrakeys(RasterData(P, D)))
+
+    $(_document_months(RasterData(P, D)))
+
+    $(_document_scenarios(RasterData(P, D)))
     """
     return Markdown.parse(full_text)
 end
 
-# Make sure the path is there
-mkpath(joinpath("docs", "src", "SimpleSDMDatasets", "catalogue"))
+# Make sure the path is therethemes
+dataset_catalogue_path = joinpath("docs", "src", "datasets")
+if ~ispath(dataset_catalogue_path)
+    mkpath(dataset_catalogue_path)
+end
+
+_dataset_catalogue = []
 
 for P in subtypes(RasterProvider)
-    open(joinpath("docs", "src", "SimpleSDMDatasets", "catalogue", "$(P).md"), "w") do io
-        return print(io, report(P))
+    # Create the path if it doesn't exist
+    if ~ispath(joinpath(dataset_catalogue_path, string(P)))
+        mkpath(joinpath(dataset_catalogue_path, string(P)))
     end
+    this_cat = []
+    # Run the report for each dataset
+    for D in subtypes(RasterDataset)
+        if SimpleSDMDatasets.provides(P, D)
+            cardfile = joinpath(dataset_catalogue_path, string(P), "$(D).md")
+            push!(this_cat, string(D) => joinpath(splitpath(cardfile)[3:end]))
+            open(
+                cardfile,
+                "w",
+            ) do io
+                return print(io, report(P, D))
+            end
+        end
+    end
+    push!(_dataset_catalogue, string(P) => this_cat)
 end
--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
+# push!(LOAD_PATH, pwd())
+
 using Documenter
 using SpeciesDistributionToolkit
 using InteractiveUtils
@@ -7,21 +9,31 @@ using Literate
 
 # Generate a report card for each known dataset
 include("dataset_report.jl")
-_dataset_catalogue = [
-    string(P) => joinpath("SimpleSDMDatasets", "catalogue", "$(P).md")
-    for P in subtypes(RasterProvider)
-]
 
 # Compile the vignettes for the top-level package
-const _vignettes_dir = "docs/src/vignettes"
-_list_of_vignettes = filter(f -> endswith(f, ".jl"), readdir(_vignettes_dir; join = true))
-_vignettes_pages = Pair{String, String}[]
-for vignette in _list_of_vignettes
-    Literate.markdown(vignette, joinpath(pwd(), _vignettes_dir))
-    compiled_vignette = replace(vignette, ".jl" => ".md")
-    title = last(split(readlines(vignette)[1], "# # "))
-    path = joinpath(splitpath(compiled_vignette)[3:end])
-    push!(_vignettes_pages, title => path)
+_vignettes_categories = [
+    "layers" => "Working with layers",
+    "occurrences" => "Working with occurrences",
+    "integration" => "Integration examples",
+]
+_vignettes_pages = Pair{String, Vector{Pair{String, String}}}[]
+for _category in _vignettes_categories
+    folder, title = _category
+    vignettes = filter(
+        endswith(".jl"),
+        readdir(joinpath("docs", "src", "vignettes", folder); join = true),
+    )
+    this_category = Pair{String, String}[]
+    for vignette in vignettes
+        Literate.markdown(vignette, joinpath("docs", "src", "vignettes", folder))
+        compiled_vignette = replace(vignette, ".jl" => ".md")
+        push!(
+            this_category,
+            last(split(readlines(vignette)[1], "# # ")) =>
+                joinpath(splitpath(compiled_vignette)[3:end]),
+        )
+    end
+    push!(_vignettes_pages, title => this_category)
 end
 
 makedocs(;
@@ -32,6 +44,7 @@ makedocs(;
     pages = [
         "Index" => "index.md",
         "Vignettes" => _vignettes_pages,
+        "List of datasets" => _dataset_catalogue,
         "GBIF.jl" => [
             "GBIF.jl" => "GBIF/index.md",
             "GBIF data representation" => "GBIF/types.md",
@@ -39,12 +52,9 @@ makedocs(;
         ],
         "SimpleSDMDatasets.jl" => [
             "SimpleSDMDatasets.jl" => "SimpleSDMDatasets/index.md",
-            "Dataset catalogue" => _dataset_catalogue,
-            "For developers" => [
-                "Data representation" => "SimpleSDMDatasets/dev/types.md",
-                "Data retrieval interface" => "SimpleSDMDatasets/dev/interface.md",
-                "Internals" => "SimpleSDMDatasets/dev/internals.md",
-            ],
+            "Data representation" => "SimpleSDMDatasets/dev/types.md",
+            "Data retrieval interface" => "SimpleSDMDatasets/dev/interface.md",
+            "Internals" => "SimpleSDMDatasets/dev/internals.md",
         ],
     ],
 )

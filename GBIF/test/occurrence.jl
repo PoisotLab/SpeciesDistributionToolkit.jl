f5e475e21040c6d8ae4e88d13f5a3a0f32314325
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-02-18 11:20:42 -0500

    GBIF throws 404 (not 410) for deleted resources (#156)
    * 🤢 GBIF throws 404 (not 410) for deleted resources
    
    * 🐛 throws an error when the occurrence is not available
    
    * ✅ 404 error on single occurrence is handled

--- GBIF/test/occurrence.jl
+++ GBIF/test/occurrence.jl
@@ -3,12 +3,17 @@ module TestOccurrence
 using GBIF
 using Test
 
-k = 1258202889
+# This occurrence exists 
+k = 3986160931
 o = occurrence(k)
 @test typeof(o) == GBIFRecord
 @test o.key == k
 
-# Piece of shit uncorrectly formatted occurence
+# This occurrence has been deleted, so this needs some wrapping
+k = 1258202889
+@test_throws "cannot be accessed - error code" occurrence(k)
+
+# This occurence is incorrectly formatted for some reason
 k = 1039645472
 o = occurrence(k)
 @test typeof(o) == GBIFRecord

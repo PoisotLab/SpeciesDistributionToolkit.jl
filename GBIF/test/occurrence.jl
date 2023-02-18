dd8e6389f4d5df6a6dadec420ff4f706a659e8da
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-02-18 09:34:01 -0500

    🤢 GBIF throws 404 (not 410) for deleted resources

--- GBIF/test/occurrence.jl
+++ GBIF/test/occurrence.jl
@@ -3,6 +3,13 @@ module TestOccurrence
 using GBIF
 using Test
 
+# This occurrence exists 
+k = 3986160931
+o = occurrence(k)
+@test typeof(o) == GBIFRecord
+@test o.key == k
+
+# This piece of shit occurrence has been deleted, so this needs some wrapping
 k = 1258202889
 o = occurrence(k)
 @test typeof(o) == GBIFRecord

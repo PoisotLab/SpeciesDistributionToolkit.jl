8e81cb476ebee7c8a5e114008435c70328005a4d
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-04-21 10:21:19 -0400

    Additional docstrings

--- SimpleSDMDatasets/src/types/datasets.jl
+++ SimpleSDMDatasets/src/types/datasets.jl
@@ -7,8 +7,12 @@ given by `RasterProvider`s, and the same dataset can have multiple providers.
 abstract type RasterDataset end
 
 # List of datasets
+# TODO write documentation to also mention what they can be used for, e.g. all DEM can
+# provide Elevation
 struct BioClim <: RasterDataset end
+
 struct Elevation <: RasterDataset end
+
 struct MinimumTemperature <: RasterDataset end
 struct MaximumTemperature <: RasterDataset end
 struct AverageTemperature <: RasterDataset end
@@ -16,6 +20,8 @@ struct Precipitation <: RasterDataset end
 struct SolarRadiation <: RasterDataset end
 struct WindSpeed <: RasterDataset end
 struct WaterVaporPressure <: RasterDataset end
+
 struct LandCover <: RasterDataset end
+
 struct HabitatHeterogeneity <: RasterDataset end
 struct Topography <: RasterDataset end

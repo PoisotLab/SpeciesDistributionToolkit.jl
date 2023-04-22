cbc92beeaf8b366aaa88e442609eab8ced267537
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-04-22 10:22:50 -0400

    feat(phylopic): add a non-exported function to ping

--- Phylopic/src/Phylopic.jl
+++ Phylopic/src/Phylopic.jl
@@ -1,5 +1,18 @@
 module Phylopic
 
-greet() = print("Hello World!")
+import HTTP
+import JSON
+
+const api = "https://api.phylopic.org/"
+
+function ping()
+    req = HTTP.get(api * "ping")
+    if req.status == 204
+        return nothing
+    else
+        throw(ErrorException("The API at $(api) is not responding"))
+    end
+    return nothing
+end
 
 end # module Phylopic

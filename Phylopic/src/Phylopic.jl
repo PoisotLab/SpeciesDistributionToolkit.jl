5098c8cea0ef41831037f54cfca88ac07568867a
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-04-22 10:57:25 -0400

    feat(phylopic): search images

--- Phylopic/src/Phylopic.jl
+++ Phylopic/src/Phylopic.jl
@@ -2,6 +2,7 @@ module Phylopic
 
 import HTTP
 import JSON
+using UUIDs
 
 const api = "https://api.phylopic.org/"
 
@@ -26,6 +27,8 @@ end
 Performs an autocomplete query based on a string, which must be at least two characters in length.
 
 This function will return an *array* of strings, which can be empty if there are no matches. In you want to do things depending on the values returned, check them with `isempty`, not `isnothing`.
+
+The output of this function, when not empty, can be passed to either `Phylopic.nodes` or `Phylopic.images` using their `filter_name` keyword argument. Note that the `filter_name` argument accepts a *single* name, not an array of names.
 """
 function autocomplete(query::AbstractString)
     if length(query) < 2
@@ -39,6 +42,26 @@ function autocomplete(query::AbstractString)
         end
     end
     return AbstractString[]
+end
+
+function images(; filter_license_by=false, filter_license_nc=false, filter_license_sa=false, filter_name="")
+    query_parameters = ""
+    if filter_license_by
+        query_parameters *= "filter_license_by=$(filter_license_by)"
+    end
+    if filter_license_nc
+        query_parameters *= "filter_license_nc=$(filter_license_nc)"
+    end
+    if filter_license_sa
+        query_parameters *= "filter_license_sa=$(filter_license_sa)"
+    end
+    if ~isempty(filter_name)
+        query_parameters *= "filter_name=$(filter_name)"
+    end
+    req = HTTP.get(api * "images?" * replace(query_parameters, " " => "%20"))
+    if isequal(200)(req.status)
+        @info JSON.parse(String(req.body))
+    end
 end
 
 end # module Phylopic

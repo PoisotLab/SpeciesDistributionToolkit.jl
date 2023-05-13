385831f8b4c2c4fa4e4272874a95c664826a9562
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-05-13 17:17:58 -0400

    bug(sdt): writing a raster works without tweaks now
    * bug(sdt): write with GTiff format
    
    Closes #190
    
    * test(sdt): add a test for write/read for geotiff
    
    * test(sdt): update the tests for io
    
    * semver(sdt): v0.0.7
    
    * feat(sdt): remove prefixed file when writing layers

--- test/01_integration_read.jl
+++ test/01_integration_read.jl
@@ -17,4 +17,14 @@ layer2 = SpeciesDistributionToolkit._read_geotiff(f, SimpleSDMPredictor)
 @test typeof(layer2) <: SimpleSDMPredictor
 @test all(values(layer) .== values(layer2))
 
+# Write the data
+f = tempname() * ".tiff" # This is important because the save function checks extensions
+SpeciesDistributionToolkit.save(f, [layer]; nodata = typemax(D))
+@test isfile(f)
+
+# Read the data
+layer2 = SimpleSDMPredictor(f)
+@test typeof(layer2) <: SimpleSDMPredictor
+@test all(values(layer) .== values(layer2))
+
 end

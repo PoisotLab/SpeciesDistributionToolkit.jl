f38893c71993aca6cbb87a0df53a1e2b929ec95e
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-05-13 13:27:08 -0400

    bug(layers): use broadcast
    The previous implementation would have crashed the F out on layers with
    nothing-valued cells

--- SimpleSDMLayers/src/lib/overloads.jl
+++ SimpleSDMLayers/src/lib/overloads.jl
@@ -335,12 +335,8 @@ Base.://(n::Number, layer::T) where {T <: SimpleSDMLayer} = broadcast(x -> n // 
 Base.://(layer::T, n::Number) where {T <: SimpleSDMLayer} = broadcast(x -> x // n, layer)
 Base.:%(n::Number, layer::T) where {T <: SimpleSDMLayer} = broadcast(x -> n % x, layer)
 Base.:%(layer::T, n::Number) where {T <: SimpleSDMLayer} = broadcast(x -> x % n, layer)
-
-function Base.:-(layer::T) where {T <: SimpleSDMLayer}
-    ngrid = -SimpleSDMLayers.grid(layer)
-    RT = T <: SimpleSDMResponse ? SimpleSDMResponse : SimpleSDMPredictor
-    return RT(ngrid, layer.left, layer.right, layer.bottom, layer.top)
-end
+Base.:-(layer::T) where {T <: SimpleSDMLayer} = broadcast(-, layer)
+Base.:!(layer::T) where {T <: SimpleSDMLayer} = broadcast(!, layer)
 
 function Base.:!(layer::T) where {T <: SimpleSDMLayer}
     ngrid = (!).(SimpleSDMLayers.grid(layer))

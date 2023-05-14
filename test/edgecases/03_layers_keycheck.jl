b7ae7512b0e577fa554abbe327c32c57e1018283
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-05-14 11:25:44 -0400

    bug(datasets): fix a typo in the unit test error message

--- test/edgecases/03_layers_keycheck.jl
+++ test/edgecases/03_layers_keycheck.jl
@@ -4,6 +4,6 @@ using SpeciesDistributionToolkit
 using Test
 
 provider = RasterData(WorldClim2, BioClim)
-@test_throws "the keyword argument layers is not" SimpleSDMPredictor(provider; layers=3)
+@test_throws "The keyword argument layers is not" SimpleSDMPredictor(provider; layers=3)
 
 end

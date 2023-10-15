7ae5b8ffbe786e62d3f43a21a5c217cb439b4e32
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-15 11:55:30 -0400

    perf(sdt): Array for a vector of layes
    Closes #212

--- src/quality_of_life.jl
+++ src/quality_of_life.jl
@@ -1,7 +1,6 @@
-Base.keys(layers::Vector{T}) where {T <: SimpleSDMLayers.SimpleSDMLayer} = reduce(intersect, keys.(layers))
-
 function Base.Array(layers::Vector{T}) where {T <: SimpleSDMLayers.SimpleSDMLayer}
-    k = keys(layers) # List of keys commons to all layers
+    @assert SimpleSDMLayers._layers_are_compatible(layers)
+    k = reduce(intersect, [findall(!isnothing, grid(layer)) for layer in layers])
     R = SimpleSDMLayers._inner_type(first(layers))
     X = Matrix{R}(undef, length(layers), length(k))
     for i in eachindex(k)

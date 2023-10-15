e6b88cda6c2b06bd9598bae7a198755d420433d0
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-15 11:49:39 -0400

    feat(sdt): auto-generate the code for most MultivariateStats

--- ext/MultivariateExtension.jl
+++ ext/MultivariateExtension.jl
@@ -4,52 +4,60 @@ using SpeciesDistributionToolkit
 using MultivariateStats
 using StatsAPI
 
-function StatsAPI.fit(
-    ::Type{MultivariateStats.PCA},
-    X::Vector{T};
-    kwargs...,
-) where {T <: SimpleSDMLayers.SimpleSDMLayer}
-    @assert SimpleSDMLayers._layers_are_compatible(X)
-    return StatsAPI.fit(MultivariateStats.PCA, Array(X); kwargs...)
-end
+# These types have a fit method
+types_to_fit = [:PCA, :PPCA, :KernelPCA, :Whitening, :MDS, :MetricMDS]
 
-function StatsAPI.predict(
-    M::MultivariateStats.PCA,
-    X::Vector{T},
-) where {T <: SimpleSDMLayers.SimpleSDMLayer}
-    @assert SimpleSDMLayers._layers_are_compatible(X)
-    D = StatsAPI.predict(M, Array(X))
-    O = [similar(X[1], eltype(D)) for i in 1:MultivariateStats.outdim(M)]
-    for i in axes(O, 1)
-        for (j, k) in enumerate(keys(O[i]))
-            O[i][k] = D[i, j]
+# These types have a transform method
+types_to_transform = [:Whitening]
+
+# These types have a predict method
+types_to_predict = [:PCA, :PPCA, :KernelPCA, :MDS, :MetricMDS]
+
+for tf in types_to_fit
+    eval(
+        quote
+            function StatsAPI.fit(::Type{MultivariateStats.$tf}, X::Vector{T}; kwargs...) where {T <: SimpleSDMLayers.SimpleSDMLayer}
+                @assert SimpleSDMLayers._layers_are_compatible(X)
+                return StatsAPI.fit(MultivariateStats.$tf, Array(X); kwargs...)
+            end
         end
-    end
-    return O
+    )
 end
 
-function StatsAPI.fit(
-    ::Type{MultivariateStats.Whitening},
-    X::Vector{T};
-    kwargs...,
-) where {T <: SimpleSDMLayers.SimpleSDMLayer}
-    @assert SimpleSDMLayers._layers_are_compatible(X)
-    return StatsAPI.fit(MultivariateStats.Whitening, Array(X); kwargs...)
-end
-
-function MultivariateStats.transform(
-    W::MultivariateStats.Whitening,
-    X::Vector{T},
-) where {T <: SimpleSDMLayers.SimpleSDMLayer}
-    @assert SimpleSDMLayers._layers_are_compatible(X)
-    D = MultivariateStats.transform(W, Array(X))
-    O = [similar(X[1], eltype(D)) for i in 1:length(X)]
-    for i in axes(O, 1)
-        for (j, k) in enumerate(keys(O[i]))
-            O[i][k] = D[i, j]
+for tf in types_to_transform
+    eval(
+        quote
+            function MultivariateStats.transform(f::MultivariateStats.$tf, X::Vector{T}; kwargs...) where {T <: SimpleSDMLayers.SimpleSDMLayer}
+                @assert SimpleSDMLayers._layers_are_compatible(X)
+                D = MultivariateStats.transform(f, Array(X); kwargs...)
+                O = [similar(X[1], eltype(D)) for i in 1:length(X)]
+                for i in axes(O, 1)
+                    for (j, k) in enumerate(keys(O[i]))
+                        O[i][k] = D[i, j]
+                    end
+                end
+                return O
+            end
         end
-    end
-    return O
+    )
+end
+
+for tf in types_to_predict
+    eval(
+        quote
+            function StatsAPI.predict(f::MultivariateStats.$tf, X::Vector{T}; kwargs...) where {T <: SimpleSDMLayers.SimpleSDMLayer}
+                @assert SimpleSDMLayers._layers_are_compatible(X)
+                D = StatsAPI.predict(f, Array(X); kwargs...)
+                O = [similar(X[1], eltype(D)) for i in 1:MultivariateStats.outdim(M)]
+                for i in axes(O, 1)
+                    for (j, k) in enumerate(keys(O[i]))
+                        O[i][k] = D[i, j]
+                    end
+                end
+                return O
+            end
+        end
+    )
 end
 
 end

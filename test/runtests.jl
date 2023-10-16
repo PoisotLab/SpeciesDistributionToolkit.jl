a8298c2132df5df081d5b53ea4a081357804136a
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-16 14:45:35 -0400

    ci(sdt): only run the top-level tests and ignore the component tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,15 @@
 using TestItemRunner
 
-@run_package_tests filter=ti->!(:skipci in ti.tags) verbose=true
+function test_is_valid(ti)
+    if :skipci in ti.tags
+        return false
+    end
+    for component in ["GBIF", "Phylopic", "Fauxcurrences", "SimpleSDMLayers", "SimpleSDMDatasets"]
+        if contains(ti.filename, "$(component)/src")
+            return false
+        end
+    end
+    return true
+end
+
+@run_package_tests filter=test_is_valid verbose=true

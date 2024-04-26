db393e7f0196f219cfab16153d35069cbf7f583b
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-04-26 12:08:47 -0400

    semver(gbif): precompile issue fixed

--- GBIF/src/GBIF.jl
+++ GBIF/src/GBIF.jl
@@ -5,6 +5,17 @@ using JSON
 using Dates
 using Tables
 
+function safeget(endpoint)
+    body = UInt8[]
+    rsp = HTTP.open("GET", endpoint) do http
+        while !eof(http)
+            append!(body, readavailable(http))
+        end
+        close(HTTP.Connections.getrawstream(http))
+    end
+    return rsp.status, String(body)
+end
+
 const gbifurl = "http://api.gbif.org/v1/"
 
 """
@@ -14,12 +25,8 @@ Returns an *array* of values that can be enumerated by the GBIF API.
 """
 function enumerablevalues()
     endpoint = GBIF.gbifurl * "enumeration/basic"
-    req = HTTP.get(endpoint)
-    if isequal(200)(req.status)
-        response = JSON.parse(String(req.body))
-        return convert(Vector{String}, response)
-    end
-    return nothing
+    st, bd = safeget(endpoint)
+    return isequal(200)(st) ? convert(Vector{String}, JSON.parse(bd)) : nothing
 end
 
 # We load the keys that can be enumerated when we load the package
@@ -33,10 +40,9 @@ For a given enumerable value (given as a string as reported by the output of the
 function enumeratedvalues(enumerable::String)
     if enumerable in GBIF.gbifenumkeys
         endpoint = GBIF.gbifurl * "enumeration/basic/$(enumerable)"
-        req = HTTP.get(endpoint)
-        if isequal(200)(req.status)
-            response = JSON.parse(String(req.body))
-            return convert(Vector{String}, response)
+        st, bd = safeget(endpoint)
+        if isequal(200)(st)
+            return convert(Vector{String}, JSON.parse(bd))
         else
             throw(ErrorException("Unable to enumerate the value $(enumerable)"))
         end

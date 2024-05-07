6f6156f27bd0f05939627f8dceb57c24315f3a71
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-05-07 11:23:14 -0400

    bug(faux): new type name

--- Fauxcurrences/src/coordinates.jl
+++ Fauxcurrences/src/coordinates.jl
@@ -1,11 +1,11 @@
 """
-    get_valid_coordinates(observations::GBIFRecords, layer::T) where {T <: SimpleSDMLayer}
+    get_valid_coordinates(observations::GBIFRecords, layer::T) where {T <: SDMLayer}
 
 Get the coordinates for a list of observations, filtering the ones that do not
 correspond to valid layer positions. Valid layer positions are defined as
 falling within a valued pixel from the layer.
 """
-function get_valid_coordinates(observations::GBIFRecords, layer::T) where {T <: SimpleSDMLayer}
+function get_valid_coordinates(observations::GBIFRecords, layer::T) where {T <: SDMLayer}
     xy = [(observations[i].longitude, observations[i].latitude) for i in eachindex(observations)]
     filter!(c -> !isnothing(layer[c...]), xy)
     return hcat(collect.(unique(xy))...)

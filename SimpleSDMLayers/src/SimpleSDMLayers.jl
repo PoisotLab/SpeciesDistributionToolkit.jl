383efc0a0910e5ccf5574cc3cf1721b50fe22715
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-05-07 10:29:21 -0400

    bug(layers): namespace

--- SimpleSDMLayers/src/SimpleSDMLayers.jl
+++ SimpleSDMLayers/src/SimpleSDMLayers.jl
@@ -1,13 +1,13 @@
 """
-### SDMLayers
+### SimpleSDMLayers
 
-`SDMLayers` is a package to provide easy to use, georeferenced representations
+`SimpleSDMLayers` is a package to provide easy to use, georeferenced representations
 of raster data to assist with the construction of species distribution models.
 The package provides a *single* type, `SDMLayer`, which behaves like an array,
 an iterable object, and a table. It furthers implements core operations on
 rasters.
 """
-module SDMLayers
+module SimpleSDMLayers
 
 using TestItems
 

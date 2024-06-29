90fc63d7a413c9b2de4cded09509153c637cafaa
tpoisot <timothee.poisot@umontreal.ca>
2024-06-29 16:36:37 -0400

    perf(layers)?: slidingwindow

--- SimpleSDMLayers/src/slidingwindow.jl
+++ SimpleSDMLayers/src/slidingwindow.jl
@@ -1,25 +1,32 @@
 function slidingwindow(f::Function, layer::SDMLayer, radius::AbstractFloat; threads::Bool=true)
+    
+    # Return type for the operation
     _rtype = eltype(f(values(layer)[1:min(3, length(layer))]))
+
+    # Allocate the return object
     windowed = similar(layer, _rtype)
+
+    # Prepare the projection to EPSG:4326
+    prj = Proj.Transformation(layer.crs, "EPSG:4326"; always_xy=true)
+
     if threads
         @info "Running on threads"
         Threads.@threads for center in CartesianIndices(layer)
-            windowed[center] = f(SimpleSDMLayers.__window(layer, center, radius))
+            windowed[center] = f(SimpleSDMLayers.__window(layer, center, radius, prj))
         end
     else
         @info "Running on single thread"
         for center in CartesianIndices(layer)
-            windowed[center] = f(SimpleSDMLayers.__window(layer, center, radius))
+            windowed[center] = f(SimpleSDMLayers.__window(layer, center, radius, prj))
         end
     end
     return windowed
 end
 
-function __get_bounded_grid_coordinate_by_latlon(layer::SDMLayer, longitude, latitude)
+function __get_bounded_grid_coordinate_by_latlon(layer::SDMLayer, longitude, latitude, prj)
     eastings = LinRange(layer.x..., size(layer.grid, 2) + 1)
     northings = LinRange(layer.y..., size(layer.grid, 1) + 1)
     
-    prj = Proj.Transformation("EPSG:4326", layer.crs; always_xy=true)
     easting, northing = prj(longitude, latitude)
 
     # Prepare the coordinate
@@ -32,7 +39,7 @@ function __get_bounded_grid_coordinate_by_latlon(layer::SDMLayer, longitude, lat
     return (ni, ei)
 end
 
-function __window(layer::SDMLayer, center::CartesianIndex, radius::AbstractFloat)
+function __window(layer::SDMLayer, center::CartesianIndex, radius::AbstractFloat, prj)
     easting_stride = 0.5 * (layer.x[2] - layer.x[1]) / size(layer, 2)
     northing_stride = 0.5 * (layer.y[2] - layer.y[1]) / size(layer, 1)
 
@@ -42,9 +49,6 @@ function __window(layer::SDMLayer, center::CartesianIndex, radius::AbstractFloat
     # Center in coordinates
     ce = eastings[center.I[2]]
     cn = northings[center.I[1]]
-
-    # Prepare the projection to EPSG:4326
-    prj = Proj.Transformation(layer.crs, "EPSG:4326"; always_xy=true)
 
     # Get the center of the focal cell
     center_latlon = prj(ce, cn)
@@ -56,10 +60,10 @@ function __window(layer::SDMLayer, center::CartesianIndex, radius::AbstractFloat
     min_lon = center_latlon[1] - (360.0 * 1.001radius) / (π * 6371.0)
 
     # Convert to grid coordinates
-    lower_left = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, min_lon, min_lat)
-    lower_right = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, max_lon, min_lat)
-    upper_left = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, min_lon, max_lat)
-    upper_right = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, max_lon, max_lat)
+    lower_left = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, min_lon, min_lat, prj)
+    lower_right = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, max_lon, min_lat, prj)
+    upper_left = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, min_lon, max_lat, prj)
+    upper_right = SimpleSDMLayers.__get_bounded_grid_coordinate_by_latlon(layer, max_lon, max_lat, prj)
 
     # Get the range for approximate window
     irange = extrema(first.([lower_left, lower_right, upper_left, upper_right]))

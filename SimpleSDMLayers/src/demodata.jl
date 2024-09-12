70e2b8b41eedb2f10c9349636bfe9a4f169bd42f
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-09-12 15:54:14 -0400

    test(layers): fix outcome

--- SimpleSDMLayers/src/demodata.jl
+++ SimpleSDMLayers/src/demodata.jl
@@ -20,12 +20,12 @@ end
 
 @testitem "We can load the demo layer" begin
     layer = SimpleSDMLayers.__demodata()
-    @test layer.crs == "NAD83 / Quebec Albers"
+    @test layer.crs == "+proj=aea +lat_0=44 +lon_0=-68.5 +lat_1=60 +lat_2=46 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 end
 
 @testitem "We can load the reduced demo layer" begin
     layer = SimpleSDMLayers.__demodata(reduced=true)
-    @test layer.crs == "NAD83 / Quebec Albers"
+    @test layer.crs == "+proj=aea +lat_0=44 +lon_0=-68.5 +lat_1=60 +lat_2=46 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
     @test size(layer) == (400, 400)
 end
 

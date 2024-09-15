98be1e42f9478450e1b3e80cef4b19b532545501
tpoisot <timothee.poisot@umontreal.ca>
2024-09-14 21:46:21 -0400

    feat(stac): add required fields to the type

--- STACApi/src/catalog.jl
+++ STACApi/src/catalog.jl
@@ -1,8 +1,10 @@
 struct Catalog
     url::String
+    id::String
     title::String
     description::String
     version::VersionNumber
+    extensions::Vector{String}
 end
 
 function Catalog(url)
@@ -10,17 +12,18 @@ function Catalog(url)
     output = __handle_get_response(req)
     return Catalog(
         url,
-        output["title"],
-        output["description"],
-        VersionNumber(output["stac_version"])
+        get(output, "id", ""),
+        get(output, "title", ""),
+        get(output, "description", ""),
+        VersionNumber(output["stac_version"]),
+        get(output, "extensions", String[]),
     )
 end
 
 @testitem "We can get the GEOBON STAC landing page" begin
     geobon = STACApi.Catalog("https://stac.geobon.org/")
     @test geobon.title == "BON in a Box STAC"
+    @test geobon.id == "geobon-stac"
 end
 
-function Base.show(io::IO, catalog::STACApi.Catalog)
-    print(io, "📚 $(catalog.title) @ $(catalog.url)")
-end
+Base.show(io::IO, catalog::STACApi.Catalog) = print(io, "📚 $(catalog.id) @ $(catalog.url)")

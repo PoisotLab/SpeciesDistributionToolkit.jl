4b9be6febb9533fd50457f6afe8a3dc23836df36
tpoisot <timothee.poisot@umontreal.ca>
2024-09-15 17:08:54 -0400

    doc: second polygon

--- docs/src/tutorials/polygons.jl
+++ docs/src/tutorials/polygons.jl
@@ -1,9 +1,9 @@
 # # Working with polygons
 
 using SpeciesDistributionToolkit
+using CairoMakie
 import GeoJSON
 import Downloads
-using CairoMakie
 CairoMakie.activate!(; type = "png", px_per_unit = 3.0) #hide
 
 # In this tutorial, we will clip a layer to a polygon (in GeoJSON format), then
@@ -19,19 +19,25 @@ CairoMakie.activate!(; type = "png", px_per_unit = 3.0) #hide
 
 # Thanks to the good people in the French Open Street Map community, it is very
 # simple to query a `.geojson` file for any OSM identifier. In our case, we will
-# download the polygon that describes the state of Vermont:
+# download the polygons that describes the state of Vermont and New Hampshire:
 
 VT = GeoJSON.read(Downloads.download("http://polygons.openstreetmap.fr/get_geojson.py?id=60759", tempname()))[1][1];
+NH = GeoJSON.read(Downloads.download("http://polygons.openstreetmap.fr/get_geojson.py?id=67213", tempname()))[1][1];
 
 # The next step is to get a layer, and so we will download the data about
 # deciduous broadleaf trees from [EarthEnv](/datasets/EarthEnv#landcover):
 
-bbox = (; left = -73.7, right = -71., bottom = 42.5, top = 45.5)
+bbox = (; left = -73.7, right = -65., bottom = 42.5, top = 45.5)
 provider = RasterData(EarthEnv, LandCover)
 layer = SDMLayer(provider; layer = "Deciduous Broadleaf Trees", bbox...)
 
 # There are a few ways to interact with a polygon. The first thing we can try is
 # to `hide!` everything that is within the polygon from the original layer:
+
+hide!(layer, NH)
+heatmap(layer; colormap = [:lightgrey, :black], axis = (; aspect = DataAspect()))
+
+#-
 
 hide!(layer, VT)
 heatmap(layer; colormap = [:lightgrey, :black], axis = (; aspect = DataAspect()))
@@ -49,6 +55,11 @@ heatmap(layer; colormap = [:lightgrey, :black], axis = (; aspect = DataAspect())
 # The opposite operation to `hide!` is `reveal!`, which will show the cells that
 # are inside the polygon. This is the inverse operation from `hide!`, so we can
 # use this to return to the original layer.
+
+reveal!(layer, NH)
+heatmap(layer; colormap = [:lightgrey, :black], axis = (; aspect = DataAspect()))
+
+#-
 
 reveal!(layer, VT)
 heatmap(layer; colormap = [:lightgrey, :black], axis = (; aspect = DataAspect()))
@@ -89,3 +100,4 @@ end
 
 scatter!(mask(presences, VT), color=:orange, markersize=3)
 current_figure() #hide
+

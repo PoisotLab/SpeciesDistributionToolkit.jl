3653000c96c9c1ed317926fb216f0c7c5954c147
tpoisot <timothee.poisot@umontreal.ca>
2024-09-15 23:13:37 -0400

    xxx: test

--- test.jl
+++ test.jl
@@ -9,7 +9,7 @@ url_gadm = "https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_CHE_0.json"
 poly = GeoJSON.read(Downloads.download(url_gadm, tempname()))
 
 provider = RasterData(WorldClim2, Elevation)
-layer = SDMLayer(provider; resolution=0.5, left=0.0, right=20.0, bottom=35.0, top=55.0)
+layer = SDMLayer(provider; resolution=2.5, left=0.0, right=20.0, bottom=35.0, top=55.0)
 
 heatmap(trim(layer); colormap = :navia, axis = (; aspect = DataAspect()))
 

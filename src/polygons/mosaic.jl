2859a05f004380fe44b8441b54b3ecf90ece676c
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-09-18 13:59:17 -0400

    feat: pass arguments to f in mosaic

--- src/polygons/mosaic.jl
+++ src/polygons/mosaic.jl
@@ -1,13 +1,13 @@
 """
     SimpleSDMLayers.mosaic(f, layer, polygons)
 """
-function SimpleSDMLayers.mosaic(f, layer::SDMLayer, polygons)
-    rtype = eltype(f(layer))
+function SimpleSDMLayers.mosaic(f, layer::SDMLayer, polygons::T, args...; kwargs...) where {T <: GeoJSON.GeoJSONT}
+    rtype = eltype(f(layer, args...; kwargs...))
     out = zeros(layer, rtype)
     zones = [mask!(copy(layer), poly) for poly in polygons]
     for i in eachindex(zones)
         if ~isempty(zones[i])
-            out.grid[findall(zones[i].indices)] .= f(zones[i])
+            out.grid[findall(zones[i].indices)] .= f(zones[i], args...; kwargs...)
         end
     end
     return out

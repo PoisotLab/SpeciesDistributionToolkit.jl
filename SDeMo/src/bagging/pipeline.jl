cabfd4ea66ff9795d6d7ba99df0883df208dd7ed
tpoisot <timothee.poisot@umontreal.ca>
2024-09-21 22:06:53 -0400

    doc: clarification

--- SDeMo/src/bagging/pipeline.jl
+++ SDeMo/src/bagging/pipeline.jl
@@ -19,6 +19,9 @@ end
 Returns the prediction for the ensemble of models a dataset `X`. The function
 used to aggregate the outputs from different models is `consensus` (defaults to
 `median`). All other keyword arguments are passed to `predict`.
+
+To get a direct estimate of the variability, the `consensus` function can be
+changed to `iqr` (inter-quantile range), or any measure of variance.
 """
 function StatsAPI.predict(ensemble::Bagging, X; consensus = median, kwargs...)
     ŷ = [predict(component, X; kwargs...) for component in ensemble.models]

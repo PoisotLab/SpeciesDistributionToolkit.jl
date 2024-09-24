397e11d92002517ba7de7c04e613a3661e0591f5
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-09-24 13:27:25 -0400

    feat(sdt): use the correct return type of SDeMo values

--- src/integrations/sdemo.jl
+++ src/integrations/sdemo.jl
@@ -23,28 +23,32 @@ end
 # TODO SDM, Bagging, and Ensemble should be AbstractSDM
 
 function SDeMo.predict(sdm::SDM, layers::Vector{T}, args...; kwargs...) where {T <: SDMLayer}
-    pr = zeros(layers[1], Float32)
     X = _X_from_layers(layers)
-    pr.grid[findall(pr.indices)] .= predict(sdm, X, args...; kwargs...)
+    prediction = predict(sdm, X, args...; kwargs...)
+    pr = zeros(layers[1], eltype(prediction))
+    pr.grid[findall(pr.indices)] .= prediction
     return pr
 end
 
 function SDeMo.predict(sdm::Bagging, layers::Vector{T}, args...; kwargs...) where {T <: SDMLayer}
-    pr = zeros(layers[1], Float32)
     X = _X_from_layers(layers)
-    pr.grid[findall(pr.indices)] .= predict(sdm, X, args...; kwargs...)
+    prediction = predict(sdm, X, args...; kwargs...)
+    pr = zeros(layers[1], eltype(prediction))
+    pr.grid[findall(pr.indices)] .= prediction
     return pr
 end
 
 function SDeMo.partialresponse(sdm::SDM, layers::Vector{T}, idx::Integer; kwargs...) where {T <: SDMLayer}
-    pr = zeros(layers[1], Float32)
-    pr.grid[findall(pr.indices)] .= partialresponse(sdm, idx, values(layers[idx]); kwargs...)[end]
+    partrep = partialresponse(sdm, idx, values(layers[idx]); kwargs...)[end]
+    pr = zeros(layers[1], eltype(partrep))
+    pr.grid[findall(pr.indices)] .= partrep
     return pr
 end
 
 function SDeMo.explain(sdm::SDM, layers::Vector{T}, idx::Integer; kwargs...) where {T <: SDMLayer}
-    pr = zeros(layers[1], Float32)
     X = _X_from_layers(layers)
-    pr.grid[findall(pr.indices)] .= explain(sdm, idx; instances=X, kwargs...)
+    expln = explain(sdm, idx; instances=X, kwargs...)
+    pr = zeros(layers[1], eltype(expln))
+    pr.grid[findall(pr.indices)] .= expln
     return pr
 end

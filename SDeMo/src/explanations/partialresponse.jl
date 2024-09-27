cc77f83aa9a17b2243634ae04c02469259bf55ea
tpoisot <timothee.poisot@umontreal.ca>
2024-09-27 13:17:50 -0400

    feat: partialresponse works with an ensemble

--- SDeMo/src/explanations/partialresponse.jl
+++ SDeMo/src/explanations/partialresponse.jl
@@ -1,35 +1,35 @@
-function _make_partialresponse_data(sdm::SDM, variable::Integer, npoints::Integer)
-    target = LinRange(extrema(features(sdm, variable))..., npoints)
-    nx = zeros(eltype(features(sdm)), size(features(sdm), 1), length(target))
+function _make_partialresponse_data(model::T, variable::Integer, npoints::Integer) where {T <: AbstractSDM}
+    target = LinRange(extrema(features(model, variable))..., npoints)
+    nx = zeros(eltype(features(model)), size(features(model), 1), length(target))
     nx[variable, :] .= target
     return nx
 end
 
-function _make_partialresponse_data(sdm::SDM, variable::Integer)
-    target = sort(unique(features(sdm, variable)))
-    nx = zeros(eltype(features(sdm)), size(features(sdm), 1), length(target))
+function _make_partialresponse_data(model::T, variable::Integer) where {T <: AbstractSDM}
+    target = sort(unique(features(model, variable)))
+    nx = zeros(eltype(features(model)), size(features(model), 1), length(target))
     nx[variable, :] .= target
     return nx
 end
 
-function _make_partialresponse_data(sdm::SDM, variable::Integer, values)
-    nx = zeros(eltype(features(sdm)), size(features(sdm), 1), length(values))
+function _make_partialresponse_data(model::T, variable::Integer, values) where {T <: AbstractSDM}
+    nx = zeros(eltype(features(model)), size(features(model), 1), length(values))
     nx[variable, :] .= collect(values)
     return nx
 end
 
-function _fill_partialresponse_data!(nx, sdm::SDM, variable, inflated)
+function _fill_partialresponse_data!(nx, model::T, variable, inflated) where {T <: AbstractSDM}
     for i in axes(nx, 1)
         if i != variable
             reducer = inflated ? rand([mean, median, maximum, minimum, rand]) : mean
-            nx[i, :] .= reducer(features(sdm, i))
+            nx[i, :] .= reducer(features(model, i))
         end
     end
     return nx
 end
 
 """
-    partialresponse(sdm::SDM, i::Integer, args...; inflated::Bool, kwargs...)
+    partialresponse(model::T, i::Integer, args...; inflated::Bool, kwargs...)
 
 This method returns the partial response of applying the trained model to a
 simulated dataset where all variables *except* `i` are set to their mean value.
@@ -46,14 +46,14 @@ The different arguments that can follow the variable position are
 
 All keyword arguments are passed to `predict`.
 """
-function partialresponse(sdm::SDM, i::Integer, args...; inflated::Bool=false, kwargs...)
-    nx = SDeMo._make_partialresponse_data(sdm, i, args...)
-    SDeMo._fill_partialresponse_data!(nx, sdm, i, inflated)
-    return (nx[i,:], predict(sdm, nx; kwargs...))
+function partialresponse(model::T, i::Integer, args...; inflated::Bool=false, kwargs...) where {T <: AbstractSDM}
+    nx = SDeMo._make_partialresponse_data(model, i, args...)
+    SDeMo._fill_partialresponse_data!(nx, model, i, inflated)
+    return (nx[i,:], predict(model, nx; kwargs...))
 end
 
 """
-    partialresponse(sdm::SDM, i::Integer, j::Integer, s::Tuple=(50, 50); inflated::Bool, kwargs...)
+    partialresponse(model::T, i::Integer, j::Integer, s::Tuple=(50, 50); inflated::Bool, kwargs...)
 
 This method returns the partial response of applying the trained model to a
 simulated dataset where all variables *except* `i` and `j` are set to their mean
@@ -65,11 +65,11 @@ and `j`, the size of which is given by the last argument `s` (defaults to 50 ×
 
 All keyword arguments are passed to `predict`.
 """
-function partialresponse(sdm::SDM, i::Integer, j::Integer, s::Tuple=(50, 50); inflated::Bool=false, kwargs...)
-    irange = LinRange(extrema(features(sdm, i))..., s[1])
-    jrange = LinRange(extrema(features(sdm, j))..., s[2])
+function partialresponse(model::T, i::Integer, j::Integer, s::Tuple=(50, 50); inflated::Bool=false, kwargs...) where {T <: AbstractSDM}
+    irange = LinRange(extrema(features(model, i))..., s[1])
+    jrange = LinRange(extrema(features(model, j))..., s[2])
 
-    nx = zeros(eltype(features(sdm)), size(features(sdm), 1), length(irange)*length(jrange))
+    nx = zeros(eltype(features(model)), size(features(model), 1), length(irange)*length(jrange))
 
     nx[i,:] .= repeat(irange, outer=length(jrange))
     nx[j,:] .= repeat(jrange, inner=length(irange))
@@ -77,16 +77,16 @@ function partialresponse(sdm::SDM, i::Integer, j::Integer, s::Tuple=(50, 50); in
     for v in axes(nx, 1)
         if (v != i)&(v != j)
             reducer = inflated ? rand([mean, median, maximum, minimum, rand]) : mean
-            nx[v,:] .= reducer(features(sdm, v))
+            nx[v,:] .= reducer(features(model, v))
         end
     end
 
-    return (irange, jrange, reshape(predict(sdm, nx; kwargs...), s))
+    return (irange, jrange, reshape(predict(model, nx; kwargs...), s))
 end
 
 @testitem "We can get partial responses for a model" begin
     X, y = SDeMo.__demodata()
-    model = SDM(MultivariateTransform{PCA}, NaiveBayes, X, y)
+    model = model(MultivariateTransform{PCA}, NaiveBayes, X, y)
     train!(model)
     pr = partialresponse(model, 1)
     @test eltype(pr[2]) <: Bool
@@ -94,7 +94,7 @@ end
 
 @testitem "We can get inflated partial responses for a model" begin
     X, y = SDeMo.__demodata()
-    model = SDM(MultivariateTransform{PCA}, NaiveBayes, X, y)
+    model = model(MultivariateTransform{PCA}, NaiveBayes, X, y)
     train!(model)
     pr = partialresponse(model, 1; inflated=true)
     @test eltype(pr[2]) <: Bool

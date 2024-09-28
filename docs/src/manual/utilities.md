84a6191251f75c8476f806a3922789595f25e5fb
tpoisot <timothee.poisot@umontreal.ca>
2024-09-28 14:07:57 -0400

    semver(sdt): v0.1.3

---
+++ docs/src/manual/utilities.md
@@ -0,0 +1,5 @@
+# Other utility functions
+
+```@docs
+rangediff
+```
--- src/utilities.jl
+++ src/utilities.jl
@@ -2,7 +2,9 @@
     gainloss(contemporary::SDMLayer{Bool}, future::SDMLayer{Bool})
 
 Returns a layer with values -1, 0, and 1 corresponding to the difference in
-ranges.
+ranges. The two layers given as input must be layers with boolean values where
+`true` indicates that the pixel is in the range. The presence of `false` pixels
+is irrelevant as these are ignored internally.
 """
 function gainloss(contemporary::SDMLayer{Bool}, future::SDMLayer{Bool})
     rangemask = nodata((contemporary)|(future), false)

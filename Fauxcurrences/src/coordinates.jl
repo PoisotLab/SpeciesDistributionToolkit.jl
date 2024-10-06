9c8d90f15125edc35b6b05632988860932257399
tpoisot <timothee.poisot@umontreal.ca>
2024-10-06 18:28:08 -0400

    bug(faux): wrong type name

--- Fauxcurrences/src/coordinates.jl
+++ Fauxcurrences/src/coordinates.jl
@@ -1,5 +1,5 @@
 """
-    get_valid_coordinates(observations::R, layer::T) where {R <: AbstractOccurrencesCollection, T <: SDMLayer}
+    get_valid_coordinates(observations::R, layer::T) where {R <: AbstractOccurrenceCollection, T <: SDMLayer}
 
 Get the coordinates for a list of observations, filtering the ones that do not
 correspond to valid layer positions. Valid layer positions are defined as
@@ -8,7 +8,7 @@ falling within a valued pixel from the layer.
 function get_valid_coordinates(
     observations::R,
     layer::T,
-) where {R <: AbstractOccurrencesCollection, T <: SDMLayer}
+) where {R <: AbstractOccurrenceCollection, T <: SDMLayer}
     xy = place(observations)
     filter!(c -> !ismissing(c), xy) # Only the non-missing observations
     filter!(c -> !isnothing(layer[c...]), xy) # Only the observations in the layer

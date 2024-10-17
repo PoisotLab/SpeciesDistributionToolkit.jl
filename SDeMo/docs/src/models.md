310adac4e86b4e9f9e726a3999a9c24f872695bf
tpoisot <timothee.poisot@umontreal.ca>
2024-10-17 16:09:33 -0400

    doc(demo): clarify docstring for mv trans

--- SDeMo/docs/src/models.md
+++ SDeMo/docs/src/models.md
@@ -19,6 +19,7 @@ ZScore
 ## Transformers (multivariate)
 
 ```@docs
+PCATransform
 MultivariateTransform
 ```
 
--- SDeMo/src/transformers/multivariate.jl
+++ SDeMo/src/transformers/multivariate.jl
@@ -31,3 +31,22 @@ for tf in types_to_train
         end,
     )
 end
+
+"""
+    PCATransform
+
+The PCA transform will project the model features, which also serves as a way to decrease the dimensionality of the problem. Note that this method will only use the training instances, and unless the `absences=true` keyword is used, only the present cases. This ensure that there is no data leak (neither validation data nor the data from the raster are used).
+
+This is an alias for `MultivariateTransform{PCA}`.
+"""
+const PCATransform = MultivariateTransform{PCA}
+const PPCATransform = MultivariateTransform{PPCA}
+const WhiteningTransform = MultivariateTransform{Whitening}
+const kPCATransform = MultivariateTransform{KernelPCA}
+export PCATransform, PPCATransform, kPCATransform, WhiteningTransform
+
+@testitem "We can declare a model with a type alias" begin
+    X, y = SDeMo.__demodata()
+    model = SDM(PCATransform, NaiveBayes, X, y)
+    @test typeof(transformer(model)) === MultivariateTransform{PCA}
+end

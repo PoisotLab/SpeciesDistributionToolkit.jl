4c89b71cec9e46e16f089171508248bc7d958e74
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-11-10 13:29:04 -0500

    doc(sdt): signature

--- docs/src/howto/get-boundingbox.jl
+++ docs/src/howto/get-boundingbox.jl
@@ -12,11 +12,21 @@ boundingbox(occ)
 
 # We can also specify a padding (in degrees) that is added to the bounding box:
 
-boundingbox(occ; padding=1.0)
+SpeciesDistributionToolkit.boundingbox(occ; padding=1.0)
+
+# ::: info Why specify the module?
+# 
+# Makie and CairoMakie export a method called `boundingbox`. To remove any
+# ambiguities when the method is called at a time where any Makie backed is
+# loaded, we must specific which module we are calling it from. Note that the
+# dispatch signature of the method implemented by SpeciesDistributionToolkit is
+# not ambiguous.
+# 
+# :::
 
 # This is useful to restrict the part of a layer that is loaded:
 
-L = SDMLayer(RasterData(EarthEnv, LandCover), layer=2; boundingbox(occ; padding=0.5)...)
+L = SDMLayer(RasterData(EarthEnv, LandCover), layer=2; SpeciesDistributionToolkit.boundingbox(occ; padding=0.5)...)
 
 # fig-partialload
 heatmap(L)

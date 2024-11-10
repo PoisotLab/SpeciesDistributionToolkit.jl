54162195b565f55d4ade3ce557da4a908ab434ff
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-11-10 09:31:26 -0500

    semver(sdt): v1.1.0

---
+++ src/boundingbox.jl
@@ -0,0 +1,41 @@
+"""
+    boundingbox()
+
+Returns a tuple with coordinates left, right, bottom, top in WGS84, which can be
+used to decide which part of a raster should be loaded.
+"""
+boundingbox() = nothing
+
+"""
+    boundingbox(occ::AbstractOccurrenceCollection; padding=0.0)
+
+Returns the bounding box for a collection of occurrences, with an additional
+padding.
+"""
+function boundingbox(occ::AbstractOccurrenceCollection; padding=0.0)
+    left, right = extrema(longitudes(occ)) .+ (padding, -padding)
+    bottom, top = extrema(latitudes(occ)) .+ (padding, -padding)
+    return (; left, right, bottom, top)
+end
+
+"""
+    boundingbox(layer::SDMLayer; padding=0.0)
+
+Returns the bounding box for a layer, with an additional padding.
+"""
+function boundingbox(layer::SDMLayer; padding=0.0)
+    EL = eastings(layer)
+    NL = northings(layer)
+    prj = SimpleSDMLayers.Proj.Transformation(layer.crs, "+proj=longlat +datum=WGS84 +no_defs"; always_xy = true)
+
+    b1 = [prj(EL[1], n) for n in NL]
+    b2 = [prj(EL[end], n) for n in NL]
+    b3 = [prj(e, NL[1]) for e in EL]
+    b4 = [prj(e, NL[end]) for e in EL]
+    bands = vcat(b1, b2, b3, b4)
+
+    left, right = extrema(first.(bands)) .+ (padding, -padding)
+    bottom, top = extrema(last.(bands)) .+ (padding, -padding)
+
+    return (; left, right, bottom, top)
+end

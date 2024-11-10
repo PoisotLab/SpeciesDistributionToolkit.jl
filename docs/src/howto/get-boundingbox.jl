d54ac324b6970e11019d8d42f5beccbf43b7f0a2
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-11-10 09:42:57 -0500

    doc(sdt): boundigbox page

--- docs/src/howto/get-boundingbox.jl
+++ docs/src/howto/get-boundingbox.jl
@@ -4,62 +4,24 @@ using SpeciesDistributionToolkit
 using CairoMakie
 CairoMakie.activate!(; type = "png", px_per_unit = 2) #hide
 
-# To illustrate the process, we will load the demo dataset:
+# The `boundingbox` method accepts most types that `SpeciesDistributionToolkit`
+# knows about, and returns a tuple:
 
-t = SimpleSDMLayers.__demodata()
-f = tempname() * ".tiff"
-SimpleSDMLayers.save(f, t)
+occ = occurrences("hasCoordinate" => true, "country" => "BR")
+boundingbox(occ)
 
-# We can check the look of this layer as a reference:
+# We can also specify a padding (in degrees) that is added to the bounding box:
 
-# fig-demo-data
-heatmap(t; colormap = :navia)
+boundingbox(occ; padding=1.0)
+
+# This is useful to restrict the part of a layer that is loaded:
+
+L = SDMLayer(RasterData(EarthEnv, LandCover), layer=2; boundingbox(occ; padding=0.5)...)
+
+# fig-partialload
+heatmap(L)
+scatter!(occ)
 current_figure() #hide
-
-# We now define a bounding box:
-
-bbox = (left = -76.0, right = -73.0, bottom = 48.0, top = 50.0)
-
-# ::: warning WGS84 is used for the bounding box
-# 
-# We are assuming that there is not information about the CRS of the layer we
-# are working on until it is read, and for this reason the bounding box limits
-# are given in lat/lon.
-# 
-# :::
-
-# We can now pass all of these arguments to the `SDMLayer` function:
-
-k = SDMLayer(f; bandnumber = 1, bbox...)
-
-# Note that this layer is indeed cropped, but has retained its CRS:
-
-# fig-cropped
-heatmap(k; colormap = :navia)
-current_figure() #hide
-
-# We can also plot this layer projected under WGS84, and overlay the
-# boundingbox:
-
-poly = [
-    (bbox.left, bbox.bottom),
-    (bbox.right, bbox.bottom),
-    (bbox.right, bbox.top),
-    (bbox.left, bbox.top),
-    (bbox.left, bbox.bottom),
-]
-
-# fig-with-box
-heatmap(
-    interpolate(k; dest = "+proj=longlat +datum=WGS84 +no_defs +type=crs", newsize=(500, 500));
-    colormap = :navia,
-)
-lines!(poly; color = :black, linewidth = 2, linestyle = :dash)
-current_figure() #hide
-
-# Note that the definition of the bounding box is that *all of the content in
-# the bounding box* is included in the final layer, even if it results in some
-# values *outside* the bounding box being included as well.
 
 # ```@meta
 # CollapsedDocStrings = true

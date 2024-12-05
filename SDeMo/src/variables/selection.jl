af9d0e7351a45c6a6c0c6c151d202a2c4659dd36
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-12-05 13:57:37 -0500

    doc(demo): clarification

--- SDeMo/src/variables/selection.jl
+++ SDeMo/src/variables/selection.jl
@@ -115,7 +115,7 @@ end
 """
     backwardselection!(model, folds; verbose::Bool = false, optimality=mcc, kwargs...)
 
-Adds variables one at a time until the `optimality` measure stops increasing.
+Removes variables one at a time until the `optimality` measure stops increasing.
 
 All keyword arguments are passed to `crossvalidate` and `train!`.
 """

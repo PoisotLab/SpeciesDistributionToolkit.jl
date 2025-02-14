78ad0a302f4daa88ae6ba3aa1c1407a191d44dea
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-14 10:44:52 -0500

    feat(sdemo): options for logistic

--- SDeMo/src/classifiers/logistic.jl
+++ SDeMo/src/classifiers/logistic.jl
@@ -2,14 +2,14 @@ __sigmoid(z::Number) = 1.0 / (1.0 + exp(-z))
 __sigmoid(z::AbstractVector{<:Number}) = __sigmoid.(z)
 
 function __interactions(X)
-    XiXi = X.^2
+    XiXi = X .^ 2
     nfeat = size(X, 1)
-    intft = round(Int64, nfeat*(nfeat-1)/2)
+    intft = round(Int64, nfeat * (nfeat - 1) / 2)
     XiXj = zeros(eltype(X), intft, size(X, 2))
     cursor = 1
     for i in Base.OneTo(nfeat)
-        for j in (i+1):nfeat
-            XiXj[cursor, : ] = X[i,:] .* X[j,:]
+        for j in (i + 1):nfeat
+            XiXj[cursor, :] = X[i, :] .* X[j, :]
             cursor += 1
         end
     end
@@ -37,7 +37,7 @@ function __maketheta(X, interactions)
     nf = size(X, 1)
     θᵢ = zeros(nf)
     θᵢᵢ = zeros(nf)
-    θᵢⱼ = zeros(round(Int64, nf*(nf-1)/2))
+    θᵢⱼ = zeros(round(Int64, nf * (nf - 1) / 2))
     θ = [θ₀, θᵢ]
     if interactions == :self
         push!(θ, θᵢᵢ)
@@ -71,20 +71,21 @@ end
 
 Base.zero(::Type{Logistic}) = 0.5
 
-function SDeMo.train!(lreg::Logistic, y::Vector{Bool}, X::Matrix{T}) where {T<:Number}
-    
+function SDeMo.train!(lreg::Logistic, y::Vector{Bool}, X::Matrix{T}) where {T <: Number}
     𝐗 = SDeMo.__makex(X, lreg.interactions)
     𝐗t = transpose(𝐗)
     lreg.θ = SDeMo.__maketheta(X, lreg.interactions)
 
-    for epoch in 1:lreg.epochs
+    for epoch in 1:(lreg.epochs)
         z = 𝐗t * lreg.θ
         h = __sigmoid(z)
-        gradient = (1/length(lreg.θ)) * 𝐗 * (h - y) + (lreg.λ/length(lreg.θ)) * lreg.θ
+        gradient = (1 / length(lreg.θ)) * 𝐗 * (h - y) + (lreg.λ / length(lreg.θ)) * lreg.θ
         lreg.θ -= lreg.η * gradient
         if lreg.verbose & iszero(epoch % 100)
             h = clamp.(h, eps(), 1 - eps())
-            loss = -mean(y .* log.(h) .+ (1 .- y) .* log.(1 .- h)) + (lreg.λ/(2*length(lreg.θ))) * sum(lreg.θ[2:end].^2)
+            loss =
+                -mean(y .* log.(h) .+ (1 .- y) .* log.(1 .- h)) +
+                (lreg.λ / (2 * length(lreg.θ))) * sum(lreg.θ[2:end] .^ 2)
             println("Epoch $epoch: Loss = $loss")
         end
     end
@@ -92,11 +93,11 @@ function SDeMo.train!(lreg::Logistic, y::Vector{Bool}, X::Matrix{T}) where {T<:N
     return lreg
 end
 
-function StatsAPI.predict(lreg::Logistic, x::Vector{T}) where {T<:Number}
+function StatsAPI.predict(lreg::Logistic, x::Vector{T}) where {T <: Number}
     return predict(lreg, reshape(x, :, 1))
 end
 
-function StatsAPI.predict(lreg::Logistic, X::Matrix{T}) where {T<:Number}
+function StatsAPI.predict(lreg::Logistic, X::Matrix{T}) where {T <: Number}
     𝐗 = __makex(X, lreg.interactions)
     z = transpose(𝐗) * lreg.θ
     return __sigmoid(z)
@@ -107,24 +108,26 @@ function __extract_coefficients(sdm::SDM)
     nf = length(variables(sdm))
     Θ = sdm.classifier.θ
     Θ₀ = Θ[1]
-    Θᵢ = Θ[2:(nf+1)]
+    Θᵢ = Θ[2:(nf + 1)]
     C = [Θ₀, Θᵢ]
     if sdm.classifier.interactions == :self
-        Θᵢᵢ = Θ[(nf+2):(2nf+1)]
+        Θᵢᵢ = Θ[(nf + 2):(2nf + 1)]
         push!(C, Θᵢᵢ)
     end
     if sdm.classifier.interactions == :all
-        Θᵢᵢ = Θ[(nf+2):(2nf+1)]
-        Θᵢⱼ = Θ[(2nf+2):end]
+        Θᵢᵢ = Θ[(nf + 2):(2nf + 1)]
+        Θᵢⱼ = Θ[(2nf + 2):end]
         push!(C, Θᵢᵢ)
         push!(C, Θᵢⱼ)
     end
     return tuple(C...)
 end
 
-function __equation(sdm::SDM; digits=2)
+__subscript(i) = join(Char(0x2080 + d) for d in reverse!(digits(i)))
+
+function __equation(sdm::SDM; digits = 2)
     function __printer(n, t)
-        rn = round(n; digits=digits)
+        rn = round(n; digits = digits)
         return rn != 0 ? "$(rn) × $(t)" : ""
     end
     @assert sdm.classifier isa Logistic
@@ -133,9 +136,33 @@ function __equation(sdm::SDM; digits=2)
     # Intercept
     push!(terms, __printer(C[1], ""))
     # Coefficients
-    for (i,c) in enumerate(C[2])
-        push!(terms, __printer(c, "𝐗[$(i)]"))
+    for (i, c) in enumerate(C[2])
+        pr = __printer(c, "𝐱$(__subscript(i))")
+        if !isempty(pr)
+            push!(terms, pr)
+        end
     end
-    # TODO NOT FINISHED
+    # Squares
+    if length(C) >= 3
+        for (i, c) in enumerate(C[3])
+            pr = __printer(c, "(𝐱$(__subscript(i)))²")
+            if !isempty(pr)
+                push!(terms, pr)
+            end
+        end
+    end
+    # Non-self interactions
+    if length(C) == 4
+        cursor = 1
+        for i in Base.OneTo(length(C[2]))
+            for j in (i + 1):length(C[2])
+                pr = __printer(C[4][cursor], "(𝐱$(__subscript(i))⋅𝐱$(__subscript(j)))")
+                if !isempty(pr)
+                    push!(terms, pr)
+                end
+                cursor += 1
+            end
+        end
+    end
     return replace(join(terms, " + "), "+ -" => "- ", "×  " => "")
 end

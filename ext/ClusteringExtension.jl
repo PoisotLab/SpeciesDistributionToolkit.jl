71935f6082d5ace31e1f8aaab303bfea1753d860
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-16 19:17:55 -0500

    feat(sdt): fuzzy c means

--- ext/ClusteringExtension.jl
+++ ext/ClusteringExtension.jl
@@ -25,10 +25,10 @@ module ClusteringExtension
 
     function SDMLayer(result::Clustering.FuzzyCMeansResult, template::SDMLayer)
         n_centers = size(result.centers, 2)
-        output = [similar(template, Int64) for _ in 1:n_centers]
+        output = [similar(template, Float64) for _ in 1:n_centers]
         memberships = result.weights
-        @assert size(memberships, 2) == count(template)
-        for i in eachindex(n_centers)
+        @assert size(memberships, 1) == count(template)
+        for i in 1:n_centers
             output[i].grid[output[i].indices] .= memberships[:,i]
         end
         return output
@@ -39,14 +39,17 @@ using Revise
 using SpeciesDistributionToolkit
 using Clustering
 using Statistics
+using CairoMakie
 spatial_extent = (left = 8.412, bottom = 41.325, right = 9.662, top = 43.060)
-dataprovider = RasterData(CHELSA2, BioClim)
+dataprovider = RasterData(CHELSA1, BioClim)
 temperature = 0.1SDMLayer(dataprovider; layer = "BIO1", spatial_extent...)
 precipitation = 1.0SDMLayer(dataprovider; layer = "BIO12", spatial_extent...)
 L = [temperature, precipitation]
 M = (L .- mean.(L)) ./ std.(L)
 cl = kmeans(M, 10)
 K = SDMLayer(cl, temperature)
+fcl = fuzzy_cmeans(M, 3, 2)
+F = SDMLayer(fcl, temperature)
 =#
 
 end

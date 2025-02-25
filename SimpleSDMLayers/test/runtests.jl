e5c7fbca0a94586017f083a8f0e8345440075619
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-25 09:15:18 -0500

    test(layers): the test needs variability

--- SimpleSDMLayers/test/runtests.jl
+++ SimpleSDMLayers/test/runtests.jl
@@ -8,6 +8,9 @@ using TestItemRunner
 @testitem "We can cluster a series of layers" begin
     using Clustering
     L = [SimpleSDMLayers.__demodata(reduced=true) for _ in 1:3]
+    for l in L
+        l.grid .+= randn(size(l.grid))
+    end
     K = kmeans(L, 3)
     C = SDMLayer(K, L)
     q = clustering_quality(L, K; quality_index = :davies_bouldin)

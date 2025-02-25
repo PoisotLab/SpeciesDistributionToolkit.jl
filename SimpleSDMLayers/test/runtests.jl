173ce466599bf6ab9420d49cd11891f219f1077b
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-25 09:06:51 -0500

    test(layer): add test for clustering

--- SimpleSDMLayers/test/runtests.jl
+++ SimpleSDMLayers/test/runtests.jl
@@ -4,6 +4,26 @@ using TestItemRunner
 
 # write tests here
 
+
+@testitem "We can cluster a series of layers" begin
+    using Clustering
+    L = [SDMLayer(
+        RasterData(CHELSA1, BioClim);
+        layer = i,
+        left = -66.0,
+        right = -62.0,
+        bottom = 45.0,
+        top = 46.5,
+    ) for i in [1, 12]]
+    K = kmeans(L, 3)
+    C = SDMLayer(K, L)
+    q = clustering_quality(L, K; quality_index = :davies_bouldin)
+    @test typeof(C) <: SDMLayer
+    @test maximum(C) == 3
+    @test K isa ClusteringResult
+end
+
+
 ## NOTE add JET to the test environment, then uncomment
 # using JET
 # @testset "static analysis with JET.jl" begin

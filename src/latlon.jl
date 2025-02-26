0871b7240e9e3467eae98e6db4ccbac96a393e02
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-26 09:50:34 -0500

    chore(sdt): format

--- src/latlon.jl
+++ src/latlon.jl
@@ -3,16 +3,14 @@
 
 Returns the longitude for a single occurrence
 """
-longitudes(o::T) where {T <: AbstractOccurrence} =
-    ismissing(place(o)) ? missing : first(place(o))
+longitudes(o::T) where {T <: AbstractOccurrence} = ismissing(place(o)) ? missing : first(place(o))
 
 """
     latitudes(o::AbstractOccurrence)
 
 Returns the latitude for a single occurrence
 """
-latitudes(o::T) where {T <: AbstractOccurrence} =
-    ismissing(place(o)) ? missing : last(place(o))
+latitudes(o::T) where {T <: AbstractOccurrence} = ismissing(place(o)) ? missing : last(place(o))
 
 longitudes(o::T) where {T <: AbstractOccurrenceCollection} = longitudes.(elements(o))
 latitudes(o::T) where {T <: AbstractOccurrenceCollection} = latitudes.(elements(o))

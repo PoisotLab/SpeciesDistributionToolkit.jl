3b7671b7a8d872a308cb5ceaffefdfc74288c601
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-27 13:44:19 -0500

    bug(layers): 1.12 requires that contructors are extended explicitely

--- SimpleSDMLayers/ext/ClusteringExtension.jl
+++ SimpleSDMLayers/ext/ClusteringExtension.jl
@@ -9,7 +9,7 @@ module ClusteringExtension
         return kmeans(X, args...; kwargs...)
     end
 
-    function SDMLayer(result::Clustering.KmeansResult, template::SDMLayer)
+    function SimpleSDMLayers.SDMLayer(result::Clustering.KmeansResult, template::SDMLayer)
         C = assignments(result)
         @assert length(C) == count(template)
         return burnin(template, C)
@@ -21,13 +21,13 @@ module ClusteringExtension
         return fuzzy_cmeans(X, args...; kwargs...)
     end
 
-    function SDMLayer(result::Clustering.FuzzyCMeansResult, template::SDMLayer)
+    function SimpleSDMLayers.SDMLayer(result::Clustering.FuzzyCMeansResult, template::SDMLayer)
         @assert size(result.weights, 1) == count(template)
         return [burnin(template, result.weights[:,i]) for i in 1:size(result.centers, 2)]
     end
 
-    SDMLayer(result::Clustering.ClusteringResult, template::Vector{<:SDMLayer}) = SDMLayer(result, first(template))
-    SDMLayer(result::Clustering.FuzzyCMeansResult, template::Vector{<:SDMLayer}) = SDMLayer(result, first(template))
+    SimpleSDMLayers.SDMLayer(result::Clustering.ClusteringResult, template::Vector{<:SDMLayer}) = SDMLayer(result, first(template))
+    SimpleSDMLayers.SDMLayer(result::Clustering.FuzzyCMeansResult, template::Vector{<:SDMLayer}) = SDMLayer(result, first(template))
 
     function Clustering.clustering_quality(L::Vector{<:SDMLayer}, K::Clustering.ClusteringResult, args...; kwargs...)
         @assert SimpleSDMLayers._layers_are_compatible(L)

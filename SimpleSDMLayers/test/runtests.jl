fc8461be67b6cdf20c4e890dc8f6b9ddc6a9968c
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-27 13:28:10 -0500

    feat(layers): NeutralLandscapes

--- SimpleSDMLayers/test/runtests.jl
+++ SimpleSDMLayers/test/runtests.jl
@@ -4,22 +4,31 @@ using TestItemRunner
 
 # write tests here
 
+@testitem "We can generate neutral landscapes" begin
+    using NeutralLandscapes
+    ds = DiamondSquare()
+    @info ds
+    @info typeof(ds)
+    @info typeof(ds) <: NeutralLandscapes.NeutralLandscapeMaker
+    @test SDMLayer(DiamondSquare(), (10, 20)) isa SDMLayer
+end
+
+@testitem "We can generate neutral landscapes with a template" begin
+    using NeutralLandscapes
+    X = SimpleSDMLayers.__demodata(reduced=true)
+    Y = SDMLayer(RectangularCluster(), X)
+    @info X
+    @info Y
+end
 
 @testitem "We can cluster a series of layers" begin
     using Clustering
-    L = SDMLayer{Float32}[SimpleSDMLayers.__demodata(reduced=true) for _ in 1:3]
-    for l in L
-        l.grid .+= randn(size(l.grid))
-    end
+    using NeutralLandscapes
+    L = [SDMLayer(DiamondSquare(), (30, 50)) for _ in 1:8]
     K = kmeans(L, 3)
     C = SDMLayer(K, L)
     q = clustering_quality(L, K; quality_index = :davies_bouldin)
     @test typeof(C) <: SDMLayer
     @test maximum(C) == 3
     @test K isa ClusteringResult
-end
-
-@testitem "We can generate neutral landscapes" begin
-    using NeutralLandscapes
-    
 end

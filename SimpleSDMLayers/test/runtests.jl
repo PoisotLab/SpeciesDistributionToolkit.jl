451b610a887fb56ddf6e62e08569802bfa641cfd
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-02-27 12:39:04 -0500

    feat(layers): prepare to support NeutralLandscapes

--- SimpleSDMLayers/test/runtests.jl
+++ SimpleSDMLayers/test/runtests.jl
@@ -19,17 +19,7 @@ using TestItemRunner
     @test K isa ClusteringResult
 end
 
-
-## NOTE add JET to the test environment, then uncomment
-# using JET
-# @testset "static analysis with JET.jl" begin
-#     @test isempty(JET.get_reports(report_package(SDMLayers, target_modules=(SDMLayers,))))
-# end
-
-## NOTE add Aqua to the test environment, then uncomment
-# @testset "QA with Aqua" begin
-#     import Aqua
-#     Aqua.test_all(SDMLayers; ambiguities = false)
-#     # testing separately, cf https://github.com/JuliaTesting/Aqua.jl/issues/77
-#     Aqua.test_ambiguities(SDMLayers)
-# end
+@testitem "We can generate neutral landscapes" begin
+    using NeutralLandscapes
+    
+end

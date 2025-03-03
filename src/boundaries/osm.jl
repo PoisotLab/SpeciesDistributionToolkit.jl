9586e449d734d03f12ec7791847e73d9bc2f98a0
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-03 08:46:24 -0500

    feat: add option for OSM by ID

--- src/boundaries/osm.jl
+++ src/boundaries/osm.jl
@@ -1,3 +1,25 @@
+"""
+    openstreetmap(id::Integer)
+
+Returns a GeoJSON polygon from an OSM ID - this calls the polygon conversion
+service hosted at polygons.openstreetmap.fr.
+"""
+function openstreetmap(id::Integer)
+    resp = HTTP.get("https://polygons.openstreetmap.fr/get_geojson.py?id=$(id)")
+    return GeoJSON.read(resp.body)
+end
+
+"""
+    openstreetmap(place::String)
+
+Returns a GeoJSON polygon from a plain text query. This uses the nominatim API
+hosted by Open Street Maps to identify the OSM ID, and then calls the polygon
+generation service to return the GeoJSON file. Note that this method may return
+big polygons.
+
+In order to minimze the calls to the nominatim service, the results for a query
+are stored locally.
+"""
 function openstreetmap(place::String)
     cache_location = joinpath(SimpleSDMDatasets._LAYER_PATH, "OSM")
     if !ispath(cache_location)
@@ -8,7 +30,8 @@ function openstreetmap(place::String)
         url_place = HTTP.URIs.escapeuri(place)
         query = "https://nominatim.openstreetmap.org/search?q=$(url_place)&format=geojson"
         id = first(GeoJSON.read(Downloads.download(query)).osm_id)
-        Downloads.download("https://polygons.openstreetmap.fr/get_geojson.py?id=$(id)", cache_file)
+        osm_poly = SpeciesDistributionToolkit.openstreetmap(id)
+        GeoJSON.write(cache_file, osm_poly)
     end
     return GeoJSON.read(cache_file)
 end

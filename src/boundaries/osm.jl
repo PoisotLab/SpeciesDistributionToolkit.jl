10269701377ea33f6610e12b62d014018b6daa08
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-02 21:56:09 -0500

    bug: escape URIs params for OSM

--- src/boundaries/osm.jl
+++ src/boundaries/osm.jl
@@ -1,5 +1,5 @@
 function openstreetmap(place::String)
-    url_place = replace(place, " " => "%20")
+    url_place = HTTP.URIs.escapeuri(place)
     query = "https://nominatim.openstreetmap.org/search?q=$(url_place)&format=geojson"
     id = first(GeoJSON.read(Downloads.download(query)).osm_id)
     return GeoJSON.read(Downloads.download("https://polygons.openstreetmap.fr/get_geojson.py?id=$(id)"))

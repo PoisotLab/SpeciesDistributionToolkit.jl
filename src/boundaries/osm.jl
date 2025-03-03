dfb1dca2d6ce5cdf28e0951768e34285223844da
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-02 21:30:03 -0500

    feat: openstreetmap

--- src/boundaries/osm.jl
+++ src/boundaries/osm.jl
@@ -1,2 +1,6 @@
-# https://nominatim.org/release-docs/develop/api/Output/
-# https://polygons.openstreetmap.fr/get_geojson.py?id=1582777
+function openstreetmap(place::String)
+    url_place = replace(place, " " => "%20")
+    query = "https://nominatim.openstreetmap.org/search?q=$(url_place)&format=geojson"
+    id = first(GeoJSON.read(Downloads.download(query)).osm_id)
+    return GeoJSON.read(Downloads.download("https://polygons.openstreetmap.fr/get_geojson.py?id=$(id)"))
+end

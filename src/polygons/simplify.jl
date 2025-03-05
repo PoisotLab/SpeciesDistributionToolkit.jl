bd77a902614517b2c613599421cdca34cc2f1fa5
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-05 14:40:16 -0500

    bug: mask when simplifying polygons

--- src/polygons/simplify.jl
+++ src/polygons/simplify.jl
@@ -1,11 +1,23 @@
+function _heal_polygon!(pol)
+    f = first(pol)
+    if pol[end] != f
+        push!(pol, f)
+    end
+    return pol
+end
+
 function VisvalingamWhyatt!(points, tolerance, minpoints)
     if length(points) <= minpoints
         return points
     end
+    _heal_polygon!(points)
     p = copy(points)
     A = zeros(length(p))
-    push!(p, first(p))
-    pushfirst!(p, last(p))
+    # Make sure that we pad the array with the correct first and last values
+    f, l = first(p), last(p)
+    push!(p, f)
+    pushfirst!(p, l)
+    # We now iterate over all points, using the CORRECT index in the original set of points
     for n in axes(points, 1)
         i = n+1
         S = p[i-1][1]*p[i][2] + p[i][1]*p[i+1][2] + p[i+1][1]*p[i-1][2]-p[i-1][1]*p[i+1][2]-p[i][1]*p[i-1][2]-p[i+1][1]*p[i][2]
@@ -19,6 +31,7 @@ function VisvalingamWhyatt!(points, tolerance, minpoints)
         deleteat!(points, to_delete)
         return VisvalingamWhyatt!(points, tolerance, minpoints)
     end
+    _heal_polygon!(points)
     return points
 end
 
@@ -46,4 +59,21 @@ Non-mutating version of `simplify!`.
 function simplify(pol, args...; kwargs...)
     cpol = deepcopy(pol)
     return simplify!(cpol, args...; kwargs...)
+end
+
+@testitem "We can simplify a polygon" begin
+    POL = SpeciesDistributionToolkit.openstreetmap("Paris")
+    MPOL = SpeciesDistributionToolkit.simplify(POL)
+    @test typeof(MPOL) == typeof(POL)
+    @test length(POL[1][1]) >= length(MPOL[1][1])
+end
+
+@testitem "We can mask with a simplified polygon" begin
+    POL = SpeciesDistributionToolkit.openstreetmap("Paris")
+    L = SDMLayer(RasterData(CHELSA1, MinimumTemperature); SpeciesDistributionToolkit.boundingbox(POL; padding=1.0)...)
+    Lc = count(L)
+    SpeciesDistributionToolkit.simplify!(POL)
+    mask!(L, POL)
+    @test typeof(L) <: SDMLayer
+    @test count(L) <= Lc
 end

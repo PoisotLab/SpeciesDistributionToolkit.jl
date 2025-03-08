a438be45f857b1bf7741a8684bd6958a17e68eee
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-07 21:13:51 -0500

    doc: fix changelog path

--- docs/changelogs.jl
+++ docs/changelogs.jl
@@ -1,4 +1,5 @@
 chg_path = joinpath(dirname(dirname(@__FILE__)), "docs", "src", "reference", "changelog")
+
 if !ispath(chg_path)
     mkpath(chg_path)
 end
@@ -6,12 +7,14 @@ end
 # SDT
 cp(
     joinpath(dirname(dirname(@__FILE__)), "CHANGELOG.md"),
-    joinpath(chg_path, "SpeciesDistributionToolkit.md")
+    joinpath(chg_path, "SpeciesDistributionToolkit.md");
+    force = true
 )
 
-for pkg in ["GBIF", "SDeMo", "OccurrencesInterfaces", "SimpleSDMLayers", "SimpleSDMDatasets", "Fauxcurrences", "Phylopic"]
+for pkg in ["GBIF", "SDeMo", "OccurrencesInterface", "SimpleSDMLayers", "SimpleSDMDatasets", "Fauxcurrences", "Phylopic"]
 cp(
     joinpath(dirname(dirname(@__FILE__)), pkg, "CHANGELOG.md"),
-    joinpath(chg_path, "$(pkg).md")
+    joinpath(chg_path, "$(pkg).md");
+    force = true
 )
 end

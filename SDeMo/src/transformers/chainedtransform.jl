14deeaf006d86e80bb21c4db3fd3edf14a2ff30e
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-17 19:40:08 -0400

    feat(demo): parametric madness

--- SDeMo/src/transformers/chainedtransform.jl
+++ SDeMo/src/transformers/chainedtransform.jl
@@ -1,45 +1,44 @@
 """
-    ChainedTransform
+    ChainedTransform{T1, T2}
 
-A transformer that applies, in sequence, several other transformers. This can be
-used to, for example, do a PCA then a z-score on the projected space.
+A transformer that applies, in sequence, a pair of other transformers. This can
+be used to, for example, do a PCA then a z-score on the projected space. This is
+limited to two steps because the value of chaining more transformers is
+doubtful.
 """
-mutable struct ChainedTransform{Tuple{Vararg{Transformer}}} <: Transformer
-    transformers::Tuple{Vararg{Transformer}}
+mutable struct ChainedTransform{T1 <: Transformer, T2 <: Transformer} <: Transformer
+    trf1::T1
+    trf2::T2
 end
 
-function ChainedTransform(steps::Type{<:Transformer}...)
-    return ChainedTransform(tuple([step() for step in steps]...))
-end
-
-function Base.getindex(chain::ChainedTransform, i::Integer)
-    return chain.transformers[i]
+function ChainedTransform(::Type{T1}, ::Type{T2}) where {T1 <: Transformer, T2 <: Transformer}
+    return ChainedTransform{T1, T2}(T1(), T2())
 end
 
 function train!(chain::ChainedTransform, X; kwdef...)
-    train!(chain[1], X; kwdef...)
-    Xₜ = predict(chain[1], Xₜ; kwdef...)
-    for i in 2:length(chain.transformers)
-        train!(chain[i], Xₜ)
-        Xₜ = predict(chain[i], Xₜ; kwdef...)
-    end
+    train!(chain.trf1, X; kwdef...)
+    Xₜ = predict(chain.trf1, Xₜ; kwdef...)
+    train!(chain.trf2, Xₜ; kwdef...)
     return chain
 end
 
 function predict(chain::ChainedTransform, X::AbstractArray)
-    Xₜ = predict(chain[1], X; kwdef...)
-    for i in 2:length(chain.transformers)
-        Xₜ = predict(chain[i], Xₜ; kwdef...)
-    end
+    Xₜ = predict(chain.trf1, X; kwdef...)
+    Xₜ = predict(chain.trf2, Xₜ; kwdef...)
     return Xₜ
 end
 
 @testitem "We can train a chained transform" begin
     X, y = SDeMo.__demodata()
-    chain = ChainedTransform(RawData, ZScore)
-    S1 = SDM(ZScore, NaiveBayes, X, y)
-    S2 = SDM(chain, NaiveBayes, X, y)
+    c1 = ChainedTransform{RawData, ZScore}
+    c2 = ChainedTransform{ZScore, RawData}
+    S0 = SDM(ZScore, NaiveBayes, X, y)
+    S1 = SDM(c1, NaiveBayes, X, y)
+    S2 = SDM(c2, NaiveBayes, X, y)
+    train!(S0)
     train!(S1)
     train!(S2)
     @test all(predict(S1) .== predict(S2))
+    @test all(predict(S0) .== predict(S1))
+    @test all(predict(S0) .== predict(S2))
 end

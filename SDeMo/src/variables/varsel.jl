423cab97e059f485b0bc058b8a9f721798f7d6a4
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-18 14:00:26 -0400

    feat(demo): new varsel fwd working

--- SDeMo/src/variables/varsel.jl
+++ SDeMo/src/variables/varsel.jl
@@ -7,28 +7,36 @@ struct ForwardSelection <: VariableSelectionStrategy end
 
 function _next_round(::Type{ForwardSelection}, current, forced, possible)
     initial_set = current ∪ forced
-    return unique([[initial_set..., p] for p in possible])
+    includable = setdiff(possible, initial_set)
+    combinations = unique([[initial_set..., p] for p in includable])
+    return combinations
+end
+
+function _initial_proposal(::Type{ForwardSelection}, model)
+    return []
 end
 
 function variables!(model::SDM, ::Type{T}, pool, folds; kwargs...) where {T <: VariableSelectionStrategy}
     baseline = mcc(noskill(model))
-    possible = copy(variables(model)) # This is the initial pool we work with
-    checkpoint = copy(variables(model))
+    checkpoint = _initial_proposal(T, model) # This is the initial pool we work with
+    possible = copy(variables(model))
     while true
-        checkpoint = copy(variables(model))
         combination_todo = _next_round(T, checkpoint, pool, possible)
-        scores = zeros(length(todo))
-        for combination in combination_todo
+        scores = zeros(length(combination_todo))
+        for (i, combination) in enumerate(combination_todo)
             variables!(model, combination)
             C = crossvalidate(model, folds)
             scores[i] = mcc(C.validation)
         end
         newbest = maximum(scores)
-        @info newbest
         if newbest <= baseline
             variables!(model, checkpoint)
             break
-        end 
+        else
+            variables!(model, combination_todo[last(findmax(scores))])
+            checkpoint = copy(variables(model))
+            baseline = newbest
+        end
     end
     train!(model; kwargs...)
     return model

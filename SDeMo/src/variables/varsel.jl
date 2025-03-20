806437262f7fae3d8b6bd4b25cc65b6b01ee0d25
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-19 20:53:57 -0400

    doc(demo): VariableSelectionStrategy

--- SDeMo/src/variables/varsel.jl
+++ SDeMo/src/variables/varsel.jl
@@ -1,3 +1,13 @@
+"""
+    VariableSelectionStrategy
+
+This is an abstract type to which all variable selection types belong. The
+variable selection methods should define a method for `variables!`, whose first
+argument is a model, and the second argument is a selection strategy. The third
+and fourth positional arguments are, respectively, a list of variables to be
+included, and the folds to use for cross-validation. They can be omitted and
+would default to no default variables, and k-fold cross-validation.
+"""
 abstract type VariableSelectionStrategy end
 
 struct ForwardSelection <: VariableSelectionStrategy end
@@ -5,7 +15,7 @@ struct BackwardSelection <: VariableSelectionStrategy end
 struct AllVariables <: VariableSelectionStrategy end
 struct VarianceInflationFactor{N} <: VariableSelectionStrategy end
 
-_demean = (x) -> (x .- mean(x; dims = 2))
+_demean(x::Maxtrix{T}) where {T <: Number} = (x .- mean(x; dims = 2))
 _vif(X::Matrix{T}) where {T <: Number} = diag(inv(cor(X)))
 _vif(sdm::T) where {T <: AbstractSDM} = _vif(permutedims(_demean(features(sdm))))
 

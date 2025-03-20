589b5cbd373ed6ef1770411da365423b846f0dbe
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-03-20 14:41:50 -0400

    perf: more benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -16,3 +16,18 @@ SUITE["SimpleSDMLayers"]["nodata"] = @benchmarkable nodata!(L, $(rand(values(L))
 SUITE["SimpleSDMLayers"]["mosaic"] = @benchmarkable mosaic(sum, $M)
 SUITE["SimpleSDMLayers"]["rescale"] = @benchmarkable rescale(L, 0., 1.)
 SUITE["SimpleSDMLayers"]["rescale"] = @benchmarkable quantize(L, 10)
+
+# SDeMo benchmark
+SUITE["SDeMo"] = BenchmarkGroup()
+
+X, y = SDeMo.__demodata()
+
+SUITE["SDeMo"]["splits"]["kfold"] = @benchmarkable kfold(sdm) setup=(SDM(RawData, NaiveBayes, X, y))
+SUITE["SDeMo"]["splits"]["LOO"] = @benchmarkable leaveoneout(sdm) setup=(SDM(RawData, NaiveBayes, X, y))
+SUITE["SDeMo"]["splits"]["montecarlo"] = @benchmarkable montecarlo(sdm) setup=(SDM(RawData, NaiveBayes, X, y))
+
+SUITE["SDeMo"]["crossvalidate"]["rawdata - decisiontree"] = @benchmarkable crossvalidate(sdm, kfold(sdm)) setup=(SDM(RawData, DecisionTree, X, y))
+SUITE["SDeMo"]["crossvalidate"]["pca - decisiontree"] = @benchmarkable crossvalidate(sdm, kfold(sdm)) setup=(SDM(PCATransform, DecisionTree, X, y))
+
+SUITE["SDeMo"]["train"]["random forest"] = @benchmarkable train!(sdm) setup=(Bagging(SDM(ZScore, DecisionTree, X, y), 20))
+SUITE["SDeMo"]["train"]["rotation forest"] = @benchmarkable train!(sdm) setup=(Bagging(SDM(PCATransform, DecisionTree, X, y), 20))

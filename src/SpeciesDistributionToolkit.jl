f16833b648d0b6a20bb6208f45c61cb3da7afb1d
Michael D. Catchen <mdcatchen@gmail.com>
2025-04-06 07:50:51 -0400

    refactor(sdt): drop poly import comment until registered

--- src/SpeciesDistributionToolkit.jl
+++ src/SpeciesDistributionToolkit.jl
@@ -11,7 +11,6 @@ using Reexport
 # Expose the components
 @reexport using OccurrencesInterface
 @reexport using SimpleSDMDatasets
-# @reexport using SimpleSDMPolygons  TO BE UNCOMMENTED AFTER PKG REGISTRATION
 @reexport using GBIF
 @reexport using SimpleSDMLayers
 @reexport using Fauxcurrences

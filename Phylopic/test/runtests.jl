8b9aba292c7377d77b1637459c3bf189ddf27ffb
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-04-11 16:54:29 -0400

    test(phylopic): silhouetteplot

--- Phylopic/test/runtests.jl
+++ Phylopic/test/runtests.jl
@@ -1,3 +1,20 @@
 using TestItemRunner
 
 @run_package_tests filter=ti->!(:skipci in ti.tags) verbose=true
+
+@testitem "We can plot a single silhouette" begin
+    using CairoMakie
+    @test silhouetteplot(0, 0, PhylopicSilhouette("Ebolavirus"))
+end
+
+@testitem "We can plot a single silhouette with a custom size" begin
+    using CairoMakie
+    @test silhouetteplot(0, 0, PhylopicSilhouette("Ebolavirus"), markersize=200)
+end
+
+@testitem "We can plot multiple silhouettes" begin
+    using CairoMakie
+    sp = PhylopicSilhouette("Procyon lotor")
+    sps = [sp for _ in 1:10]
+    @test silhouetteplot(randn(10), randn(10), sps, markersize=rand(100:150, 10))
+end

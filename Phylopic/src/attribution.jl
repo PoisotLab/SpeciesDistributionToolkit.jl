5a10e9ba45ac37a56d8d9a8416c14a94dee18c59
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-04-11 14:53:10 -0400

    doc(phylopic): attribution docstring

--- Phylopic/src/attribution.jl
+++ Phylopic/src/attribution.jl
@@ -1,8 +1,3 @@
-"""
-    Phylopic.attribution(uuid::UUIDs.UUID)
-
-Generates a string for the attribution of an image, as identified by its `uuid`. This string is markdown-formatted, and will include a link to the license.
-"""
 function attribution(uuid::UUIDs.UUID)
     data = Phylopic.images_data(uuid)
     contributorname = data["attribution"]
@@ -15,5 +10,12 @@ end
 attribution(pair::Pair{String,UUIDs.UUID}; kwargs...) = attribution(pair.second; kwargs...)
 attribution(dict::Dict{String,UUIDs.UUID}; kwargs...) = attribution.(collect(dict); kwargs...)
 
+"""
+    Phylopic.attribution(ps::PhylopicSilhouette)
+
+Generates a string for the attribution of an image. This string is
+markdown-formatted, and will include a link to the license. When given a vector
+of silhouettes, a _single_ markdown block is returned.
+"""
 attribution(ps::PhylopicSilhouette; kwargs...) = attribution(ps.id; kwargs...)
 attribution(vps::Vector{PhylopicSilhouette}; kwargs...) = Markdown.parse(join(attribution.(vps; kwargs...), "   \n"))

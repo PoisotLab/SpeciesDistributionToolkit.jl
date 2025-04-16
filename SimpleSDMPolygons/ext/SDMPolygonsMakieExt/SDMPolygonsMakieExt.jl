182a4bbca28c76cfdfa448f0a1898e31f63d7f31
Michael D. Catchen <mdcatchen@gmail.com>
2025-04-16 14:49:49 -0400

    refactor(poly): using MakieCore and refactoring SSP Makie extension

--- SimpleSDMPolygons/ext/SDMPolygonsMakieExt/SDMPolygonsMakieExt.jl
+++ SimpleSDMPolygons/ext/SDMPolygonsMakieExt/SDMPolygonsMakieExt.jl
@@ -1,70 +1,36 @@
 module SDMPolygonsMakieExt
 
 @static if isdefined(Base, :get_extension)
-    using Makie, SimpleSDMPolygons
+    using MakieCore, SimpleSDMPolygons
 else
-    using ..Makie, ..SimpleSDMPolygons
+    using ..MakieCore, ..SimpleSDMPolygons
 end
 
-Makie.poly(feat::Feature) = poly(feat.geometry)
-Makie.poly(polygon::Polygon) = poly(polygon.geometry)
-Makie.poly(mp::MultiPolygon) = poly(mp.geometry)
-Makie.poly(fc::FeatureCollection; kw...) = begin
-    f = Figure(kw...)
-    ax = Axis(f[1, 1])
-    for feat in fc
-        poly!(ax, feat)
+const SSP_TYPES = Union{Polygon,MultiPolygon,Feature}
+
+function MakieCore.convert_arguments(P::MakieCore.NoConversion, t::SSP_TYPES)
+    return MakieCore.convert_arguments(P, t.geometry)
+end
+
+function MakieCore.convert_arguments(P::MakieCore.PointBased, t::SSP_TYPES)
+    return MakieCore.convert_arguments(P, t.geometry)
+end
+
+function MakieCore.lines(fc::FeatureCollection; kw...)
+    fig = lines(fc[begin])
+    for f in fc[2:end]
+        lines!(f; kw...)
     end
-    f
+    fig
 end
 
-
-Makie.poly!(feat::Feature; kw...) = poly!(feat.geometry; kw...)
-Makie.poly!(polygon::Polygon; kw...) = poly!(polygon.geometry; kw...)
-Makie.poly!(mp::MultiPolygon; kw...) = poly!(mp.geometry; kw...)
-Makie.poly!(fc::FeatureCollection; kw...) = begin
-    for feat in fc
-        poly!(feat; kw...)
+function MakieCore.poly(fc::FeatureCollection; kw...)
+    fig = poly(fc[begin])
+    for f in fc[2:end]
+        poly!(f; kw...)
     end
+    fig
 end
 
-Makie.poly!(ax, feat::Feature; kw...) = poly!(ax, feat.geometry; kw...)
-Makie.poly!(ax, polygon::Polygon; kw...) = poly!(ax, polygon.geometry; kw...)
-Makie.poly!(ax, mp::MultiPolygon; kw...) = poly!(ax, mp.geometry; kw...)
-Makie.poly!(ax, fc::FeatureCollection; kw...) = begin
-    for feat in fc
-        poly!(ax, feat; kw...)
-    end
-end
-
-Makie.lines(feat::Feature) = lines(feat.geometry)
-Makie.lines(polygon::Polygon) = lines(polygon.geometry)
-Makie.lines(mp::MultiPolygon) = lines(mp.geometry)
-Makie.lines(fc::FeatureCollection; kw...) = begin
-    f = Figure(kw...)
-    ax = Axis(f[1, 1])
-    for feat in fc
-        lines!(ax, feat)
-    end
-    f
-end
-
-Makie.lines!(feat::Feature; kw...) = lines!(feat.geometry; kw...)
-Makie.lines!(polygon::Polygon; kw...) = lines!(polygon.geometry; kw...)
-Makie.lines!(mp::MultiPolygon; kw...) = lines!(mp.geometry; kw...)
-Makie.lines!(fc::FeatureCollection; kw...) = begin
-    for feat in fc
-        lines!(feat; kw...)
-    end
-end
-
-Makie.lines!(ax, feat::Feature; kw...) = lines!(ax, feat.geometry; kw...)
-Makie.lines!(ax, polygon::Polygon; kw...) = lines!(ax, polygon.geometry; kw...)
-Makie.lines!(ax, mp::MultiPolygon; kw...) = lines!(ax, mp.geometry; kw...)
-Makie.lines!(ax, fc::FeatureCollection; kw...) = begin
-    for feat in fc
-        lines!(ax, feat; kw...)
-    end
-end
 
 end

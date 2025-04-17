6d5a3f2462839b46b9475764226b81bf6a19f042
Michael D. Catchen <mdcatchen@gmail.com>
2025-04-17 15:47:18 -0400

    docs(sdt): zonal docstring update

--- src/polygons/zonal.jl
+++ src/polygons/zonal.jl
@@ -1,5 +1,5 @@
 """
-    zone(layer::SDMLayer, polygons)
+    zone(layer::SDMLayer, polygons::Vector{T}) where {T <: Union{Polygon,MultiPolygon,Feature,FeatureCollection}}
 
 Returns a layer in which the value of each pixel is set to the index of the polygon to which it belongs. Initially valued cells that are not part of a polygon are turned off.
 """

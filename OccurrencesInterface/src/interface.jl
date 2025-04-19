900155c0f2ecf3265a6dd255d46388c8d14374b4
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-04-18 20:51:57 -0400

    Fix various occint bugs (#447)
    * doc(occint): elements must return a vector
    
    * doc(occint): CHANGELOG
    
    * semver(occint): v1.1.1
    
    * bug(occint): only return valid occurrences
    
    * test(occint): add the test for the demo data

--- OccurrencesInterface/src/interface.jl
+++ OccurrencesInterface/src/interface.jl
@@ -1,7 +1,10 @@
 """
     elements(::T) where {T<:AbstractOccurrenceCollection} 
 
-Returns the elements contained in an abstract collection of occurrences -- this must be something that can be iterated. The default value, when unimplemented, is `nothing`.
+Returns the elements contained in an abstract collection of occurrences -- this
+must be something that can be iterated. The default value, when unimplemented,
+is `nothing`. Note that when overloaded as part of your own implementation of
+the interface, this must return a `Vector`.
 """
 elements(::T) where {T<:AbstractOccurrenceCollection} = nothing
 elements(c::Occurrences) = c.records

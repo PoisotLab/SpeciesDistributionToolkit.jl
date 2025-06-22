0061edb00e4c4ae6db0ffd39f443fcac8afff963
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-06-22 11:12:11 -0400

    bug(poly): Bioregions is a superset of Ecoregions, not the same level

--- SimpleSDMPolygons/CHANGELOG.md
+++ SimpleSDMPolygons/CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## `v1.1.3`
 
+- **fixed** the difference between `Bioregions` and `Ecoregions`
 - **fixed** the name of the `Resolve` provider [#480]
 
 ## `v1.1.2`
--- SimpleSDMPolygons/src/providers/oneearth.jl
+++ SimpleSDMPolygons/src/providers/oneearth.jl
@@ -1,25 +1,25 @@
 SimpleSDMDatasets.provides(::Type{OneEarth}, ::Type{Bioregions}) = true
-SimpleSDMDatasets.downloadtype(::PolygonData{OneEarth, Bioregions}) = _Zip
-SimpleSDMDatasets.filetype(::PolygonData{OneEarth, Bioregions}) = _GeoJSON
-root(::PolygonData{OneEarth, Bioregions}) = "https://assets.takeshape.io/237b7856-239f-4c62-9f9e-0b9ce73fa864/dev/aeb6d1cd-6cc7-4849-b0fd-70eeed16a417/"
+SimpleSDMDatasets.downloadtype(::PolygonData{OneEarth,Bioregions}) = _Zip
+SimpleSDMDatasets.filetype(::PolygonData{OneEarth,Bioregions}) = _GeoJSON
+root(::PolygonData{OneEarth,Bioregions}) = "https://assets.takeshape.io/237b7856-239f-4c62-9f9e-0b9ce73fa864/dev/aeb6d1cd-6cc7-4849-b0fd-70eeed16a417/"
 
 _slug(::PolygonData{OneEarth,Bioregions}) = "one_earth-bioregions-2023.geojson.zip"
 
-function source(data::PolygonData{OneEarth, Bioregions})
+function source(data::PolygonData{OneEarth,Bioregions})
     stem = _slug(data)
     return (
-        url = root(data) * stem,
-        filename = stem,
-        outdir = destination(data)
+        url=root(data) * stem,
+        filename=stem,
+        outdir=destination(data)
     )
 end
 
-function postprocess(data::PolygonData{OneEarth, Bioregions}, res::R; kw...) where {R}
+function postprocess(data::PolygonData{OneEarth,Bioregions}, res::R; kw...) where {R}
     feats = [Feature(_polygonize(res.geometry[i]), _ONEEARTH_CODES[res.Bioregions[i]]) for i in eachindex(res.geometry)]
     return FeatureCollection(feats)
-end 
+end
 
-_fields_to_extract(::PolygonData{OneEarth,Bioregions}) =  Dict(
+_fields_to_extract(::PolygonData{OneEarth,Bioregions}) = Dict(
     :Bioregions => "Name"
 )
 
@@ -38,7 +38,7 @@ end
 @testitem "We can access OneEarth polygons by region" begin
     prov = PolygonData(OneEarth, Bioregions)
     polys = getpolygon(prov)
-    subreg = polys["Subregion" => "Southern Afrotropics"]
+    subreg = polys["Subregion"=>"Southern Afrotropics"]
     @test subreg isa FeatureCollection
 end
 

cbea8a944b4a6180fec2fb7b400029303a3cfee1
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-06-23 14:21:11 -0400

    ci: build the vignettes and how-to separately

---
+++ docs/vignettes.jl
@@ -0,0 +1,32 @@
+# Make sure we work from the version in the repo
+sdt_path = dirname(dirname(Base.current_project()))
+push!(LOAD_PATH, sdt_path)
+using SpeciesDistributionToolkit
+
+# Load the rest of the build environment
+using Literate
+using Markdown
+using InteractiveUtils
+using Dates
+using PrettyTables
+import Downloads
+
+# Additional functions to process the text when handled by Literate
+include("processing.jl")
+
+# Render the tutorials and how-to using Literate
+for folder in ["howto", "tutorials"]
+    fpath = joinpath(@__DIR__, "src", folder)
+    files_to_build = filter(endswith(".jl"), readdir(fpath; join = true))
+    for docfile in files_to_build
+        if ~isfile(replace(docfile, r".jl$" => ".md"))
+            Literate.markdown(
+                docfile, fpath;
+                flavor = Literate.DocumenterFlavor(),
+                config = Dict("credit" => false, "execute" => true),
+                preprocess = pre!,
+                postprocess = post!,
+            )
+        end
+    end
+end

22e13090bb6a404fc8a3d394e15cfcf15d4bf7fc
Michael D. Catchen <mdcatchen@gmail.com>
2025-07-24 11:34:37 -0400

    doc(poly): howto uses sdt instead of ssp

--- docs/src/howto/polygon-operations.jl
+++ docs/src/howto/polygon-operations.jl
@@ -2,7 +2,7 @@
 
 # Polygons can be manipulated with various different operations: _(1) adding_, which returns the union of polygons, _(2) subtracting_, which returns the difference of two polygons, _(3) intersecting_, which returns the regions both polygons contain, and _(4) inverting_, which takes a single polygon and returns a new polygon that encloses all the area within the original polygons bounding box that is not contained in the original. 
 
-using SimpleSDMPolygons
+using SpeciesDistributionToolkit
 using CairoMakie
 CairoMakie.activate!(; type = "png", px_per_unit = 2) #hide
 

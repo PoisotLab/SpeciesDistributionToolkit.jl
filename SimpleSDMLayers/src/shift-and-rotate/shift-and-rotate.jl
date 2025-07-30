e71f0ba122f7c36b16d2d2f2ff3c477e5ba3a6bd
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-07-29 22:33:26 -0400

    doc(layer): docstrings

--- SimpleSDMLayers/src/shift-and-rotate/shift-and-rotate.jl
+++ SimpleSDMLayers/src/shift-and-rotate/shift-and-rotate.jl
@@ -1,3 +1,8 @@
+"""
+    lonlat(L::SDMLayer)
+
+Returns a vector of longitudes and latitudes for a layer.
+"""
 function lonlat(L::SDMLayer)
     E, N, K = eastings(L), northings(L), keys(L)
     xy = [(E[k[2]], N[k[1]]) for k in K]
@@ -8,22 +13,74 @@ roll(angle) = (xy) -> _spherical_rotation(xy, angle, 1)
 pitch(angle) = (xy) -> _spherical_rotation(xy, angle, 2)
 yaw(angle) = (xy) -> _spherical_rotation(xy, angle, 3)
 
+"""
+    shiftlongitudes(angle)
+
+Shifts the longitudes of a group of points by performing a rotation alonside the
+yaw axis (going through the Nort pole). This preserves the latitudes of all
+points exactly and does not deform the shape.
+"""
 shiftlongitudes(angle) = (xy) -> _spherical_rotation(xy, angle, 3)
+
+"""
+    shiftlatitudes(angle)
+    
+Returns a function to move coordinates up or down in latitudes by a given angle
+in degree. Note that this accounts for the curvature of the Earth, and therefore
+requires three distinct operations. First, the points are moved so that their
+centroid has a longitude of 0; then, the points are shifted in latitute by
+performing a rotation along the pitch axis by the desired angle; finally, the
+centroid of the points is brought back to its original longitude. For this
+reason, and because the rotation along the pitch axis accounts for the curvature
+of the Earth, this function will deform the shape, and specifically change the
+range of longitudes covered.
+"""
 shiftlatitudes(angle) = (xy) -> _rotate_latitudes(xy, -angle)
 
+"""
+    localrotation(angle)
+    
+Returns a function to create a rotation of coordinates around their centroids.
+"""
 localrotation(angle) = (xy) -> _centroid_rotation(xy, angle)
 
-shiftandrotate(longitude, latitude, rotation) = (xy) -> xy |> shiftlongitudes(longitude) |> shiftlatitudes(latitude) |> localrotation(rotation)
+"""
+    shiftandrotate(longitude::T, latitude::T, rotation::T) where T <: Number
 
-function find_rotations(L, P; longitudes=(-10., 10.), latitudes=(-10., 10.), rotation=(-10., 10.), maxiter=10_000, steps=150)
+Returns a function to transform a vector of lon,lat points according to three
+operations done in order:
+
+    - a shift of the longitudes
+    - a shift of the latitudes (the order of these two operations is actually irrelevant)
+    - a local rotation around the centroid
+
+All the angles are given in degrees. The output of this function is a function
+that takes a vector of coordinates to transform. The transformations do account
+for the curvature of the Earth. For this reason, rotations and changes in the
+latitudes will deform the points, but shift in the longitudes will not.
+"""
+function shiftandrotate(longitude::T, latitude::T, rotation::T) where T <: Number
+    return shiftlongitudes(longitude) ∘ shiftlatitudes(latitude) ∘ localrotation(rotation)
+end
+
+"""
+    findrotation(L, P; longitudes=-10:0.1:10, latitudes=-10:0.1:10, rotations=-10:0.1:10, maxiter=10_000)
+
+Find a possible rotation for the `shiftandrotate` function, by attempting to
+move a target layer `L` until all of the shifted and rotated coordinates are
+valid coordinates in the layer `P`. The range of angles to explore is given as
+keywords, and the function accepts a `maxiter` argument after which, if no
+rotation is found, it returns `nothing`.
+
+Note that it is almost always a valid strategy to look for shifts and rotations
+on a raster at a coarser resolution.
+"""
+function findrotation(L::SDMLayer, P::SDMLayer; longitudes=-10:0.1:10, latitudes=-10:0.1:10, rotations=-10:0.1:10, maxiter=10_000)
     iter = 1
-    rangelon = LinRange(extrema(longitudes)..., steps)
-    rangelat = LinRange(extrema(latitudes)..., steps)
-    rangerot = LinRange(extrema(rotation)..., steps)
-    r = (rand(rangelon), rand(rangelat), rand(rangerot))
+    r = (rand(longitudes), rand(latitudes), rand(rotations))
     while iter < maxiter
         iter += 1
-        r = (rand(rangelon), rand(rangelat), rand(rangerot))
+        r = (rand(longitudes), rand(latitudes), rand(rotations))
         trf = shiftandrotate(r...)
         u = [P[c...] for c in trf(lonlat(L))]
         if !any(isnothing, u)

38ac8fc8cbee8915602d42ba0538a3ced89e2e5c
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-07-29 22:40:38 -0400

    feat(layers): use all values for quantile transfer

--- SimpleSDMLayers/src/quantiletransfer.jl
+++ SimpleSDMLayers/src/quantiletransfer.jl
@@ -1,13 +1,13 @@
 """
-    quantiletransfer!(target::SDMLayer{T}, reference::SDMLayer; n::Int=10) where {T <: Real}
+    quantiletransfer!(target::SDMLayer{T}, reference::SDMLayer) where {T <: Real}
 
 Replaces the values in the `target` layer so that they follow the distribution
 of the values in the `reference` layer. This works by (i) identifying the
 quantile of each cell in the target layer, then (ii) replacing this with the
 value for the same quantile in the reference layer.
 """
-function quantiletransfer!(target::SDMLayer{T}, reference::SDMLayer; n::Int=10) where {T <: Real}
-    Qt = quantize(target, round(Int, n * sqrt(count(reference))))
+function quantiletransfer!(target::SDMLayer{T}, reference::SDMLayer{T}) where {T <: Real}
+    Qt = quantize(target)
     target.grid = quantile(reference, Qt.grid)
     return target
 end
@@ -17,7 +17,7 @@ end
 
 Non-mutating version of `quantiletransfer!`
 """
-function quantiletransfer(target, reference)
+function quantiletransfer(target::SDMLayer{T}, reference::SDMLayer{T}) where {T <: Real}
     t = deepcopy(target)
     quantiletransfer!(t, reference)
     return t

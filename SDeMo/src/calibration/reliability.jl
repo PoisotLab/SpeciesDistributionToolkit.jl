3917011a7934c65d83c00288d092bb5e3ad4240e
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-11-15 14:04:21 -0500

    feat(demo): use bins for reliability

--- SDeMo/src/calibration/reliability.jl
+++ SDeMo/src/calibration/reliability.jl
@@ -3,13 +3,16 @@ function _calibration_bins(x, y, bins)
 
     X = zeros(bins - 1)
     Y = zeros(bins - 1)
+    filled = zeros(Bool, bins - 1)
 
     for i in 1:(bins-1)
         in_chunk = findall(qs[i] .<= x .< qs[i+1])
+        filled[i] = !isempty(in_chunk)
         X[i] = mean(x[in_chunk])
         Y[i] = mean(y[in_chunk])
     end
-    return X, Y
+
+    return X[findall(filled)], Y[findall(filled)]
 end
 
 """
@@ -19,15 +22,7 @@ Returns a binned reliability curve for a series of predicted quantitative scores
 and a series of truth values.
 """
 function reliability(yhat::Vector{<:Real}, y::Vector{Bool}; bins=9)
-    cutoffs = LinRange(minimum(yhat), maximum(yhat)+eps(), bins)
-    avgpred = zeros(bins-1)
-    avgactu = zeros(bins-1)
-    for i in eachindex(avgpred)
-        inbin = findall(x -> cutoffs[i] < x <= cutoffs[i+1], yhat)
-        avgpred[i] = mean(yhat[inbin])
-        avgactu[i] = mean(y[inbin])
-    end
-    return (avgpred, avgactu)
+    return _calibration_bins(yhat, y, bins)
 end
 
 """
@@ -37,8 +32,8 @@ Returns a binned reliability curve for a trained model, where the raw scores are
 transformed with a specified `link` function (which defaults to `identity`).
 Keyword arguments other than `bins` are passed to `predict`. 
 """
-function reliability(sdm::AbstractSDM; link::Function=identity, bins=9, kwargs...)
-    ŷ = link.(predict(sdm; threshold=false, kwargs...))
-    y = labels(sdm)
+function reliability(sdm::AbstractSDM; link::Function=identity, bins=9, samples=:, kwargs...)
+    ŷ = link.(predict(sdm; threshold=false, kwargs...))[samples]
+    y = labels(sdm)[samples]
     return reliability(ŷ, y; bins=bins)
 end

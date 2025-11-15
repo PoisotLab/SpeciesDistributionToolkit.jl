784e9fbe2be01713d47ad244abface921c333bf2
Timothée Poisot <timothee.poisot@umontreal.ca>
2025-11-15 14:04:32 -0500

    doc(demo): how-to calibration

--- docs/src/howto/sdm-calibration.jl
+++ docs/src/howto/sdm-calibration.jl
@@ -29,13 +29,14 @@ current_figure() #hide
 # fig-calibration-reliability
 f = Figure()
 ax = Axis(f[1, 1])
-scatter!(ax, reliability(sdm, bins=11))
+lines!(ax, [0, 1], [0, 1], color=:grey, linestyle=:dash)
+scatterlines!(ax, reliability(sdm, bins=15)..., color=:red)
 current_figure() #hide
 
 # We can apply different types of calibration functions, such as for example
 # isotonic regression:
 
-C = calibrate(IsotonicRegression, sdm)
+C = calibrate(IsotonicCalibration, sdm)
 
 # The calibration can be applied by passing it to the `correct` function, which
 # returns a function to correct a point prediction:
@@ -44,8 +45,42 @@ C = calibrate(IsotonicRegression, sdm)
 f = Figure()
 x = LinRange(0.0, 1.0, 50)
 ax = Axis(f[1, 1])
-scatter!(ax, reliability(sdm, bins=11))
-lines!(ax, x, correct(C).(x))
+lines!(ax, [0, 1], [0, 1], color=:grey, linestyle=:dash)
+scatterlines!(ax, reliability(sdm, bins=15)..., color=:red)
+lines!(ax, x, correct(C).(x), color=:black, linewidth=2)
 current_figure() #hide
 
-# The calibration functions can be 
+# We can check that this calibration is indeed making the model more reliable
+# compared to the initial version:
+
+# fig-calibration-check
+f = Figure()
+x = LinRange(0.0, 1.0, 50)
+ax = Axis(f[1, 1])
+lines!(ax, [0, 1], [0, 1], color=:grey, linestyle=:dash)
+scatterlines!(ax, reliability(sdm, bins=15, link=correct(C))..., color=:red)
+current_figure() #hide
+
+# It is possible to pass a `sample` keyword to `calibrate` to only use a series
+# of training points for calibration. This allows to use multiple samples to
+# estimate the calibration function:
+
+samples = first.(bootstrap(sdm))
+C = [calibrate(IsotonicCalibration, sdm; samples=s) for s in samples]
+cfunc = correct(C)
+
+# The correction function will then average the results for each calibration to
+# return the final probability:
+
+# fig-calibration-ensemble
+f = Figure()
+x = LinRange(0.0, 1.0, 50)
+ax = Axis(f[1, 1])
+lines!(ax, [0, 1], [0, 1], color=:grey, linestyle=:dash)
+scatterlines!(ax, reliability(sdm, bins=15)..., color=:red)
+lines!(ax, x, cfunc.(x), color=:black, linewidth=2)
+current_figure() #hide
+
+# The package also implements Platt's calibration with a fast algorithm, which
+# is appropriate when the relationship between scores and probabilities is
+# sigmoid.

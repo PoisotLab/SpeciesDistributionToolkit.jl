37caf80d9792b62928fdc424dda4f7943e44dfac
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-03 11:57:47 -0500

    doc: coarsen

--- docs/src/howto/usage/coarsening.jl
+++ docs/src/howto/usage/coarsening.jl
@@ -5,56 +5,58 @@ using CairoMakie
 CairoMakie.activate!(; type = "png", px_per_unit = 3) #hide
 import Statistics
 
-# Get some data
+# We will use the `coarsen` function to decrease the size of a layer while also
+# applying an operation on the cells that are merged. Note that this is an
+# operation similar to `interpolate`.
 
-region = getpolygon(PolygonData(NaturalEarth, Countries); resolution=10)["Honduras"]
-
-# boundingbox
-
+region = getpolygon(PolygonData(NaturalEarth, Countries); resolution=10)["Panama"]
 extent = SpeciesDistributionToolkit.boundingbox(region)
-
-# layers
-
 temperature = SDMLayer(RasterData(CHELSA2, AverageTemperature); extent...)
 mask!(temperature, region)
 
-# see data
+# We will look at the temperature distribution in the raw data.
 
 # fig-data-temp
 f = Figure()
-ax = Axis(f[1,1]; aspect=DataAspect())
-hm = heatmap!(ax, temperature, colormap=Reverse(:heat))
-lines!(ax, region, color=:grey10)
+ax = Axis(f[1, 1]; aspect = DataAspect())
+hm = heatmap!(ax, temperature; colormap = :magma)
+lines!(ax, region; color = :grey10)
 hidedecorations!(ax)
 hidespines!(ax)
-Colorbar(f[1,2], hm)
+Colorbar(f[1, 2], hm; label = "Average temperature in jan. (°C)")
 current_figure() #hide
 
-# coarsened version
+# To reduce the size of the layer, we must include a mask, which gives the size
+# of the cells that will be merged to create a new cell. Here, we will merge 16
+# cells together.
 
-coarse_temperature = coarsen(Statistics.mean, temperature, (3, 7))
+coarse_temperature = coarsen(Statistics.mean, temperature, (4, 4))
 
-# if you want to get rid of pixels whose center now lies outside the polygon, you can mask this layer
-
-mask!(coarse_temperature, region)
-
+# ::: warning The layer size must be compatible
 #
+# The number of cells on each size must a multiple of the coarsening window
+# size.
+#
+# :::
+
+# New pixels for which at least of the cells had a value will also have a value
+# in the coarsened layer. If this is an issue (for example, look at coastlines
+# in the next figure), you can use the `mask!` function after the coarsening.
+
+# We now look at the layer with coarser resolution:
 
 # fig-data-coarsen
 f = Figure()
 ax = Axis(f[1,1]; aspect=DataAspect())
-hm = heatmap!(ax, coarse_temperature, colormap=Reverse(:heat), colorrange=extrema(temperature))
+hm = heatmap!(ax, coarse_temperature, colormap=:magma, colorrange=extrema(temperature))
 lines!(ax, region, color=:grey10)
 hidedecorations!(ax)
 hidespines!(ax)
-Colorbar(f[1,2], hm)
+Colorbar(f[1, 2], hm; label = "Average temperature in jan. (°C)")
 current_figure() #hide
 
-
-# note that this is a long operation because it needs to be done for every pixel
-# in the layer
-
-# std_5km = slidingwindow(Statistics.std, temperature; radius=2.0)
+# This method is primarily useful when you want to test an approach without
+# using the full data resolution.
 
 # ## Related documentation
 
@@ -64,4 +66,5 @@ current_figure() #hide
 
 # ```@docs; canonical=false
 # coarsen
+# interpolate
 # ```

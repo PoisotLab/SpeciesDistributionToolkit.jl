f339320b7b1f08ac7618fbd8d7367c63af0b85a4
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-02 21:41:11 -0500

    doc: draft sliding

--- docs/src/howto/usage/sliding-windows.jl
+++ docs/src/howto/usage/sliding-windows.jl
@@ -35,7 +35,19 @@ current_figure() #hide
 # note that this is a long operation because it needs to be done for every pixel
 # in the layer
 
-# std_5km = slidingwindow(Statistics.std, temperature; radius=2.0)
+std_5km = slidingwindow(Statistics.std, temperature; radius=5.0)
+
+# fig-data-sliding
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+hm = heatmap!(ax, std_5km, colormap=Reverse(:heat))
+lines!(ax, region, color=:grey10)
+hidedecorations!(ax)
+hidespines!(ax)
+Colorbar(f[1,2], hm)
+current_figure() #hide
+
+# add info about timing and why it's long
 
 # ## Related documentation
 

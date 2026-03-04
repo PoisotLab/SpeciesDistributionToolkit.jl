d1c99ac8241578113c90a3a1cf0dc9c16394eebf
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-03 20:53:15 -0500

    doc: add docstrings for reproject

--- src/reproject.jl
+++ src/reproject.jl
@@ -1,37 +1,48 @@
+function _projector(source, target)
+    return SimpleSDMLayers.Proj.Transformation(
+        source,
+        target;
+        always_xy = true,
+    )
+end
 
+"""
+    reproject(pol::SimpleSDMPolygons.AbstractGeometry, dest)
+
+Converts the points in a polygon to a different CRS, defined by its WKT.
+"""
 function reproject(pol::SimpleSDMPolygons.AbstractGeometry, dest)
-    source_wkt = SimpleSDMPolygons.GI.crs(pol).val
-    target_wkt = dest
-    xytrans = SimpleSDMLayers.Proj.Transformation(
-        source_wkt,
-        target_wkt;
-        always_xy = true,
-    )
-
-    return SpeciesDistributionToolkit._reproject(xytrans, pol[1].geometry)
+    source = SimpleSDMPolygons.GI.crs(pol).val
+    target = dest
+    return SpeciesDistributionToolkit._reproject(_projector(source, target), pol[1].geometry)
 end
 
+"""
+    reproject(occ::AbstractOccurrenceCollection, dest)
+
+Converts the points in an abstract occurrence collection to a different CRS,
+defined by its WKT. Note that this only returns the coordinates, and not the
+rest of the occurrence information.
+"""
 function reproject(occ::AbstractOccurrenceCollection, dest)
-    source_wkt = "+proj=longlat +datum=WGS84 +no_defs"
-    target_wkt = dest
-    xytrans = SimpleSDMLayers.Proj.Transformation(
-        source_wkt,
-        target_wkt;
-        always_xy = true,
-    )
-
-    return xytrans.(place(occ))
+    source = "+proj=longlat +datum=WGS84 +no_defs"
+    target = dest
+    return _projector(source, target).(place(occ))
 end
-function reproject(sdm::AbstractSDM, dest)
-    source_wkt = "+proj=longlat +datum=WGS84 +no_defs"
-    if isgeoreferenced(sdm)
-        target_wkt = dest
-        xytrans = SimpleSDMLayers.Proj.Transformation(
-            source_wkt,
-            target_wkt;
-            always_xy = true,
-        )
 
-        return xytrans.(sdm.coordinates)
+"""
+    reproject(sdm::AbstractSDM, dest)
+
+Converts the georeferences instances in an SDM to a different CRS, defined by
+its WKT. Note that this only returns the coordinates, and not the rest of the
+model.
+"""
+function reproject(sdm::AbstractSDM, dest)
+    if isgeoreferenced(sdm)
+        source = "+proj=longlat +datum=WGS84 +no_defs"
+        target = dest
+        return _projector(source, target).(sdm.coordinates)
+    else
+        error("This model has no georeferenced instances and cannot be projected")
     end
 end

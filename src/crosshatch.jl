587b393418ee00d6bb088264e1d9e54fcb5b2567
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-05 16:11:39 -0500

    feat: crosshatch improvements

--- src/crosshatch.jl
+++ src/crosshatch.jl
@@ -7,30 +7,38 @@ function _pts_to_poly(pol, pts)
     return Polygon(cpol)
 end
 
-function crosshatch(region::SimpleSDMPolygons.AbstractGeometry; spacing = 1.0, angle = 80.0)
+function crosshatch(region::SimpleSDMPolygons.AbstractGeometry; spacing = 1.0, angle = 60.0)
     bbox = SimpleSDMPolygons.boundingbox(region)
 
     O = bbox.top - bbox.bottom
     A = O / tan(deg2rad(angle))
 
-    start_at = bbox.left - 2abs(A)
-    stop_at = bbox.right + 2abs(A)
+    start_at = bbox.left - abs(A)
+    stop_at = bbox.right + abs(A)
     xpoints = start_at:spacing:stop_at
+
+    if !iszero(length(xpoints) % 2)
+        stop_at = bbox.right + 2abs(A)
+        xpoints = start_at:spacing:stop_at
+    end
 
     pols = []
     for i in eachindex(xpoints)
-        p = [
-            (xpoints[i], bbox.bottom),
-            (xpoints[i] + A, bbox.top),
-            (xpoints[i] + A + spacing, bbox.top),
-            (xpoints[i] + spacing, bbox.bottom),
-            (xpoints[i], bbox.bottom),
-        ]
-        push!(pols, p)
+        if iszero(i % 2)
+            p = [
+                (xpoints[i], bbox.bottom),
+                (xpoints[i] + A, bbox.top),
+                (xpoints[i] + A + spacing, bbox.top),
+                (xpoints[i] + spacing, bbox.bottom),
+                (xpoints[i], bbox.bottom),
+            ]
+            push!(pols, p)
+        end
     end
 
     P = [_pts_to_poly(region, p) for p in pols]
-    hatch = FeatureCollection([Feature(p, Dict()) for p in P])
+    hatch = sum(P)
+    hatch = intersect(hatch, region)
 
-    return intersect(region, hatch)
+    return hatch
 end

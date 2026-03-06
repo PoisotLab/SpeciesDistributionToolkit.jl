c13e10d3f7c01e11bcf230d185f05c9fbeedfb3f
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-06 10:19:59 -0500

    feat(layers): also support ESRI: string

--- SimpleSDMLayers/src/projections.jl
+++ SimpleSDMLayers/src/projections.jl
@@ -2,11 +2,15 @@
     _parse_projection_from_string(p::String)
 
 This function will return the ArchGDAL SRS from a string. The three options are
-an EPSG string, a PROJ4 string, or the WKT itself.
+an EPSG (or ESRI) string, a PROJ4 string, or the WKT itself.
 """
 function _parse_projection_from_string(p::String)
     if startswith(p, "EPSG:")
         epsg_code = parse(Int, replace(p, "EPSG:" => ""))
+        return AG.importEPSG(epsg_code)
+    end
+    if startswith(p, "ESRI:")
+        epsg_code = parse(Int, replace(p, "ESRI:" => ""))
         return AG.importEPSG(epsg_code)
     end
     if startswith(p, "+proj")
@@ -16,15 +20,15 @@ function _parse_projection_from_string(p::String)
 end
 
 @testitem "We can get the SRS from a string" begin
-    from_epsg = _parse_projection_from_string("EPSG:4326")
+    from_epsg = SimpleSDMLayers._parse_projection_from_string("EPSG:4326")
     from_proj =
-        _parse_projection_from_string("+proj=longlat +datum=WGS84 +no_defs +type=crs")
-    from_wkt = _parse_projection_from_string(
+        SimpleSDMLayers._parse_projection_from_string("+proj=longlat +datum=WGS84 +no_defs +type=crs")
+    from_wkt = SimpleSDMLayers._parse_projection_from_string(
         """
 GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]
 """,
     )
-    from_esri_wkt = _parse_projection_from_string(
+    from_esri_wkt = SimpleSDMLayers._parse_projection_from_string(
         """
 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]
 """,

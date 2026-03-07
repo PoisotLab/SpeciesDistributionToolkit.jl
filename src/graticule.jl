2281c476824e79c120e1bbd71e1a8e3ce0ae13ec
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-07 12:25:56 -0500

    feat(sdt)?: graticule

--- src/graticule.jl
+++ src/graticule.jl
@@ -1,9 +1,10 @@
+using Revise
 using SpeciesDistributionToolkit
 const SDT = SpeciesDistributionToolkit
 using CairoMakie
 
-pol = getpolygon(PolygonData(NaturalEarth, Countries))["Region" => "Europe"]["Subregion" => "Eastern Europe"]
-proj = "+proj=ortho"
+pol = getpolygon(PolygonData(NaturalEarth, Countries))["Brazil"]
+proj = "ESRI:54009"
 
 temp =
     SDMLayer(RasterData(WorldClim2, Elevation); SDT.boundingbox(pol)..., resolution = 5.0)
@@ -29,75 +30,113 @@ current_figure()
     padding
     """
     padding = 0.0
-end
 
-function _bbox_to_grid(obj; kwargs...)
-    box = SDT.boundingbox(obj; kwargs...)
-    lonticks = Makie.PlotUtils.optimize_ticks(box.left, box.right)
-    latticks = Makie.PlotUtils.optimize_ticks(box.bottom, box.top)
-    lons = [lonticks[2], lonticks[1]..., lonticks[3]]
-    lats = [latticks[2], latticks[1]..., latticks[3]]
-    pols = Feature[]
-    # Longitudes
-    for i in eachindex(lons)
-        if i > 1
-            points = (
-                (lons[i - 1], box.bottom),
-                (lons[i - 1], box.top),
-                (lons[i], box.top),
-                (lons[i], box.bottom),
-                (lons[i - 1], box.bottom),
-            )
-            p = SimpleSDMPolygons.Polygon(
-                points...,
-            )
-            push!(pols, Feature(p, Dict()))
-        end
-    end
-    # Latitudes
-    for i in eachindex(lats)
-        if i > 1
-            points = (
-                (box.left, lats[i - 1]),
-                (box.right, lats[i - 1]),
-                (box.right, lats[i]),
-                (box.left, lats[i]),
-                (box.left, lats[i - 1]),
-            )
-            p = SimpleSDMPolygons.Polygon(
-                points...,
-            )
-            push!(pols, Feature(p, Dict()))
-        end
-    end
-    # Box
-    boxpoints = (
-        (box.left, box.bottom),
-        (box.right, box.bottom),
-        (box.right, box.top),
-        (box.left, box.top),
-        (box.left, box.bottom),
-    )
-    outer = SimpleSDMPolygons.Polygon(
-        boxpoints...,
-    )
+    """
+    number of points in graticule
+    """
+    npoints = 20
 
-    # Return
-    return FeatureCollection(pols), FeatureCollection(Feature(outer, Dict()))
+    """
+    style for the grid line
+    """
+    gridlinestyle = :solid
+
+    """
+    grid line width
+    """
+    gridlinewidth = 1.0
+
+    """
+    box line width
+    """
+    boxlinewidth = 1.2
 end
 
 Makie.convert_arguments(::Type{Graticule}, layer::SDMLayer) = (layer,)
 
 function Makie.plot!(graticule::Graticule)
-    grid, box = _bbox_to_grid(graticule.layer[]; padding = graticule.padding[])
-    grid = reproject(grid, graticule.layer[].crs)
-    box = reproject(box, graticule.layer[].crs)
-    lines!(graticule, grid; color = graticule.gridcolor[])
-    lines!(graticule, box; color = graticule.boxcolor[])
+    # We start by getting the bounding box in latitude/longitude
+    box = SpeciesDistributionToolkit.boundingbox(
+        graticule.layer[];
+        padding = graticule.padding[],
+    )
+    ew_ticks, _, _ = Makie.PlotUtils.optimize_ticks(box.left, box.right; k_min=3, k_max=10, k_ideal=6)
+    ns_ticks, _, _ = Makie.PlotUtils.optimize_ticks(box.bottom, box.top; k_min=3, k_max=10, k_ideal=6)
+    # Projection function
+    prj = SpeciesDistributionToolkit._projector(
+        "EPSG:4326",
+        SimpleSDMLayers.AG.toPROJ4(projection(graticule.layer[])),
+    )
+    # Then we will do a line with a bunch of points for each of the ticks
+    for i in eachindex(ew_ticks)
+        points =
+            prj.([
+                (ew_ticks[i], p) for p in LinRange(box.bottom, box.top, graticule.npoints[])
+            ])
+        lines!(
+            graticule,
+            points;
+            color = graticule.gridcolor[],
+            linestyle = graticule.gridlinestyle[],
+            linewidth = graticule.gridlinewidth[],
+        )
+    end
+    for i in eachindex(ns_ticks)
+        points =
+            prj.([
+                (p, ns_ticks[i]) for p in LinRange(box.left, box.right, graticule.npoints[])
+            ])
+        lines!(
+            graticule,
+            points;
+            color = graticule.gridcolor[],
+            linestyle = graticule.gridlinestyle[],
+            linewidth = graticule.gridlinewidth[],
+        )
+    end
+    # And then we do the box
+    points =
+        prj.([(p, box.bottom) for p in LinRange(box.left, box.right, graticule.npoints[])])
+    lines!(
+        graticule,
+        points;
+        color = graticule.boxcolor[],
+        linewidth = graticule.boxlinewidth[],
+    )
+    points =
+        prj.([(p, box.top) for p in LinRange(box.left, box.right, graticule.npoints[])])
+    lines!(
+        graticule,
+        points;
+        color = graticule.boxcolor[],
+        linewidth = graticule.boxlinewidth[],
+    )
+    points =
+        prj.([(box.left, p) for p in LinRange(box.bottom, box.top, graticule.npoints[])])
+    lines!(
+        graticule,
+        points;
+        color = graticule.boxcolor[],
+        linewidth = graticule.boxlinewidth[],
+    )
+    points =
+        prj.([(box.right, p) for p in LinRange(box.bottom, box.top, graticule.npoints[])])
+    lines!(
+        graticule,
+        points;
+        color = graticule.boxcolor[],
+        linewidth = graticule.boxlinewidth[],
+    )
+    # Next we add the text
+
     return graticule # return type doesn't actually matter
 end
 
-graticule(itemp; padding = 2.5)
-heatmap!(itemp)
-hidespines!(temp)
+graticule(itemp; padding = 0.2, gridlinestyle = :dash, boxlinewidth=2)
+poly!(reproject(pol, proj), color=:grey80)
+heatmap!(itemp, colormap=:terrain)
+lines!(reproject(pol, proj), color=:grey10)
+graticule!(itemp; padding = 0.4, gridcolor=:transparent, boxcolor=:transparent)
+hidespines!(current_axis())
+hidedecorations!(current_axis())
 current_figure()

0076a466f9342d0faaab77812d32e70fbb73d3c3
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-06 20:26:29 -0500

    bug(polygons): fix stack overflow when plotting outside of an axis

--- SimpleSDMPolygons/ext/MakieExtension.jl
+++ SimpleSDMPolygons/ext/MakieExtension.jl
@@ -7,12 +7,7 @@ else
 end
 
 const SSP_TYPES = Union{Polygon,MultiPolygon,Feature}
-
 const GEOM_TYPES = Union{Polygon,MultiPolygon}
-
-function Makie.convert_arguments(P::Makie.NoConversion, t::SSP_TYPES)
-    return Makie.convert_arguments(P, t.geometry)
-end
 
 function Makie.convert_arguments(P::Makie.NoConversion, fc::FeatureCollection)
     return Makie.convert_arguments(P, fc.features)
@@ -22,59 +17,62 @@ function Makie.convert_arguments(P::Makie.PointBased, t::SSP_TYPES)
     return Makie.convert_arguments(P, t.geometry)
 end
 
+function Makie.convert_arguments(P::Makie.NoConversion, t::SSP_TYPES)
+    return Makie.convert_arguments(P, t.geometry)
+end
+
 _nonempty_geometry(f::Feature) = _nonempty_geometry(f.geometry)
-_nonempty_geometry(p::Polygon) = !SimpleSDMPolygons.AG.isempty(p.geometry)
-_nonempty_geometry(mp::MultiPolygon) = !SimpleSDMPolygons.AG.isempty(mp.geometry)
+_nonempty_geometry(p::GEOM_TYPES) = !SimpleSDMPolygons.AG.isempty(p.geometry)
 
 for plot_type in (:lines, :poly)
     mutating_plot = Symbol(String(plot_type) * "!")
     eval(quote
-        function Makie.$plot_type(fc::FeatureCollection; kw...)
+        function Makie.$plot_type(fc::FeatureCollection; kwargs...)
             start_at = findfirst(_nonempty_geometry, fc.features)
             if !isnothing(start_at)
-                fig = $plot_type(fc[start_at]; kw...)
-                do_next = findnext(_nonempty_geometry, fc.features, start_at)
+                fig = $plot_type(fc[start_at]; kwargs...)
+                do_next = findnext(_nonempty_geometry, fc.features, start_at + 1)
                 while !isnothing(do_next)
-                    $mutating_plot(fc[do_next]; kw...)
+                    $mutating_plot(fc[do_next]; kwargs...)
                     do_next = findnext(_nonempty_geometry, fc.features, do_next + 1)
                 end
                 return fig
             end
-            return nothing
         end
 
-        function Makie.$mutating_plot(ax, fc::FeatureCollection; kw...)
-            for f in fc
+        function Makie.$mutating_plot(ax, fc::FeatureCollection; kwargs...)
+            for f in fc.features
                 if _nonempty_geometry(f)
-                    $mutating_plot(ax, f; kw...)
+                    $mutating_plot(ax, f; kwargs...)
                 end
             end
         end
 
-        function Makie.$mutating_plot(fc::FeatureCollection; kw...)
-            for f in fc
+        function Makie.$mutating_plot(fc::FeatureCollection; kwargs...)
+            for f in fc.features
                 if _nonempty_geometry(f)
-                    $mutating_plot(f; kw...)
+                    $mutating_plot(f; kwargs...)
                 end
             end
         end
 
-        function Makie.$plot_type(g::GEOM_TYPES; kw...)
+        function Makie.$plot_type(g::GEOM_TYPES; kwargs...)
+            if !_nonempty_geometry(g)
+                return nothing
+            end
+            fig = $plot_type(g.geometry; kwargs...)
+            return fig
+        end
+
+        function Makie.$mutating_plot(ax, g::GEOM_TYPES; kwargs...)
             if _nonempty_geometry(g)
-                fig = $plot_type(g; kw...)
-                return fig
+                $mutating_plot(ax, g; kwargs...)
             end
         end
 
-        function Makie.$mutating_plot(ax, g::GEOM_TYPES; kw...)
+        function Makie.$mutating_plot(g::GEOM_TYPES; kwargs...)
             if _nonempty_geometry(g)
-                $mutating_plot(ax, g; kw...)
-            end
-        end
-
-        function Makie.$mutating_plot(g::GEOM_TYPES; kw...)
-            if _nonempty_geometry(g)
-                $mutating_plot(g; kw...)
+                $mutating_plot(g; kwargs...)
             end
         end
 
--- SimpleSDMPolygons/test/runtests.jl
+++ SimpleSDMPolygons/test/runtests.jl
@@ -1,19 +1,19 @@
 using TestItemRunner
 
-@run_package_tests filter=ti->!(:skipci in ti.tags)
+@run_package_tests filter = ti -> !(:skipci in ti.tags)
 
 # THESE TESTS ARE MEANT TO BE RUN MANUALLY - LOOK AT THE OUTPUT
 
 @testitem "We can do a plot and the arguments are respected" begin
     using CairoMakie
     pd = PolygonData(NaturalEarth, Countries)
-    d = getpolygon(pd)["Subregion" => "Northern America"]
+    d = getpolygon(pd)["Subregion"=>"Northern America"]
     poly(d, color=:teal)
     lines!(d, color=:pink, linewidth=1)
     current_figure()
 
     # test obviously true statement to count it towards completing in test summary
-    @test 1 == 1 
+    @test 1 == 1
 end
 
 @testitem "The EPA polygons are the right side up" begin
@@ -29,12 +29,30 @@ end
 @testitem "We can plot even when there is an empty polygon" begin
     using CairoMakie
     pd = PolygonData(NaturalEarth, Countries)
-    pol = getpolygon(pd)["Region" => "Africa"]
+    pol = getpolygon(pd)["Region"=>"Africa"]
     c1 = pol["Ghana"]
     c2 = pol["Kenya"]
     c3 = intersect(c1, c2)
-    @test SimpleSDMPolygons.AG.isempty(c3)
+    @test SimpleSDMPolygons.AG.isempty(c3.geometry)
     poly(c3, color=:grey80, strokecolor=:black, strokewidth=1)
+
     # test obviously true statement to count it towards completing in test summary
-    @test 1 == 1 
+    @test 1 == 1
+end
+
+
+@testitem "We can plot when creating a figure" begin
+    using CairoMakie
+    pd = PolygonData(NaturalEarth, Countries)
+    pol = getpolygon(pd)["Region"=>"Africa"]
+    c1 = pol["Ghana"]
+    c2 = pol["Kenya"]
+    f = Figure()
+    ax = Axis(f[1,1], aspect=DataAspect())
+    poly!(ax, c1 + c2)
+    lines!(ax, c1)
+    lines!(ax, c2)
+    
+    # test obviously true statement to count it towards completing in test summary
+    @test 1 == 1
 end

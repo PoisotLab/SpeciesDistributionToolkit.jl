8e6391fe8502f933ce1497d84a2027cd8a507fb7
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-14 15:07:59 -0400

    feat: hexagons (very unsatisfying version)

--- src/polygons/hexagons.jl
+++ src/polygons/hexagons.jl
@@ -1,12 +1,12 @@
 function _hexagon(p, s = 1.0)
     pts = [
-        (p[1] + s * sin(k * π / 3), p[2] + s * cos(k * π / 3)) for k in 1:6
+        (p[1] + s * sin(k * π / 3 + π / 6), p[2] + s * cos(k * π / 3 + π / 6)) for k in 1:6
     ]
     push!(pts, first(pts))
     return pts
 end
 
-function hexagons(layer::SDMLayer, d::Float64; padding = 0.5)
+function hexagons(layer::SDMLayer, d::Float64; padding = 1.0)
     bb = boundingbox(layer; padding = padding)
 
     # We estimate the span at the middle of the latitude
@@ -23,43 +23,63 @@ function hexagons(layer::SDMLayer, d::Float64; padding = 0.5)
         (middle_longitude, bb.top),
     )
 
-    # How many hexagons should this be?
-    w, h = ceil(Int, W / d), ceil(Int, H / d)
+    # We want d to be the circumradius of the hexagon
+    R = d
+    r = (sqrt(3.0) / 2) * R
+
+    # We want our hexagons to be flat side up, so we need to first divide the
+    # layer height by the indiameter
+    h = ceil(Int, H / 2r)
+
+    # Next we do the same thing for the width of the layer with the outdiameter
+    w = ceil(Int, W / 2R)
 
     # Now we generate the grid of hexagons
-    p1 = [(3 * j, i * sqrt(3)) for i in 1:w, j in 1:h]
-    p2 = [(3 * j + 3 / 2, (i + 1 / 2) * sqrt(3)) for i in 1:w, j in 1:h]
-    grid = vcat(vec(p1), vec(p2))
+    grid = Tuple{Float64, Float64}[]
 
-    # Corrected to the unit interval
-    dx, dy = extrema(first.(grid)), extrema(last.(grid))
-    for i in eachindex(grid)
-        g = grid[i]
-        grid[i] = ((g[1] - dx[1]) / (dx[2] - dx[1]), (g[2] - dy[1]) / (dy[2] - dy[1]))
+    for y in 1:ceil(Int, h)
+        for x in 1:2ceil(Int, w)+1
+            # Given a pair of integers x, y, we generate two points: (3y, x√3)
+            # and (3y+3/2, (x+1/2)√3)
+            p1 = (3 * y, x * sqrt(3))
+            p2 = (3 * y + 3 / 2, (x + 1 / 2) * sqrt(3))
+
+            # We now need to re-calculate everything in order to bring the
+            # centers to the correct layer space. By convention, we will ensure
+            # that the width of the space covered by the hexagons goes from 0 to
+            # 1.
+
+            # These is the correct way to range the values in the width unit space
+            m, M = 3, 3h + 3/2            
+            px = [tuple([(c - m)/(M - m) for c in p]...) for p in [p1, p2]]
+
+            # To bring these to the space of the layer, we then need to rescale
+            # by the length of the layer, but we know this one in units of
+            # lat/lon
+            px = [
+                (p[1]*(bb.right-bb.left)+bb.left, p[2]*(bb.right-bb.left)+bb.bottom)
+                for p in px
+            ]
+
+            # Next we add these two points to the grid
+            append!(grid, px)
+        end
     end
 
-    # We need to resize the grid so that it covers the right bbox
-    for i in eachindex(grid)
-        g = grid[i]
-        grid[i] = (
-            (g[1] * (bb.right - bb.left) + bb.left),
-            (g[2] * (bb.top - bb.bottom) + bb.bottom),
-        )
-    end
-
-    # Now we need to get the size of the resulting hexagons
+    # Now the centers are correct, so we can work on getting the true radius - we get this from the circumradius,
     ref = rand(grid)
-    sur = filter(r -> (r[1]==ref[1]), grid)
+    sur = filter(r -> (r[2] == ref[2]), grid)
     sur = filter(r -> r != ref, sur)
-    _, ix = findmin([abs(ref[2]-s[2]) for s in sur])
+    _, ix = findmin([abs(ref[1] - s[1]) for s in sur])
 
-    @info sur[ix], ref
-    D = abs(sur[ix][2]-ref[2])
-    R = D / 2
-    
+    D = abs(sur[ix][1] - ref[1])
+
+    # This is the distance between two centers, which is equal to 3R
+    R = D / 3
+
     polys = [
-        Polygon(_hexagon(g, R)) for g in grid
+        Feature(Polygon(_hexagon(g, R)), Dict()) for g in grid
     ]
 
-    return polys
+    return FeatureCollection(polys)
 end
--- test.jl
+++ test.jl
@@ -4,15 +4,15 @@ using SpeciesDistributionToolkit
 const SDT = SpeciesDistributionToolkit
 
 # Get data
-pol = getpolygon(PolygonData(NaturalEarth, Countries))["Uruguay"]
+pol = getpolygon(PolygonData(NaturalEarth, Countries))["Kenya"]
 bb = SDT.boundingbox(pol)
 
 layer = SDMLayer(RasterData(CHELSA2, AverageTemperature); bb...)
 mask!(layer, pol)
 L = interpolate(layer, dest="EPSG:5382")
 
-grid = hexagons(layer, 60.; padding=0.2)
-
-heatmap(layer)
-lines!.(grid)
+grid = hexagons(layer, 200.; padding=0.2)
+heatmap(layer, axis=(;aspect=DataAspect()), colormap=:cividis)
+lines!(intersect(grid, pol), color=:white)
+lines!(pol, color=:black)
 current_figure()

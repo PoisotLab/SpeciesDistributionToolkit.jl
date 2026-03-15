671b874d66067ac06463d57164b9d38e17849ae0
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-15 16:10:08 -0400

    test(demo): additional tests

--- SDeMo/src/interfaces/OccurrencesInterface.jl
+++ SDeMo/src/interfaces/OccurrencesInterface.jl
@@ -20,9 +20,10 @@ function OccurrencesInterface.Occurrences(model::Bagging; kwargs...)
 end
 
 @testitem "We can turn an SDM into a collection of Occurrences" begin
+    import OccurrencesInterface
     model = SDM(RawData, NaiveBayes, SDeMo.__demodata()...)
     train!(model)
-    @test Occurrences(model) isa OccurrencesInterface.Occurrences
+    @test OccurrencesInterface.Occurrences(model) isa OccurrencesInterface.Occurrences
 end
 
 function OccurrencesInterface.presences(model::AbstractSDM)
@@ -41,13 +42,31 @@ function OccurrencesInterface.elements(model::AbstractSDM)
 end
 
 @testitem "We can use the presences function on an SDM" begin
+    OccurrencesInterface
     model = SDM(RawData, NaiveBayes, SDeMo.__demodata()...)
     train!(model)
     @test length(OccurrencesInterface.presences(model)) == sum(labels(model))
 end
 
 @testitem "We can use the absences function on an SDM" begin
+    import OccurrencesInterface
     model = SDM(RawData, NaiveBayes, SDeMo.__demodata()...)
     train!(model)
     @test length(OccurrencesInterface.absences(model)) == length(labels(model)) - sum(labels(model))
+end
+
+@testitem "We can set the name of the species when creating occurrences" begin
+    import OccurrencesInterface
+    model = SDM(RawData, NaiveBayes, SDeMo.__demodata()...)
+    occ = OccurrencesInterface.Occurrences(model; name="Sitta whiteheadi")
+    @test OccurrencesInterface.entity(first(occ)) == "Sitta whiteheadi"
+end
+
+@testitem "We can get occurrences from a model without goespatil data" begin
+    import OccurrencesInterface
+    X, y, C = SDeMo.__demodata()
+    model = SDM(RawData, NaiveBayes, X, y)
+    occ = OccurrencesInterface.Occurrences(model; name="Sitta whiteheadi")
+    @test OccurrencesInterface.entity(first(occ)) == "Sitta whiteheadi"
+    @test ismissing(OccurrencesInterface.place(first(occ)))
 end

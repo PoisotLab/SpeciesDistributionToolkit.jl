7ff490e4cc7a9103017af62762655576946accb9
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-18 08:50:47 -0400

    doc: test

--- test.jl
+++ test.jl
@@ -31,7 +31,7 @@ lay = [1 2; 3 4]
 
 nfolds = 5
 for t in tess
-    SDT.cvlabel!(t; n = nfolds, order = :balanced, verbose=true, maxiter=5000)
+    SDT.cvlabel!(t; n = nfolds, order = :balanced)
 end
 
 colpal = cgrad(:jet, nfolds; categorical = true)
@@ -47,4 +47,23 @@ for i in eachindex(tess)
     end
     lines!(axs[i], tess[i], color=:grey40)
 end
+current_figure()
+
+# Check byzone and mosaic
+import Statistics
+
+f = Figure()
+ax1 = Axis(f[1,1]; aspect=DataAspect())
+ax2 = Axis(f[1,2]; aspect=DataAspect())
+ax3 = Axis(f[2,1]; aspect=DataAspect())
+
+hm = heatmap!(ax1, mosaic(Statistics.mean, L[1], pointyhex, "__centroid"), colormap=:thermal, colorrange=extrema(L[1]))
+heatmap!(ax2, mosaic(Statistics.mean, L[1], pointyhex, "__fold"), colormap=:thermal, colorrange=extrema(L[1]))
+
+mean_by_tile = byzone(Statistics.mean, L[1], pointyhex, "__centroid")
+for f in pointyhex.features
+    poly!(ax3, f, color=mean_by_tile[f.properties["__centroid"]], colormap=:thermal, colorrange=extrema(L[1]))
+end
+
+Colorbar(f[2,2], hm, tellheight=false, tellwidth=false)
 current_figure()

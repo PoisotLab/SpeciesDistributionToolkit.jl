d6a218a06baa21e6511689e75b530517a46c03bf
tpoisot <timothee.poisot@umontreal.ca>
2026-03-20 12:21:14 -0400

    feat(polygon): more points when clipping by bbox

--- SimpleSDMPolygons/src/operations/clip.jl
+++ SimpleSDMPolygons/src/operations/clip.jl
@@ -1,17 +1,32 @@
 function _get_polygon_from_bbox(bbox::NamedTuple)
-    all(haskey(bbox, k) for k in [:left, :bottom, :right, :top]) || throw(ArgumentError("Bounding box tuple doesn't have correct keys. It must contain :top, :bottom, :left, and :right"))
+    all(haskey(bbox, k) for k in [:left, :bottom, :right, :top]) || throw(
+        ArgumentError(
+            "Bounding box tuple doesn't have correct keys. It must contain :top, :bottom, :left, and :right",
+        ),
+    )
 
-    bl, tl, tr, br = (bbox[:left], bbox[:bottom]),
-        (bbox[:left], bbox[:top]),
-        (bbox[:right], bbox[:top]),
-        (bbox[:right], bbox[:bottom])
-    return Polygon(bl, tl, tr, br)
-end 
+    L, B, R, T = bbox[:left], bbox[:bottom], bbox[:right], bbox[:top]
 
+    # L, B to T
+    side1 = [(L,i) for i in LinRange(B, T, 50)]
+
+    # T, L to R
+    side2 = [(i,T) for i in LinRange(L, R, 50)]
+
+    # R, T to B
+    side3 = [(R,i) for i in LinRange(T, B, 50)]
+
+    # B, R to L
+    side4 = [(i,B) for i in LinRange(R, L, 50)]
+
+    cycle = vcat(side1, side2, side3, side4)
+    
+    return Polygon(cycle...)
+end
 
 function clip(
     feat::Feature,
-    bbox::NamedTuple
+    bbox::NamedTuple,
 )
     bbox_poly = _get_polygon_from_bbox(bbox)
     return Feature(intersect(feat, bbox_poly), feat.properties)
@@ -19,15 +34,15 @@ end
 
 function clip(
     fc::FeatureCollection,
-    bbox::NamedTuple
+    bbox::NamedTuple,
 )
     bbox_poly = _get_polygon_from_bbox(bbox)
     return intersect(fc, bbox_poly)
 end
 
 function clip(
-    poly::Union{Polygon,MultiPolygon},
-    bbox::NamedTuple
+    poly::Union{Polygon, MultiPolygon},
+    bbox::NamedTuple,
 )
     bbox_poly = _get_polygon_from_bbox(bbox)
     return intersect(poly, bbox_poly)

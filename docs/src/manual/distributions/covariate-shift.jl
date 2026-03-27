556e359069a7f19b4c8de1e2571d32306ed392f2
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-26 22:52:23 -0400

    doc: some text for covariate shift vignette

--- docs/src/manual/distributions/covariate-shift.jl
+++ docs/src/manual/distributions/covariate-shift.jl
@@ -1,18 +1,38 @@
 # # Measuring and mapping covariate shift
 
+# In this vignette, we will measure the reconstruction error of a PCA, in order
+# to measure the amount of covariate shift between the training data and the
+# data used for prediction.
+
 using SpeciesDistributionToolkit
 const SDT = SpeciesDistributionToolkit
-using Statistics
-import StatsBase
-using MultivariateStats # [!code highlight]
 using CairoMakie
 
-# The support is currently limited to PCA
+# We will need to rely on the integration with `MultivariateStats` in order to
+# fit and then apply the PCA, so we will load this package as well:
 
-model = SDM(RawData, Logistic, SDeMo.__demodata()...)
+using Statistics
+using MultivariateStats # [!code highlight]
+
+# ::: info This is a temporary vignette!
+#
+# This functionality will ultimately be ported to the `SDeMo` package. It is
+# included as a reference, but the code will be updated to reflect the new
+# functions.
+#
+# :::
+
+# ## Declaring the model
+
+# We will measure the multi-variate covariate shift, which reflects how much the
+# joint distribution of variables shifts from the training data to the
+# prediction data. In order to do this, we first need a model:
+
+model = SDM(ZScore, Logistic, SDeMo.__demodata()...)
 variables!(model, ForwardSelection)
 
-# 
+# We then collect the data from the CHELSA2 BioClim on which this model was
+# trained, as a series of layers:
 
 pol = getpolygon(PolygonData(OpenStreetMap, Places); place = "Corse")
 L = SDMLayer{Float32}[
@@ -20,25 +40,43 @@ L = SDMLayer{Float32}[
     i in 1:19
 ]
 mask!(L, pol)
+
+# ::: tip Reconciling a stack of layers
+#
+# In rare cases, layers from the same providers may have minute differences in
+# their `x` and `y` coordinates after being clipped. In order to repair them, we
+# can use the `reconcile` function or its mutating variant. This should only be
+# used when the layers have non-matching coordinates.
+#
+# :::
+
 reconcile!(L)
 
-# train a PCA on the training data
+# ## PCA on the training data
+
+# Our first step is to fit a PCA on the training data. Because we are working
+# with a dataset with variables on different scales, we will transform all the
+# variables so that they have a mean of 0 and unit variance.
 
 X = features(model)[variables(model),:]
 μ = vec(mean(X, dims=2))
 σ = vec(mean(X, dims=2))
-X = (X .- μ) ./ σ
+X = (X .- μ) ./ σ;
 
-# fit the PCA for real
+# We can now fit a PCA on this matrix. The package supports [fitting directly
+# from layers as well](/manual/layers/multivariate/).
 
 M = fit(PCA, X)
 
-# Note that this will return a `PCA` result object. We can use it to project a
-# vector of layers:
+# ## Measuring the reconstruction error
+
+# We now need to transform the layers, but only after ensuring that they are
+# transformed using the same operations that gave us a training dataset with
+# mean of 0 and unit variance.
 
 K = (L[variables(model)] .- μ)./σ
 
-# we can also reconstruct
+# Our measure of covariate shit will be as follow
 
 Y = reconstruct(M,  SpeciesDistributionToolkit._X_from_layers(predict(M, K)))
 

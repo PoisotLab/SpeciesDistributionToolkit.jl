6d16f0e9f8085591f532ee85a9ef3b7ae3ba6eee
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-27 21:49:47 -0400

    doc: wombling vignette update

--- docs/src/manual/usecases/spatial-boundaries.jl
+++ docs/src/manual/usecases/spatial-boundaries.jl
@@ -1,44 +1,43 @@
 # # Integration with SpatialBoundaries
 
 # In this tutorial, we will use methods from `SpatialBoundaries`
-# [strydom2023spatialboundariesjl](@cite) to estimate boundaries for (*i.e.*
-# patches of) wooded areas on the Southwestern islands of the Hawaiian Islands
-# using landcover data from the EarthEnv project. The `SpatialBoundaries`
-# package works with `SpeciesDistributionToolkit` through an extension, so that
-# you can get access to the integration when `SpatialBoundaries` is loaded in
-# your project:
+# [strydom2023spatialboundariesjl](@cite) to estimate boundaries for the
+# presence of the trees in the landscape.
 
 using SpeciesDistributionToolkit
-using SpatialBoundaries # [!code highlight]
-using CairoMakie
 using StatsBase
 using Statistics
+using CairoMakie
+
+# The `SpatialBoundaries` package works with `SpeciesDistributionToolkit`
+# through an extension, so that you can get access to the integration when
+# `SpatialBoundaries` is loaded in your project:
+
+using SpatialBoundaries # [!code highlight]
 
 # Because there are four different layers in the EarthEnv database that
-# represent different types of woody cover, we will use the overall mean wombling
-# value.
+# represent different types of woody cover, we will use the overall mean
+# wombling across all classes of landscape that correspond to trees.
 
-# First we can start by defining the extent of the Southwestern islands of
-# Hawaii, which can be used to restrict the extraction of the various landcover
-# layers from the EarthEnv database.
+# First, we get a region of interest, as well as the relevant landcover
+# categories.
 
-hawaii = (left = -160.2, right = -154.5, bottom = 18.6, top = 22.5)
+borders = getpolygon(PolygonData(NaturalEarth, Countries))
+region = borders["Austria"]
+bbox = SpeciesDistributionToolkit.boundingbox(region; padding=0.3)
+borders = clip(borders, bbox)
 dataprovider = RasterData(EarthEnv, LandCover)
 landcover_classes = layers(dataprovider)
+
+# We need to figure out which of the layers are relevant, which is simply "the
+# layers with trees in their name":
+
+classes_with_trees = findall(contains.(landcover_classes, "Trees"))
 landcover = [
-    SDMLayer(dataprovider; layer = class, full = true, hawaii...) for
-    class in landcover_classes
+    SDMLayer(dataprovider; layer = class, full = true, bbox...)
+    for class in classes_with_trees
 ]
-
-# We can remove all the areas that contain 100% water from the landcover data as
-# our question of interest is restricted to the terrestrial realm. We do this by
-# using the "Open Water" layer to mask over each of the landcover layers
-# individually:
-
-ow_index = findfirst(isequal("Open Water"), landcover_classes)
-not_water = landcover[ow_index] .!== 0x64
-nodata!(not_water, false)
-[mask!(layer, not_water) for layer in landcover]
+mask!(landcover, region)
 
 # As layers one through four of the EarthEnv data are concerned with data on
 # woody cover (*i.e.* "Evergreen/Deciduous Needleleaf Trees", "Evergreen
@@ -47,19 +46,35 @@ nodata!(not_water, false)
 # landcover data looks like we can sum these four layers and plot the total
 # woody cover:
 
-classes_with_trees = findall(contains.(landcover_classes, "Trees"))
-tree_lc = convert(SDMLayer{Float64}, reduce(+, landcover[classes_with_trees]))
+trees = reduce(+, landcover)
 
 # Let's check that the layer looks correct:
 
 #figure boundaries-tree
-heatmap(tree_lc; colormap = :Greens)
+f = Figure(; size=(620,400))
+ax = Axis(f[1,1], aspect=DataAspect())
+hm = heatmap!(ax, trees; colormap = :Greens)
+lines!(ax, borders, color=:black)
+Colorbar(f[1,2], hm, label="% of tree cover", height=Relative(0.7))
 current_figure() #hide
 
 # Because there are several layers, we can pass them to the wombling function
 # directly, to return the mean rate and mean angular direction:
 
-W = wombling(landcover[classes_with_trees]);
+W = wombling(landcover);
+
+# We start by looking at the rate of change - this is useful to pinpoint which
+# area are likely to be identified as zones of transition. The rate of change is
+# in units of the measurement, which in this case is percentages. Larger values
+# correspond to sharper transitions.
+
+#figure spbnd-rate
+f = Figure(; size=(620,400))
+ax = Axis(f[1,1], aspect=DataAspect())
+hm = heatmap!(ax, W.rate, colormap=:batlowW)
+lines!(ax, borders, color=:black)
+Colorbar(f[1,2], hm, label="Rate of change", height=Relative(0.7))
+current_figure() #hide
 
 # We can get a sense of which cells are candidate boundaries, by picking the top
 # 15%:
@@ -71,38 +86,31 @@ candidates = (W.rate .>= cutoff)
 # potential boundaries between low and high value area:
 
 #figure spbnd-candidate
-heatmap(candidates; colormap = [:grey85, :black])
+f = Figure(; size=(620,400))
+ax = Axis(f[1,1], aspect=DataAspect())
+heatmap!(ax, candidates; colormap = [:grey85, :black])
+lines!(ax, borders, color=:black)
 current_figure() #hide
 
-# We can also look at the (log of the) rate of change - this is useful to
-# pinpoint which area are likely to be identified as zones of transition:
+# The wombling function will also return a layer that gives the direction of
+# change. The direction is expressed as wind direction, _i.e._ the direction in
+# which the change originates.
 
-#figure spbnd-rate
-heatmap(log1p.(W.rate))
+#figure spbnd-direction
+f = Figure(; size=(620,400))
+ax = Axis(f[1,1], aspect=DataAspect())
+hm = heatmap!(ax, deg2rad.(W.direction), colormap=:vikO, colorrange=(0, 2π))
+lines!(ax, borders, color=:black)
+Colorbar(f[1,2], hm, label="Direction of change", height=Relative(0.7))
 current_figure() #hide
 
-# For this example we will plot the direction of change as radial plots to get
-# an idea of the prominent direction of change. Here we will plot *all* the
-# direction values from `direction` for which the rate of change is greater than
-# zero (so as to avoid denoting directions for a slope that does not exist):
-
-direction = mask(W.direction, nodata(W.rate, 0.0))
-
-# Because stephist() requires a vector of radians for plotting we must first
-# collect the cells and convert them from degrees to radians. Then we can start
-# by plotting the direction of change of *all* cells.
+# We can also plot the direction of change as radial plots to get an idea of the
+# prominent direction of change.
 
 #figure boundaries-polar
 f = Figure()
 ax = PolarAxis(f[1, 1]; theta_0 = -pi / 2, direction = -1)
-h = fit(Histogram, deg2rad.(values(direction)); nbins = 100)
-stairs!(
-    ax,
-    h.edges[1],
-    h.weights[[axes(h.weights, 1)..., 1]];
-    color = :purple,
-    linewidth = 2,
-)
+scatter!(ax, deg2rad.(W.direction), W.rate, alpha=0.05, color=:black)
 current_figure() #hide
 
 # ## References

2f6377bfb2e86271b2f0b11b40d8eae211fdfb82
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-03-30 17:35:09 -0400

    doc: cv comparison table

--- docs/src/manual/distributions/maxent.jl
+++ docs/src/manual/distributions/maxent.jl
@@ -2,16 +2,20 @@
 
 using SpeciesDistributionToolkit
 const SDT = SpeciesDistributionToolkit
+using PrettyTables
 using CairoMakie
 
 # In this vignette, we will use the Maxent model [phillips2006maximum](@cite).
 # Specifically, we rely on the maxnet version from
 # [phillips2017opening](@citet).
 
+# ## Observations and environmental data
+
 # We will collect observations of the Northern Cardinal (_Cardinalis
 # cardinalis_) for the country of Belize. They are available on GBIF as a
 # DarwinCore archive, which we can [download and use
-# directly](/manual/retrieval/gbif-download/).
+# directly](/manual/retrieval/gbif-download/). The usual Maxent example is the
+# three-toed sloth or the Eucalyptus, but some variety is good for the soul.
 
 records = GBIF.download("10.15468/dl.y8d8yb");
 
@@ -29,20 +33,30 @@ provider = RasterData(CHELSA2, BioClim)
 L = SDMLayer{Float32}[SDMLayer(provider; bb..., layer=i) for i in 1:19]
 mask!(L, aoi)
 
-# PseudoAbsences
+# ## Pseudo-absences
 
 presencelayer = mask(L[1], records)
 background = pseudoabsencemask(BetweenRadius, presencelayer; closer = 10.0, further = 40.0)
 absencelayer = backgroundpoints(background, 2sum(presencelayer))
 
-# model
+# ## Training and validating the model
 
 m = SDM(RawData, Maxent, L, presencelayer, absencelayer)
-variables!(m, ForwardSelection)
+
+# add a type of feature
+
+hyperparameters!(classifier(m), :product, true);
+
+# standard folds
+folds = kfold(m);
+
+# variables
+
+variables!(m, ForwardSelection, folds)
 
 # 
 
-cv = crossvalidate(m, kfold(m))
+cv = crossvalidate(m, folds);
 
 # is it good?
 
@@ -52,10 +66,35 @@ mcc(cv.validation)
 
 mcc(cv.training)
 
-# compare with logistic
+# ## Logistic as a benchmark
 
 n = SDM(RawData, Logistic, L, presencelayer, absencelayer)
 variables!(n, ForwardSelection)
+
+# logicstic cv
+
+cvl = crossvalidate(n, folds);
+
+# comparison of the two models
+
+ms = [mcc, ppv, npv, f1]
+M = permutedims([m(c) for m in ms, c in [cv.training, cv.validation, cvl.training, cvl.validation]]);
+M = hcat(["Maxent", "", "Logistic", ""], ["Train.", "Val.", "Train.", "Val."], M);
+
+# Show the table
+
+pretty_table(
+    M;
+    alignment = [:l, :l, :c, :c, :c, :c],
+    backend = :markdown,
+    column_labels = [""; "Dataset"; string.(ms)],
+    formatters = [fmt__printf("%5.3f", [3, 4, 5, 6])],
+)
+
+# Maxent is not as good as logistic regression for this model, but we will keep
+# going with it regardless for the sale of illustration.
+
+# ## Maxent results
 
 #figure Maxent map
 f = Figure()

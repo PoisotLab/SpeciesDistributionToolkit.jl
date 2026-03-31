ec11b389af8ccd4caa61d462b54f9f4288a5fcb8
tpoisot <timothee.poisot@umontreal.ca>
2026-03-31 12:31:41 -0400

    doc(demo):  docstring and tests

--- SDeMo/src/classifiers/maxent.jl
+++ SDeMo/src/classifiers/maxent.jl
@@ -1,7 +1,18 @@
 """
     Maxent
 
-Maximum entropy, using the `Maxnet.jl` package
+Maximum entropy, using the `Maxnet.jl` package.
+
+Different types of features can be included/excluded using the `hyperparamters!`
+function, with `:linear` and `:categorical` (default to `true`), as well as
+`:hinge` and `:product` (default to `false`). If all four responses are set to
+`false`, the default decision rule based on number of presences will be applied.
+
+The `:regularization` hyper-parameter is a scalar (defaults to `1.0`) for the
+inclusion of new features.
+
+The `:linkfunction` is used at prediction time, and the value of `:default` is C
+log-log link. Other values are `:logit`, `:log`, and `:identity`.
 """
 Base.@kwdef mutable struct Maxent <: Classifier
     linear::Bool = true
@@ -16,6 +27,17 @@ export Maxent
 
 hyperparameters(::Type{Maxent}) =
     (:linear, :categorical, :hinge, :product, :regularization, :linkfunction)
+
+@testitem "Maxent has hyperparameters" begin
+    @test !isnothing(hyperparameters(Maxent))
+end
+
+@testitem "We can set the categories of a Maxent classifier" begin
+    m = SDM(RawData, Maxent, SDeMo.__demodata()...)
+    hyperparameters!(classifier(m), :product, true)
+    hyperparameters!(classifier(m), :regularization, 0.9)
+    hyperparameters!(classifier(m), :linkfunction, :logit)
+end
 
 Base.zero(::Type{Maxent}) = 0.5
 
@@ -55,7 +77,7 @@ function _maxent_link_function(me::Maxent)
         :default => Maxnet.CloglogLink(),
         :logit => Maxnet.LogitLink(),
         :log => Maxnet.LogLink(),
-        :identity => Maxnet.IdentityLink
+        :identity => Maxnet.IdentityLink()
     )
     if !(me.linkfunction in keys(fncs))
         throw(ArgumentError("""The link function $(me.linkfunction) does not exist
@@ -73,4 +95,17 @@ end
 
 function StatsAPI.predict(me::Maxent, X::Matrix{T}) where {T <: Number}
     return Maxnet.predict(me.model, Tables.table(permutedims(X)); link=_maxent_link_function(me))
+end
+
+@testitem "We can train a Maxent model" begin
+    m = SDM(RawData, Maxent, SDeMo.__demodata()...)
+    train!(m)
+    @test istrained(m)
+end
+
+@testitem "We can predict with a trained Maxent model" begin
+    m = SDM(RawData, Maxent, SDeMo.__demodata()...)
+    train!(m)
+    p = predict(m)
+    @test eltype(p) == Bool
 end

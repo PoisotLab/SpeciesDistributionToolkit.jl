231faddd8bf8838a74d84d5b07de1ef29819388d
tpoisot <timothee.poisot@umontreal.ca>
2026-03-31 08:50:15 -0400

    doc: maxent vignette with addnl links

--- docs/src/manual/distributions/maxent.jl
+++ docs/src/manual/distributions/maxent.jl
@@ -5,9 +5,9 @@ const SDT = SpeciesDistributionToolkit
 using PrettyTables
 using CairoMakie
 
-# In this vignette, we will use the Maxent model [phillips2006maximum](@cite).
-# Specifically, we rely on the maxnet version from
-# [phillips2017opening](@citet).
+# In this vignette, we will use the Maxent model [phillips2006maximum](@cite) to
+# predict the distribution of a bird species. Specifically, we rely on the more
+# modern maxnet version [phillips2017opening](@cite).
 
 # ## Observations and environmental data
 
@@ -18,12 +18,12 @@ using CairoMakie
 # three-toed sloth or the Eucalyptus, but some variety is good for the soul.
 
 records = GBIF.download("10.15468/dl.y8d8yb");
+bb = SDT.boundingbox(records; padding=0.5)
 
-# We will first grab a series of polygons to highlight our study area.
+# We will first grab a series of polygons to define our study area.
 
 borders = getpolygon(PolygonData(NaturalEarth, Countries))
 aoi = borders["Belize"]
-bb = SDT.boundingbox(records; padding=0.5)
 landmass = clip(borders, bb)
 
 # We finally get the BioClim variables from CHELSA2, and mask them using the
@@ -35,53 +35,85 @@ mask!(L, aoi)
 
 # ## Pseudo-absences
 
+# We start by spatially thinning the observations by mapping them to the predictor layer:
+
 presencelayer = mask(L[1], records)
+
+# We will now define the area in which we can sample background points, by
+# allowing any pixel that is at least 10km from an observation, but not more
+# than 40km.
+
 background = pseudoabsencemask(BetweenRadius, presencelayer; closer = 10.0, further = 40.0)
+
+# We finally sample two background points for every cell with at least an
+# observation:
+
 absencelayer = backgroundpoints(background, 2sum(presencelayer))
+
+# ::: tip Pseudo-absences
+#
+# There are many additional ways to define pseudo-absences, all described in
+# [the relevant vignette](/manual/generation/pseudoabsences/).
+# 
+# :::
+
+# At this point, we have a dataset with all the relevant information, and we are
+# ready to start training the model.
 
 # ## Training and validating the model
 
+# Our model will use `Maxent` for the prediction, and keep the predictor data as
+# they are.
+
 m = SDM(RawData, Maxent, L, presencelayer, absencelayer)
 
-# add a type of feature
+# We can change the hyper-parameters of the model, by allowing product features:
 
 hyperparameters!(classifier(m), :product, true);
 
-# standard folds
+# For cross-validation, we will rely on a series of 10 folds:
+
 folds = kfold(m);
 
-# variables
+# Because we do not know whether all variables are relevant, we will select the
+# variables using cross-validation, which will also train the model.
 
 variables!(m, ForwardSelection, folds)
 
+# ::: tip Variable selection
+#
+# There are additional ways to do variable selection, and they are described in
+# the [relevant vignette](/manual/sdm/variableselection/).
+#
+# :::
+
 # 
 
-cv = crossvalidate(m, folds);
 
-# is it good?
+# ## Comparing to another model
 
-mcc(cv.validation)
-
-#-
-
-mcc(cv.training)
-
-# ## Logistic as a benchmark
+# It may be a good idea to compare the model with Maxent to another model. We
+# will use logistic regression, and select the variables using the same division
+# of the dataset.
 
 n = SDM(RawData, Logistic, L, presencelayer, absencelayer)
-variables!(n, ForwardSelection)
+variables!(n, ForwardSelection, folds)
 
-# logicstic cv
+# In order to compare the two models, we will cross-validate them (note that we
+# use a different division of the dataset here):
 
+folds = kfold(m);
+cv = crossvalidate(m, folds);
 cvl = crossvalidate(n, folds);
 
-# comparison of the two models
+# FInally, we can compare the performance of the two models, using a series of
+# relevant measures:
 
 ms = [mcc, ppv, npv, f1]
 M = permutedims([m(c) for m in ms, c in [cv.training, cv.validation, cvl.training, cvl.validation]]);
 M = hcat(["Maxent", "", "Logistic", ""], ["Train.", "Val.", "Train.", "Val."], M);
 
-# Show the table
+# These are the results:
 
 pretty_table(
     M;
@@ -91,8 +123,9 @@ pretty_table(
     formatters = [fmt__printf("%5.3f", [3, 4, 5, 6])],
 )
 
-# Maxent is not as good as logistic regression for this model, but we will keep
-# going with it regardless for the sale of illustration.
+# Maxent is not as good as logistic regression for this problem. But because
+# this is the Maxent vignette, we will nonetheless keep going with it, for the
+# sake of illustration.
 
 # ## Maxent results
 
@@ -146,3 +179,23 @@ scatter!(
     color = (:black, 0.5)
 )
 current_figure() #hide
+
+# ## Related documentation
+
+# ```@meta
+# CollapsedDocStrings = true
+# ```
+
+# ```@docs; canonical=false
+# Maxent
+# hyperparameters
+# hyperparameters!
+# crossvalidate
+# ```
+
+# ## References
+
+# ```@bibliography
+# Pages = [@__FILE__]
+# Style = :authoryear
+# ```

69bbfe3375325d6a7f76e883e67134c986e228b5
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-04-03 12:41:05 -0400

    doc(demo): featureimportance docstrings

--- SDeMo/src/explanations/featureimportance.jl
+++ SDeMo/src/explanations/featureimportance.jl
@@ -1,5 +1,12 @@
 struct PermutationImportance end
 
+"""
+    featureimportance(PermutationImportance, model, variable; kwargs...)
+
+Returns the feature importance of a `variable` for a `model` over the entire
+model training data. Note that to avoid overfitting, it is better to use a
+different testing set to evaluate the importance of features.
+"""
 function featureimportance(
     ::Type{PermutationImportance},
     model::T,
@@ -16,29 +23,61 @@ function featureimportance(
     )
 end
 
+"""
+    featureimportance(PermutationImportance, model, variable, X, y; kwargs...)
+
+Returns the feature importance of a `variable` for a `model` by predicting over
+features `X` with a ground truth vector `y` .
+
+The `samples` keyword (defaults to 50) is the number of samples to use to
+evaluate the average importance.
+
+When the `ratio` keyword is `true`, the importance is evaluated as the permuted
+performance divided by the original performance (values lower than 1 indicate
+that the variable is increasingly important). When it is `false`, the
+performance is measured as a difference (values above 0 indicate that the
+variable is increasingly important).
+
+The `optimality` keyword takes a function (defaults to `mcc`) that operates on a
+a vector of predictions and a vector of correct labels. The interpretation of
+feature importance assumes that higher values of this measure correspond to a
+better model.
+
+The other `kwargs...` are passed to `predict`. For example, `threshold=false, optimality=crossentropyloss` is the way to measure the importance of variables
+based on their effect on cross-entropy loss.
+"""
 function featureimportance(
     ::Type{PermutationImportance},
     model::T,
     variable::Integer,
     X::Matrix,
     y::Vector{Bool};
-    samples = 10,
+    samples = 50,
     ratio::Bool = true,
     optimality = mcc,
     kwargs...,
 ) where {T <: AbstractSDM}
-    O₀ = optimality(ConfusionMatrix(predict(model, X), y))
+    O₀ = optimality(predict(model, X; kwargs...), y)
     Oᵢ = zeros(Float64, samples)
     for i in Base.OneTo(samples)
         Xₚ = copy(X)
         Xₚ[variable, :] .= Random.shuffle!(Xₚ[variable, :])
         yₚ = predict(model, Xₚ; kwargs...)
-        Oᵢ[i] = optimality(ConfusionMatrix(yₚ, y))
+        Oᵢ[i] = optimality(yₚ, y)
     end
     O = sum(Oᵢ) / samples
     return ratio ? O / O₀ : O - O₀
 end
 
+"""
+    featureimportance(PartialDependence, model, variable; kwargs...)
+
+Measures the feature importance of a `variable` by measuring the distance of all
+points in the partial dependence curve to all other points in the curve, and
+then averaging it. Low values indicates a flat PDP (feature is less important).
+This method can be applied on a fully trained model as it focuses on the
+_response_, not the individual predictions.
+"""
 function featureimportance(
     ::Type{PartialDependence},
     model::T,
@@ -89,7 +128,7 @@ end
         @test featureimportance(PartialDependence, model, 2; threshold = t) < 10eps()
         @test featureimportance(PartialDependence, model, 8; threshold = t) < 10eps()
         @test featureimportance(PartialDependence, model, 1; threshold = t) > 0.0
-        @test featureimportance(PartialDependence, model, 12; threshold = t) > 10.0
+        @test featureimportance(PartialDependence, model, 12; threshold = t) > 0.0
     end
 end
 
@@ -124,6 +163,15 @@ end
     ) != 1.0
 end
 
+"""
+    featureimportance(PermutationImportance, model, variable, training, testing; kwargs...)
+
+Calculates the feature importance of a model by _re-training_ a copy of the
+model using the `training` instances, then evaluating the feature importance on
+the `testing` instances.
+
+Other `kwargs...` are passed to `featureimportance`.
+"""
 function featureimportance(
     ::Type{PermutationImportance},
     model::T,
@@ -149,7 +197,7 @@ end
     folds = holdout(model)
 
     variables!(model, [1, 12])
-    
+
     @test featureimportance(
         PermutationImportance,
         model,
@@ -168,4 +216,20 @@ end
         ratio = true,
         optimality = accuracy,
     ) != 1.0
+end
+
+@testitem "We can measure feature importance with cross-entropy loss" begin
+    model = SDM(RawData, NaiveBayes, SDeMo.__demodata()...)
+    variables!(model, [1, 12])
+    train!(model)
+
+    @test featureimportance(
+        PermutationImportance, model, 2;
+        threshold = false, optimality = crossentropyloss
+    ) ≈ 1.0
+
+    @test featureimportance(
+        PermutationImportance, model, 1;
+        threshold = false, optimality = crossentropyloss
+    ) > 1.0
 end

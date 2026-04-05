b00513a93cc45995580b053797914102889ce332
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-04-05 16:46:25 -0400

    doc: cleanup tutorial for zonal

--- docs/src/manual/polygons/zonal-statistics.jl
+++ docs/src/manual/polygons/zonal-statistics.jl
@@ -1,81 +1,87 @@
 # # Caclulating zonal statistics
 
-# In this tutorial, we will grab some bioclimatic variables for Bretagne, and
-# then identify the departments that have extreme values of this variable.
+# In this tutorial, we will calculate the average temperature across different
+# countries, and present a map that highlights which of them is the warmest.
 
 using SpeciesDistributionToolkit
+using Dates
 using Statistics
 using CairoMakie
 
-# We can get polygon data from Open Street Map:
+# We will grab some country from the NaturalEarth database:
 
-BZH = getpolygon(PolygonData(OpenStreetMap, Places); place = "Bretagne")
-departements = ["Ille-et-Vilaine", "Morbihan", "Côtes-d'Armor", "Finistère"]
-DPTs = map(dpt -> getpolygon(PolygonData(OpenStreetMap, Places); place = dpt), departements)
+borders = getpolygon(PolygonData(NaturalEarth, Countries))
+countries = FeatureCollection([borders[c] for c in ["Hungary", "Austria", "Slovenia", "Slovakia"]])
 
-# We will get the BIO1 layer from CHELSA2 (average annual temperature):
+# The variable we are interested in is the average temperature in june:
 
-spatial_extent = SpeciesDistributionToolkit.boundingbox(BZH; padding = 0.2)
-dataprovider = RasterData(CHELSA2, BioClim)
-layer = SDMLayer(dataprovider; layer = "BIO1", spatial_extent...)
+spatial_extent = SpeciesDistributionToolkit.boundingbox(countries; padding=0.2)
+dataprovider = RasterData(CHELSA2, AverageTemperature)
+temperature = SDMLayer(dataprovider; month = Dates.Month(6), spatial_extent...)
 
-# This layer is trimmed to the landmass (according to GADM):
+# We remove everything that is not part of the four countries:
 
-mask!(layer, BZH)
-layer = trim(layer)
-
-#-
+mask!(temperature, countries)
 
 #figure trimmed-layer
-fig, ax, plt = heatmap(layer; axis = (; aspect = DataAspect()), colormap = :Greys)
-hidespines!(ax)
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+lines!(ax, clip(borders, spatial_extent), color=:grey20)
+hm = heatmap!(ax, temperature, colormap=:thermal)
+tightlimits!(ax)
 hidedecorations!(ax)
-lines!.(DPTs)
+lines!(ax, countries, color=:black)
+Colorbar(f[2,1], hm, vertical=false)
 current_figure() #hide
 
-# We can start looking at how the departments map onto the landscape, using the `zone`
+# We can start looking at how the countries map onto the landscape, using the `zone`
 # function. It will return a layer where the value of each pixel is the index of
 # the polygon containing this pixel:
 
 #figure districts
-heatmap(zone(layer, DPTs); colormap = :hokusai, axis = (; aspect = DataAspect()))
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+lines!(ax, clip(borders, spatial_extent), color=:grey20)
+cmap = cgrad(Makie.wong_colors()[1:length(countries)], length(countries), categorical=true)
+heatmap!(ax, zone(temperature, countries); colormap = cmap)
+tightlimits!(ax)
+hidedecorations!(ax)
+lines!(ax, countries, color=:black)
 current_figure() #hide
 
 # Note that the pixels that are not within a polygon are turned off, which can
 # sometimes happen if the overlap between polygons is not perfect. There is a
-# variant of the `mosaic` method that uses polygon to assign the values:
+# variant of the `mosaic` method that uses polygon to assign the values. Note
+# that for a feature collection, it takes an extra argument which is the
+# property on which the features are grouped.
 
-z = mosaic(mean, layer, DPTs)
-nodata!(z, 0.0)
-
-#-
-
-#figure zone-index
-fig, ax, plt = heatmap(z; axis = (; aspect = DataAspect()))
-hidespines!(ax)
-hidedecorations!(ax)
-current_figure() #hide
+zones = mosaic(mean, temperature, countries, "Name")
+nodata!(zones, iszero)
 
 # Finally, we can get the mean value within each of these polygons using the
 # `byzone` method:
 
-depts_ranked = sort(
-    byzone(mean, layer, DPTs, departements);
+warmest = sort(
+    collect(byzone(mean, temperature, countries, "Name"));
     by = (x) -> x.second,
     rev = true,
 )
 
-# The index of the department with the highest value is
+# The country with the warmest temperature value is
 
-i = findfirst(isequal(depts_ranked[1].first), departements)
+warmest = warmest[1].first
 
-#-
+# We can finally highlight the warmest area:
 
 #figure highlight-areas
-fig, ax, plt =
-    heatmap(layer; axis = (; aspect = DataAspect()), colormap = [:grey80, :grey20])
-lines!(ax, DPTs[i]; label = departements[i], linewidth = 2, color = :red)
-axislegend(; position = (0, 0.1), nbanks = 1, framevisible = false)
-hidedecorations!(ax) #hide
-hidespines!(ax) #hide
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+lines!(ax, clip(borders, spatial_extent), color=:grey20)
+hm = heatmap!(ax, zones, colormap=:thermal, colorrange=extrema(temperature))
+tightlimits!(ax)
+hidedecorations!(ax)
+lines!(ax, countries, color=:black)
+lines!(ax, crosshatch(countries[warmest], spacing=0.4), color=:red)
+lines!(ax, countries[warmest], color=:red)
+Colorbar(f[2,1], hm, vertical=false)
 current_figure() #hide

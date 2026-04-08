befe5e0aded0950cb6a436b25f2d1187321f7ab1
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-04-08 10:10:54 -0400

    doc: OSM vignette

--- docs/src/manual/retrieval/openstreetmap.jl
+++ docs/src/manual/retrieval/openstreetmap.jl
@@ -17,14 +17,48 @@ lnd = getpolygon(PolygonData(NaturalEarth, Land))
 
 # ## Countries
 
-osmpol = getpolygon(PolygonData(OpenStreetMap, Countries); country="Laos")
+# force result to be a country + manmade layer
+
+osmpol = getpolygon(PolygonData(OpenStreetMap, Countries); country="Malaysia")
 
 #figure map-laos
 f = Figure()
 ax = Axis(f[1,1]; aspect=DataAspect())
 poly!(ax, clip(lnd, SDT.boundingbox(osmpol; padding=1.0)), color=:grey90)
 lines!(ax, osmpol, color=:red)
+tightlimits!(ax)
 current_figure()
+
+# Note that the polygons are returned with territorial waters, but [vignette
+# op](/manual/polygons/operations/)
+
+uniqueproperties(osmpol)
+
+# ## Places
+
+osmpol = getpolygon(PolygonData(OpenStreetMap, Places); place="Occitanie")
+
+#figure map-alps
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+poly!(ax, clip(lnd, SDT.boundingbox(osmpol; padding=1.0)), color=:grey90)
+lines!(ax, osmpol, color=:red)
+tightlimits!(ax)
+current_figure()
+
+
+# ## Places
+
+osmpol = getpolygon(PolygonData(OpenStreetMap, Places); place="Quebec")
+
+#figure map-qc
+f = Figure()
+ax = Axis(f[1,1]; aspect=DataAspect())
+poly!(ax, clip(lnd, SDT.boundingbox(osmpol; padding=1.0)), color=:grey90)
+lines!(ax, osmpol, color=:red)
+tightlimits!(ax)
+current_figure()
+
 
 # ## Related documentation
 

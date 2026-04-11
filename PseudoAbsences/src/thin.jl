f78b72dfb0a4ec984926eccbd203fe92ffa3de91
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-04-11 18:39:32 -0400

    perf: faster thinning

--- PseudoAbsences/src/thin.jl
+++ PseudoAbsences/src/thin.jl
@@ -1,46 +1,45 @@
+function Dᵢⱼ(records)
+    D = fill(Inf16, length(records), length(records))
+
+    for i in axes(D, 1)
+        for j in axes(D, 2)
+            if j > i
+                dᵢⱼ = PseudoAbsences._distancefunction(place(records[i]), place(records[j]))
+                D[j, i] = D[i, j] = dᵢⱼ
+            end
+        end
+    end
+
+    return D
+end
+
 """
     thin( records::T, d::Float64; nmin::Integer = 30, ) where {T <: AbstractOccurrenceCollection}
 
 Returns a spatially thinned collection of occurrences so that no points are
 closer than `d` kilometers from one another. The `nmin` keyword (default to 30)
 can also be set to ensure that there are at least that many points, regardless
-of the distance between points at the end.
+of the distance between points at the end. If there are fewer than `nmin` points
+at the specified distance, the minimum distance will be changed to allow points
+that are closer, until the `nmin` condition is met.
 """
 function thin(
     records::T,
     d::Float64;
-    nmin::Integer = 30,
-) where {T <: OccurrencesInterface.AbstractOccurrenceCollection}
-    D = fill(Inf16, length(records), length(records))
-    
-    # We fill the distances but ONLY for the pairs of points that are closer
-    # than the threshold - the rest of the points do not matter
-    for i in axes(D, 1)
-        for j in axes(D, 2)
-            if j > i
-                dᵢⱼ = PseudoAbsences._distancefunction(place(records[i]), place(records[j]))
-                if dᵢⱼ <= d
-                    D[i, j] = dᵢⱼ
-                end
-            end
-        end
+    nmin::Integer=30,
+) where {T<:OccurrencesInterface.AbstractOccurrenceCollection}
+
+    D = Dᵢⱼ(records)
+
+    too_close = D .< d
+    n_violations = vec(sum(too_close, dims=1))
+    while count(iszero, n_violations) < nmin
+        d *= 0.99
+        too_close = D .< d
+        n_violations = vec(sum(too_close, dims=1))
     end
 
-    candidates = dropdims(mapslices(x -> !all(isinf, x), D; dims = 1); dims = 1)
-    max_iter = length(candidates) - nmin
-    counter = 0
-
-    while (minimum(D) <= d) & (counter < max_iter)
-        counter += 1
-        min_idx = last(findmin(D))
-        removed = rand(min_idx.I)
-        D[removed, :] .= Inf
-        D[:, removed] .= Inf
-    end
-
-    retained = dropdims(mapslices(x -> !all(isinf, x), D; dims = 1); dims = 1)
-    kept = findall(retained .| (.! candidates))
-
+    kept = findall(iszero, n_violations)
     return records[kept]
 end
 
@@ -50,10 +49,25 @@ end
     records = Occurrences(filter(
         e ->
             (-130. <= place(e)[1] <= -110.) &
-            (30. <= place(e)[2] <= 50.)
-        , elements(records)
+            (30. <= place(e)[2] <= 50.), elements(records)
     ))
 
     thinned = thin(records, 150.)
     @test length(thinned) < length(records)
+end
+
+@testitem "We get the correct distance when thinning" begin
+    using PseudoAbsences.OccurrencesInterface
+    records = OccurrencesInterface.__demodata()
+    records = Occurrences(filter(
+        e ->
+            (-130. <= place(e)[1] <= -110.) &
+            (30. <= place(e)[2] <= 50.), elements(records)
+    ))
+
+    @test minimum(Dᵢⱼ(records)) == Float16(0.0)
+
+    thinned = thin(records, 25.)
+    
+    @test minimum(Dᵢⱼ(thinned)) >= Float16(25.)
 end

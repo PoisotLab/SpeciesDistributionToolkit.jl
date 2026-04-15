65f3b6d6d775ec1d54528d693df5674aba67db6c
Timothée Poisot <timothee.poisot@umontreal.ca>
2026-04-14 21:00:26 -0400

    doc: thinning

--- docs/src/manual/generation/thinning.jl
+++ docs/src/manual/generation/thinning.jl
@@ -5,53 +5,66 @@ using SpeciesDistributionToolkit
 const SDT = SpeciesDistributionToolkit
 using CairoMakie
 
-# get data
+# To illustrate spatial thinning, we will look at observations of the sasquatch
+# in the USA. The polygon is clipped to focus on the continental US.
 
 aoi = getpolygon(PolygonData(NaturalEarth, Countries))["United States of America"]
 aoi = clip(aoi, (left=-140., right=-50., top=50., bottom=20.))
 
-# records
+# The sightings data are available as a demonstration dataset:
 
 records = Occurrences(mask(OccurrencesInterface.__demodata(), aoi));
 
+# Some of the sightings are very clustered, both to one another and also to more
+# densely populated places.
+
 #figure initial-datapoints
-f = Figure()
+f = Figure(; size=(600, 300))
 ax = Axis(f[1,1], aspect=DataAspect())
 poly!(ax, aoi, color=:grey98)
 lines!(ax, aoi, color=:grey10)
-scatter!(ax, records, color=:darkgreen)
+scatter!(ax, records, color=:darkgreen, markersize=4)
+hidespines!(ax)
+hidedecorations!(ax)
 current_figure() #hide
 
-# get a thinned version with at least 100km between each point
+# We start by getting a thinned version where a point will have no neighbors
+# within a 60km radius:
 
-thinned = thin(records, 80.);
+thinned = thin(records, 60.);
+
+# ::: info Runtime of spatial thinning
+#
+# The algorithm we use tends to remain efficient until the distance requested is
+# much smaller than the average distance between neighboring points. In other
+# words, asking for occurrences separated by 50km will be much faster than
+# asking for occurrences separated by 1km.
+#
+# :::
+
+# The points we get after this step should respect the spatial structure of the
+# data, but be much sparser.
 
 #figure thinned-datapoints
-f = Figure()
+f = Figure(; size=(600, 300))
 ax = Axis(f[1,1], aspect=DataAspect())
 poly!(ax, aoi, color=:grey98)
 scatter!(ax, records, color=:grey80, markersize=12)
 lines!(ax, aoi, color=:grey10)
-scatter!(ax, thinned, color=:darkgreen, markersize=6)
+scatter!(ax, thinned, color=:darkgreen, markersize=4)
+hidespines!(ax)
+hidedecorations!(ax)
 current_figure() #hide
 
-# results are differentso good idea to replicate
+# Note that the algorithm works with some stochasticity, so it is a good idea to
+# either set the RNG seed before proceeding, or to run it several time to check
+# that the specific points retained do not change the results too much.
 
-#figure thinned-datapoints-replicated
-f = Figure()
-ax = Axis(f[1,1], aspect=DataAspect())
-poly!(ax, aoi, color=:grey98)
-scatter!(ax, records, color=:grey80, markersize=12)
-lines!(ax, aoi, color=:grey10)
-for _ in 1:5
-    scatter!(ax, thin(records, 80.), markersize=6)
-end
-current_figure() #hide
-
-# effect of distance on number points
+# Finally, it is a good idea to examine how the number of remaining points is
+# affected by distance.
 
 #figure effect-distance-n-points
-D = LinRange(0, 350, 20)
+D = LinRange(0, 150, 25)
 n = [length(thin(records, d)) for d in D]
 f = Figure()
 ax = Axis(f[1,1]; xlabel="Distance (km)", ylabel="Nb. points")

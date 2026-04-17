dcdf5e244569322bd644f005ed66e21616bd399b
tpoisot <timothee.poisot@umontreal.ca>
2026-04-17 11:29:57 -0400

    feat: SGD implementation

--- SDeMo/src/classifiers/sgd.jl
+++ SDeMo/src/classifiers/sgd.jl
@@ -1,9 +1,17 @@
 # Log-loss
 #L(X, θ, β, Y) = log(1 + exp(-Y * f(X, θ, β)))
 
+# Hinge
+# L(X, θ, β, Y) = max(0, 1 - Y * f(X, θ, β))
+
+"""
+    SGD
+
+Stochastic gradient descent classifier.
+"""
 Base.@kwdef mutable struct SGD <: Classifier
-    μ::Vector{<:Real} = zeros(2) # Averages
-    σ::Vector{<:Real} = zeros(2) # Standard deviations
+    μ::Vector{Float64} = zeros(2) # Averages
+    σ::Vector{Float64} = ones(2) # Standard deviations
     η::Float64 = 0.01
     λ::Float64 = 0.1
     θ::Vector{Float64} = zeros(Float64, 2) # Coefficients
@@ -16,6 +24,8 @@ Base.@kwdef mutable struct SGD <: Classifier
     intercept::Bool = true
 end
 export SGD
+
+hyperparameters(::Type{SGD}) = (:η, :λ, :loss, :L2, :L1, :α, :epochs, :intercept)
 
 """
     lasso!(∂, θ, args...)
@@ -63,50 +73,84 @@ function _sgd_gradient_logloss!(∂, β, X, θ, Y)
     return ∂β
 end
 
+# ∂θ(X, Ŷ, Y) = Y * Ŷ < 1 ? -(Y * X) : 0.0
+# ∂β(X, Ŷ, Y) = Y * Ŷ < 1 ? -Y : 0.0
+# relink(x) = clamp(0.5 * (x + 1), 0, 1)
+
+
+function _sgd_gradient_hinge!(∂, β, X, θ, Y)
+    # Make prediction
+    ŷ = β
+    for i in eachindex(θ)
+        ŷ += θ[i] * X[i]
+    end
+    # Calculate gradient
+    for i in eachindex(θ)
+        ∂[i] = (Y * ŷ < 1) ? -(Y * X[i]) : 0.0
+    end
+    ∂β = (Y * ŷ < 1) ? -Y : 0.0
+    return ∂β
+end
+
 function train!(sgd::SGD, y::Vector{Bool}, X::Matrix{T}; kwargs...) where {T <: Number}
-    if sdg.loss == :logloss
+    if sgd.loss == :logloss
         gradient! = _sgd_gradient_logloss!
+    elseif sgd.loss == :hinge
+        gradient! = _sgd_gradient_hinge!
     end
 
-    sgd.μ = mean(X; dims = 2)
-    sgd.σ = std(X; dims = 2)
+    if sgd.L1 & sgd.L2
+        regul! = elasticnet!
+    elseif sgd.L1
+        regul! = lasso!
+    else
+        regul! = ridge!
+    end 
+
+    sgd.μ = vec(mean(X; dims = 2))
+    sgd.σ = vec(std(X; dims = 2))
     Z = (X .- sgd.μ) ./ sgd.σ
     Y = 2 .* y .- 1
 
     # Starting parameters
-    sdg.θ = randn(size(Z, 1)) .* 1e-3
+    sgd.θ = randn(size(Z, 1)) .* 1e-5
 
     # Positions for the stochastic update
     positions = collect(eachindex(Y))
 
     # Temporary arrays for the gradient
-    ∂r = zeros(Float64, length(θ))
-    ∂g = zeros(Float64, length(θ))
+    ∂r = zeros(Float64, length(sgd.θ))
+    ∂g = zeros(Float64, length(sgd.θ))
 
     # Main loop
-    for epoch in Base.OneTo(sdg.epochs)
-        ηₜ = 0.9999^(epoch - 1) * sdg.η / length(Y)
+    for epoch in Base.OneTo(sgd.epochs)
+        ηₜ = 0.9999^(epoch - 1) * sgd.η / length(Y)
         Random.shuffle!(positions)
         for position in positions
-            ∂β = gradient!(∂g, sdg.β, Z[:, position], sdg.θ, Y[position])
-            elasticnet!(∂r, sdg.θ, sdg.α)
-            for i in eachindex(sdg.θ)
-                sdg.θ[i] -= ηₜ * (∂g[i] + sdg.λ * ∂r[i])
+            ∂β = gradient!(∂g, sgd.β, Z[:, position], sgd.θ, Y[position])
+            regul!(∂r, sgd.θ, sgd.α)
+            for i in eachindex(sgd.θ)
+                sgd.θ[i] -= ηₜ * (∂g[i] + sgd.λ * ∂r[i])
             end
-            if intercept
-                sdg.β -= ηₜ * ∂β
+            if sgd.intercept
+                sgd.β -= ηₜ * ∂β
             end
         end
     end
 
     # This is trained now
-    return sdg
+    return sgd
 end
 
-function StatsAPI.predict(sdg::SDG, x::Vector{T}) where {T <: Number}
-    if sdg.loss == :logloss
-        relink(x) = 1 / (1 + exp(-x))
+_sgd_link_logloss(x) = 1 / (1 + exp(-x))
+_sgd_link_hinge(x) = clamp(0.5 * (x + 1), 0, 1)
+
+function StatsAPI.predict(sgd::SGD, X::Matrix{T}) where {T <: Number}
+    if sgd.loss == :logloss
+        link = _sgd_link_logloss
+    elseif sgd.loss == :hinge
+        link = _sgd_link_hinge
     end
-    z = (x .- sgd.μ) ./ sgd.σ
-    return relink.(sdg.θ' * z .+ sdg.β)
+    Z = (X .- sgd.μ) ./ sgd.σ
+    return vec(link.(sgd.θ' * Z .+ sgd.β))
 end

42837a4189b8c22ac64b59f652b6ca7e53277d2a
tpoisot <timothee.poisot@umontreal.ca>
2026-08-24 12:40:38 -0400

    revert: background points need a smarter API
    This reverts commit a0ccbcc15b7d190be974164d156544cd926132be.

--- PseudoAbsences/src/pseudoabsencemask.jl
+++ PseudoAbsences/src/pseudoabsencemask.jl
@@ -4,7 +4,7 @@ function _layer_works_for_pseudoabsence(layer::SDMLayer{T}) where {T<:Bool}
 end
 
 """
-    pseudoabsencemask(::Type{RandomSelection}, presences::SDMLayer{Bool}; background::Bool=false)
+    pseudoabsencemask(::Type{RandomSelection}, presences::SDMLayer{Bool}, background::Bool = false)
 
 Generates a mask for pseudo-absences using the random selection method. Candidate
 cells for the pseudo-absence mask are (i) within the bounding box of the _layer_
@@ -12,9 +12,9 @@ cells for the pseudo-absence mask are (i) within the bounding box of the _layer_
 layer.
 
 The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data.
+presence location can be part of the background point data.
 """
-function pseudoabsencemask(::Type{RandomSelection}, presences::SDMLayer{Bool}; background::Bool=false)
+function pseudoabsencemask(::Type{RandomSelection}, presences::SDMLayer{Bool}, background::Bool=false)
   _layer_works_for_pseudoabsence(presences)
   if background
     return presences | !presences
@@ -42,35 +42,28 @@ end
 end
 
 """
-    pseudoabsencemask(::Type{SurfaceRangeEnvelope}, presences::SDMLayer{Bool}; background::Bool=false)
+    pseudoabsencemask(::Type{SurfaceRangeEnvelope}, presences::SDMLayer{Bool})
 
 Generates a mask from which pseudo-absences can be drawn, by picking cells that
 are (i) within the bounding box of occurrences, (ii) valued in the layer, and
 (iii) not already occupied by an occurrence
-
-The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data.
 """
-function pseudoabsencemask(::Type{SurfaceRangeEnvelope}, presences::SDMLayer{Bool}; background::Bool=false)
+function pseudoabsencemask(::Type{SurfaceRangeEnvelope}, presences::SDMLayer{Bool})
   _layer_works_for_pseudoabsence(presences)
   presence_only = nodata(presences, false)
-  bgarea = zeros(presences, Bool)
+  background = zeros(presences, Bool)
   lon = extrema([k[1] for k in keys(presence_only)])
   lat = extrema([k[2] for k in keys(presence_only)])
   for occupied_cell in keys(presences)
     if lon[1] <= occupied_cell[1] <= lon[2]
       if lat[1] <= occupied_cell[2] <= lat[2]
-        if background
-          bgarea[occupied_cell] = true
-        else
-          if ~(presences[occupied_cell])
-            bgarea[occupied_cell] = true
-          end
+        if ~(presences[occupied_cell])
+          background[occupied_cell] = true
         end
       end
     end
   end
-  return bgarea
+  return background
 end
 
 @testitem "We can use SurfaceRangeEnvelope" begin
@@ -92,45 +85,20 @@ end
   end
 end
 
-@testitem "We can use SurfaceRangeEnvelope with background" begin
-  using PseudoAbsences.SimpleSDMLayers
-  n = SDMLayer(zeros(Bool, 10, 10))
-  n.grid[2, 2] = true
-  n.grid[9, 9] = true
-  p = pseudoabsencemask(SurfaceRangeEnvelope, n; background=true)
-  for i in axes(n.grid, 1)
-    for j in axes(n.grid, 2)
-      if (2 <= i <= 9) & (2 <= j <= 9)
-        # Test the surface range
-        @test p.grid[i, j] == true
-      else
-        # Test the exterior borders (all false)
-        @test p.grid[i, j] == false
-      end
-    end
-  end
-end
-
 """
-    pseudoabsencemask(::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f=minimum, background::Bool=true)
+    pseudoabsencemask( ::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f = minimum, )
 
 Generates a mask for pseudo-absences using the distance to event method.
-Candidate cells are weighted according to their distance to a known
-observation, with far away places being more likely. Depending on the
-distribution of distances, it may be a very good idea to flatten this layer
-using `log` or an exponent. The `f` function is used to determine which
-distance is reported (`minimum` by default, but can be any other relevant
-function).
-
-The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data in all other
-functions, but is a noop here. Setting it to either value makes no difference
-as the distance to an event will always be represented as 0.
+Candidate cells are weighted according to their distance to a known observation,
+with far away places being more likely. Depending on the distribution of
+distances, it may be a very good idea to flatten this layer using `log` or an
+exponent. The `f` function is used to determine which distance is reported
+(`minimum` by default, but can be any other relevant function).
 """
-function pseudoabsencemask(::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f=minimum, background::Bool=true)
+function pseudoabsencemask(::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f=minimum)
   _layer_works_for_pseudoabsence(presences)
   presence_only = nodata(presences, false)
-  bgarea = zeros(presences, Float64)
+  background = zeros(presences, Float64)
 
   prj = SimpleSDMLayers.Proj.Transformation(
     presences.crs,
@@ -142,14 +110,14 @@ function pseudoabsencemask(::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f
   points = [prj(E[i.I[2]], N[i.I[1]]) for i in keys(presence_only)]
 
   # Prepare for thread-safe parallelism
-  bg = keys(bgarea)
+  bg = keys(background)
   chunk_size = max(1, length(bg) ÷ (50 * Threads.nthreads()))
   data_chunks = Base.Iterators.partition(bg, chunk_size)
   tasks = map(data_chunks) do chunk
     Threads.@spawn begin
       for k in chunk
         pk = prj(E[k.I[2]], N[k.I[1]])
-        bgarea[k] =
+        background[k] =
           f([PseudoAbsences._distancefunction(pk, ko) for ko in points])
       end
     end
@@ -158,7 +126,7 @@ function pseudoabsencemask(::Type{DistanceToEvent}, presences::SDMLayer{Bool}; f
   # Fetch the tasks
   fetch.(tasks)
 
-  return bgarea
+  return background
 end
 
 function _zero_occ!(bg, presences)
@@ -169,89 +137,65 @@ function _zero_occ!(bg, presences)
 end
 
 """
-    pseudoabsencemask( ::Type{WithinRadius}, presences::SDMLayer{Bool}; distance::Number=100.0, background::Bool=false, kwargs...)
+    pseudoabsencemask(::Type{WithinRadius}, presences::SDMLayer{Bool}; distance::Number = 100.0, )
 
 Generates a mask for pseudo-absences where pseudo-absences can be within a
 `distance` (in kilometers) of the original observation. Internally, this uses
 `DistanceToEvent`. All other keyword arguments are passed to
 `pseudoabsencemask`.
-
-The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data.
 """
 function pseudoabsencemask(
   ::Type{WithinRadius},
   presences::SDMLayer{Bool};
   distance::Number=100.0,
-  background::Bool=false,
   kwargs...
 )
   _layer_works_for_pseudoabsence(presences)
   bg = pseudoabsencemask(DistanceToEvent, presences; kwargs...)
-  bgarea = bg .<= distance
-  if background
-    return bgarea
-  else
-    return _zero_occ!(bgarea, presences)
-  end
+  background = bg .<= distance
+  return _zero_occ!(background, presences)
 end
 
 """
-    pseudoabsencemask( ::Type{WithoutRadius}, presences::SDMLayer{Bool}; distance::Number=100.0, background::Bool=false, kwargs...)
+    pseudoabsencemask(::Type{WithoutRadius}, presences::SDMLayer{Bool}; distance::Number = 100.0, )
 
 Generates a mask for pseudo-absences where pseudo-absences can be outside of a
 `distance` (in kilometers) of the original observation. Internally, this uses
 `DistanceToEvent`. All other keyword arguments are passed to
 `pseudoabsencemask`.
-
-The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data.
 """
 function pseudoabsencemask(
   ::Type{WithoutRadius},
   presences::SDMLayer{Bool};
   distance::Number=100.0,
-  background::Bool=false,
   kwargs...
 )
   _layer_works_for_pseudoabsence(presences)
   bg = pseudoabsencemask(DistanceToEvent, presences; kwargs...)
-  bgarea = bg .>= distance
-  if background
-    return bgarea
-  else
-    return _zero_occ!(bgarea, presences)
-  end
+  background = bg .>= distance
+  return _zero_occ!(background, presences)
 end
 
 
 """
-    pseudoabsencemask( ::Type{BetweenRadius}, presences::SDMLayer{Bool}; closer::Number=10.0, further::Number=100.0, background::Bool=false, kwargs...)
+    pseudoabsencemask(::Type{BetweenRadius}, presences::SDMLayer{Bool}; closer::Number = 10.0, further::Number=100.0)
 
 Generates a mask for pseudo-absences where pseudo-absences can be in a band
 given by two distances (in kilometers) of the original observation. Internally,
 this uses `DistanceToEvent`. All other keyword arguments are passed to
 `pseudoabsencemask`.
-
-The `background` keyword (defaults to `false`) is used to determine whether the
-presence locations can be part of the background point data.
 """
 function pseudoabsencemask(
   ::Type{BetweenRadius},
   presences::SDMLayer{Bool};
   closer::Number=10.0,
   further::Number=100.0,
-  background::Bool=false,
   kwargs...
 )
   _layer_works_for_pseudoabsence(presences)
   bg = pseudoabsencemask(DistanceToEvent, presences; kwargs...)
-  bgarea = closer .<= bg .<= further
-  if background
-    return bgarea
-  else
-    return _zero_occ!(bgarea, presences)
-  end
+  background = closer .<= bg .<= further
+  return _zero_occ!(background, presences)
 end
 
 """
